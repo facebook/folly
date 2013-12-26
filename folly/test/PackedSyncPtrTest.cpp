b4d29567be019d95235f4f35be47d5c6f9cf40d9
Jim Meyering <meyering@fb.com>
2013-12-26 10:44:10 -0800

    folly: avoid ASAN-detected new[] vs "delete" mismatch
    Summary:
    * folly/test/PackedSyncPtrTest.cpp: Avoid an operator new [] vs
    operator delete mismatch.
    
    Test Plan:
    fbconfig --platform-all=gcc-4.8.1-glibc-2.17 --sanitize=address \
    folly/test:packed_sync_ptr_test && fbmake runtests
    
    Reviewed By: tudorb@fb.com
    
    FB internal diff: D1111227

--- folly/test/PackedSyncPtrTest.cpp
+++ folly/test/PackedSyncPtrTest.cpp
@@ -53,7 +53,7 @@ TEST(PackedSyncPtr, Basic) {
   sp.setExtra(0x13);
   EXPECT_EQ(sp.extra(), 0x13);
   EXPECT_EQ((sp.get() + 1)->second, 7);
-  delete sp.get();
+  delete[] sp.get();
   auto newP = new std::pair<int,int>();
   sp.set(newP);
   EXPECT_EQ(sp.extra(), 0x13);
