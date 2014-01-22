8c54ed8b5d71b83d5cdc047a67b252938ec82bba
Nicholas Ormrod <njormrod@fb.com>
2014-01-21 16:09:49 -0800

    Removed duplicate #includes
    Summary:
    Removed duplicate #includes.
    
    Facebook: The removals were examined by hand: several duplicate includes
    were not removed because they were/seemed to
    be in different preprocessor branches of the source.
    
    @override-unit-failures
    
    Test Plan: arc unit
    
    Reviewed By: robbert@fb.com
    
    FB internal diff: D1136491

--- folly/test/ApplyTupleTest.cpp
+++ folly/test/ApplyTupleTest.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013 Facebook, Inc.
+ * Copyright 2014 Facebook, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,7 +19,6 @@
 #include "folly/ApplyTuple.h"
 #include <gtest/gtest.h>
 
-#include <iostream>
 #include <memory>
 
 namespace {
