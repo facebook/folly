b7433faa1bfffab07e581cf431371293b2f5e697
Elizabeth Smith <elizabeths@fb.com>
2014-04-18 09:28:00 -0700

    x64 detection missing an include
    Summary: rwspinlock.h was missing the portability.h header which made the x64 detection go wonky, but only shows up when used with isolated rwspinlock use
    
    Test Plan: fbmake runtests
    
    Reviewed By: andrei.alexandrescu@fb.com
    
    FB internal diff: D1284387
    
    Blame Revision: D1282140

--- folly/RWSpinLock.h
+++ folly/RWSpinLock.h
@@ -106,6 +106,8 @@ pthread_rwlock_t Read        728698     24us       101ns     7.28ms     194us
 
 */
 
+#include "folly/Portability.h"
+
 #if defined(__GNUC__) && !defined(__clang__) && \
   (defined(__i386) || FOLLY_X64 || \
    defined(ARCH_K8))
