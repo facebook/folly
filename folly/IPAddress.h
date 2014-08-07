a1f3fd0497e2917af25163859de3ae961ebf1f78
Marc Celani <marccelani@fb.com>
2014-08-07 13:24:05 -0700

    store ipaddresses in folly::IPAddress
    Summary: Save space and avoid extra mallocs
    
    Test Plan: fbconfig -r smc/zeus; fbmake runtests
    
    Reviewed By: henryf@fb.com
    
    Subscribers: zeus-diffs@
    
    FB internal diff: D1476688
    
    Tasks: 4832974

--- folly/IPAddress.h
+++ folly/IPAddress.h
@@ -255,7 +255,7 @@ class IPAddress : boost::totally_ordered<IPAddress> {
   bool empty() const { return (family_ == AF_UNSPEC); }
 
   // @return true if address is initialized
-  explicit operator bool() { return !empty(); }
+  explicit operator bool() const { return !empty(); }
 
   // @return true if this is an IPAddressV4 instance
   bool isV4() const { return (family_ == AF_INET); }
