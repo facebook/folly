f72645a63bd710bd0613eb7dad60e7f690fc8bc8
Mike Kaplinskiy <mikekap@vineapp.com>
2014-11-10 02:08:38 -0500

    Fix stack size detection on OS X

--- folly/futures/Future.h
+++ folly/futures/Future.h
@@ -33,7 +33,7 @@
 
 namespace folly {
 
-template <class> struct Promise;
+template <class> class Promise;
 
 template <typename T>
 struct isFuture : std::false_type {
@@ -53,7 +53,7 @@ struct isTry<Try<T>> : std::true_type {};
 
 namespace detail {
 
-template <class> struct Core;
+template <class> class Core;
 template <class...> struct VariadicContext;
 
 template<typename F, typename... Args>
