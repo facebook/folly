7660d85b9e902101b5415f618613adca5bd17f94
Dave Watson <davejwatson@fb.com>
2015-01-21 08:12:33 -0800

    codecs
    Summary:
    Start of codec framework.  Copied the frame based codecs almost exactly from netty, but made to fit our pipeline management.
    
    BytesToMessageCodec is slightly different: Netty preprocesses all the available data to a List<Message>, while this codec a) Only does one message at a time to avoid queueing issues, and b) doesn't template the message type, and just passes an IOBuf
    
    I'm fighting the type system to get the pipelines to play nice to each other:  I'd rather template the message type, but it prevents stacking outgoing handlers on top of it, since you ahve to specify both the input/output type for each handler, even if you only care about the output type.  Suggestions to fix?   Netty gets around this by lots of dynamic casting to Object type, but we can't do that in C++ since we don't have a base object type
    
    Test Plan: Includes lots of tests
    
    Reviewed By: jsedgwick@fb.com
    
    Subscribers: jsedgwick, doug, fugalh, folly-diffs@
    
    FB internal diff: D1758189
    
    Tasks: 5002361, 5002316
    
    Signature: t1:1758189:1421170225:6bc8cc6a0bb461a965665bc88f7009033b215da9

---
+++ folly/wangle/codec/CodecTest.cpp
@@ -0,0 +1,350 @@
+/*
+ * Copyright 2015 Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <gtest/gtest.h>
+
+#include <folly/wangle/codec/FixedLengthFrameDecoder.h>
+#include <folly/wangle/codec/LengthFieldBasedFrameDecoder.h>
+#include <folly/wangle/codec/LengthFieldPrepender.h>
+
+using namespace folly;
+using namespace folly::wangle;
+using namespace folly::io;
+
+class FrameTester
+    : public BytesToBytesHandler {
+ public:
+  FrameTester(std::function<void(std::unique_ptr<IOBuf>)> test)
+    : test_(test) {}
+
+  void read(Context* ctx, IOBufQueue& q) {
+    test_(q.move());
+  }
+ private:
+  std::function<void(std::unique_ptr<IOBuf>)> test_;
+};
+
+class BytesReflector
+    : public BytesToBytesHandler {
+ public:
+
+  Future<void> write(Context* ctx, std::unique_ptr<IOBuf> buf) {
+    IOBufQueue q_(IOBufQueue::cacheChainLength());
+    q_.append(std::move(buf));
+    ctx->fireRead(q_);
+
+    return makeFuture();
+  }
+};
+
+TEST(CodecTest, FixedLengthFrameDecoder) {
+  ChannelPipeline<IOBufQueue&, std::unique_ptr<IOBuf>> pipeline;
+  int called = 0;
+
+  pipeline
+    .addBack(FixedLengthFrameDecoder(10))
+    .addBack(FrameTester([&](std::unique_ptr<IOBuf> buf) {
+        auto sz = buf->computeChainDataLength();
+        called++;
+        EXPECT_EQ(sz, 10);
+      }))
+    .finalize();
+
+  auto buf3 = IOBuf::create(3);
+  buf3->append(3);
+  auto buf11 = IOBuf::create(11);
+  buf11->append(11);
+  auto buf16 = IOBuf::create(16);
+  buf16->append(16);
+
+  IOBufQueue q(IOBufQueue::cacheChainLength());
+
+  q.append(std::move(buf3));
+  pipeline.read(q);
+  EXPECT_EQ(called, 0);
+
+  q.append(std::move(buf11));
+  pipeline.read(q);
+  EXPECT_EQ(called, 1);
+
+  q.append(std::move(buf16));
+  pipeline.read(q);
+  EXPECT_EQ(called, 3);
+}
+
+TEST(CodecTest, LengthFieldFramePipeline) {
+  ChannelPipeline<IOBufQueue&, std::unique_ptr<IOBuf>> pipeline;
+  int called = 0;
+
+  pipeline
+    .addBack(BytesReflector())
+    .addBack(LengthFieldBasedFrameDecoder())
+    .addBack(FrameTester([&](std::unique_ptr<IOBuf> buf) {
+        auto sz = buf->computeChainDataLength();
+        called++;
+        EXPECT_EQ(sz, 2);
+      }))
+    .addBack(LengthFieldPrepender())
+    .finalize();
+
+  auto buf = IOBuf::create(2);
+  buf->append(2);
+  pipeline.write(std::move(buf));
+  EXPECT_EQ(called, 1);
+}
+
+TEST(CodecTest, LengthFieldFramePipelineLittleEndian) {
+  ChannelPipeline<IOBufQueue&, std::unique_ptr<IOBuf>> pipeline;
+  int called = 0;
+
+  pipeline
+    .addBack(BytesReflector())
+    .addBack(LengthFieldBasedFrameDecoder(4, 100, 0, 0, 4, false))
+    .addBack(FrameTester([&](std::unique_ptr<IOBuf> buf) {
+        auto sz = buf->computeChainDataLength();
+        called++;
+        EXPECT_EQ(sz, 1);
+      }))
+    .addBack(LengthFieldPrepender(4, 0, false, false))
+    .finalize();
+
+  auto buf = IOBuf::create(1);
+  buf->append(1);
+  pipeline.write(std::move(buf));
+  EXPECT_EQ(called, 1);
+}
+
+TEST(CodecTest, LengthFieldFrameDecoderSimple) {
+  ChannelPipeline<IOBufQueue&, std::unique_ptr<IOBuf>> pipeline;
+  int called = 0;
+
+  pipeline
+    .addBack(LengthFieldBasedFrameDecoder())
+    .addBack(FrameTester([&](std::unique_ptr<IOBuf> buf) {
+        auto sz = buf->computeChainDataLength();
+        called++;
+        EXPECT_EQ(sz, 1);
+      }))
+    .finalize();
+
+  auto bufFrame = IOBuf::create(4);
+  bufFrame->append(4);
+  RWPrivateCursor c(bufFrame.get());
+  c.writeBE((uint32_t)1);
+  auto bufData = IOBuf::create(1);
+  bufData->append(1);
+
+  IOBufQueue q(IOBufQueue::cacheChainLength());
+
+  q.append(std::move(bufFrame));
+  pipeline.read(q);
+  EXPECT_EQ(called, 0);
+
+  q.append(std::move(bufData));
+  pipeline.read(q);
+  EXPECT_EQ(called, 1);
+}
+
+TEST(CodecTest, LengthFieldFrameDecoderNoStrip) {
+  ChannelPipeline<IOBufQueue&, std::unique_ptr<IOBuf>> pipeline;
+  int called = 0;
+
+  pipeline
+    .addBack(LengthFieldBasedFrameDecoder(2, 10, 0, 0, 0))
+    .addBack(FrameTester([&](std::unique_ptr<IOBuf> buf) {
+        auto sz = buf->computeChainDataLength();
+        called++;
+        EXPECT_EQ(sz, 3);
+      }))
+    .finalize();
+
+  auto bufFrame = IOBuf::create(2);
+  bufFrame->append(2);
+  RWPrivateCursor c(bufFrame.get());
+  c.writeBE((uint16_t)1);
+  auto bufData = IOBuf::create(1);
+  bufData->append(1);
+
+  IOBufQueue q(IOBufQueue::cacheChainLength());
+
+  q.append(std::move(bufFrame));
+  pipeline.read(q);
+  EXPECT_EQ(called, 0);
+
+  q.append(std::move(bufData));
+  pipeline.read(q);
+  EXPECT_EQ(called, 1);
+}
+
+TEST(CodecTest, LengthFieldFrameDecoderAdjustment) {
+  ChannelPipeline<IOBufQueue&, std::unique_ptr<IOBuf>> pipeline;
+  int called = 0;
+
+  pipeline
+    .addBack(LengthFieldBasedFrameDecoder(2, 10, 0, -2, 0))
+    .addBack(FrameTester([&](std::unique_ptr<IOBuf> buf) {
+        auto sz = buf->computeChainDataLength();
+        called++;
+        EXPECT_EQ(sz, 3);
+      }))
+    .finalize();
+
+  auto bufFrame = IOBuf::create(2);
+  bufFrame->append(2);
+  RWPrivateCursor c(bufFrame.get());
+  c.writeBE((uint16_t)3); // includes frame size
+  auto bufData = IOBuf::create(1);
+  bufData->append(1);
+
+  IOBufQueue q(IOBufQueue::cacheChainLength());
+
+  q.append(std::move(bufFrame));
+  pipeline.read(q);
+  EXPECT_EQ(called, 0);
+
+  q.append(std::move(bufData));
+  pipeline.read(q);
+  EXPECT_EQ(called, 1);
+}
+
+TEST(CodecTest, LengthFieldFrameDecoderPreHeader) {
+  ChannelPipeline<IOBufQueue&, std::unique_ptr<IOBuf>> pipeline;
+  int called = 0;
+
+  pipeline
+    .addBack(LengthFieldBasedFrameDecoder(2, 10, 2, 0, 0))
+    .addBack(FrameTester([&](std::unique_ptr<IOBuf> buf) {
+        auto sz = buf->computeChainDataLength();
+        called++;
+        EXPECT_EQ(sz, 5);
+      }))
+    .finalize();
+
+  auto bufFrame = IOBuf::create(4);
+  bufFrame->append(4);
+  RWPrivateCursor c(bufFrame.get());
+  c.write((uint16_t)100); // header
+  c.writeBE((uint16_t)1); // frame size
+  auto bufData = IOBuf::create(1);
+  bufData->append(1);
+
+  IOBufQueue q(IOBufQueue::cacheChainLength());
+
+  q.append(std::move(bufFrame));
+  pipeline.read(q);
+  EXPECT_EQ(called, 0);
+
+  q.append(std::move(bufData));
+  pipeline.read(q);
+  EXPECT_EQ(called, 1);
+}
+
+TEST(CodecTest, LengthFieldFrameDecoderPostHeader) {
+  ChannelPipeline<IOBufQueue&, std::unique_ptr<IOBuf>> pipeline;
+  int called = 0;
+
+  pipeline
+    .addBack(LengthFieldBasedFrameDecoder(2, 10, 0, 2, 0))
+    .addBack(FrameTester([&](std::unique_ptr<IOBuf> buf) {
+        auto sz = buf->computeChainDataLength();
+        called++;
+        EXPECT_EQ(sz, 5);
+      }))
+    .finalize();
+
+  auto bufFrame = IOBuf::create(4);
+  bufFrame->append(4);
+  RWPrivateCursor c(bufFrame.get());
+  c.writeBE((uint16_t)1); // frame size
+  c.write((uint16_t)100); // header
+  auto bufData = IOBuf::create(1);
+  bufData->append(1);
+
+  IOBufQueue q(IOBufQueue::cacheChainLength());
+
+  q.append(std::move(bufFrame));
+  pipeline.read(q);
+  EXPECT_EQ(called, 0);
+
+  q.append(std::move(bufData));
+  pipeline.read(q);
+  EXPECT_EQ(called, 1);
+}
+
+TEST(CodecTest, LengthFieldFrameDecoderStripPrePostHeader) {
+  ChannelPipeline<IOBufQueue&, std::unique_ptr<IOBuf>> pipeline;
+  int called = 0;
+
+  pipeline
+    .addBack(LengthFieldBasedFrameDecoder(2, 10, 2, 2, 4))
+    .addBack(FrameTester([&](std::unique_ptr<IOBuf> buf) {
+        auto sz = buf->computeChainDataLength();
+        called++;
+        EXPECT_EQ(sz, 3);
+      }))
+    .finalize();
+
+  auto bufFrame = IOBuf::create(6);
+  bufFrame->append(6);
+  RWPrivateCursor c(bufFrame.get());
+  c.write((uint16_t)100); // pre header
+  c.writeBE((uint16_t)1); // frame size
+  c.write((uint16_t)100); // post header
+  auto bufData = IOBuf::create(1);
+  bufData->append(1);
+
+  IOBufQueue q(IOBufQueue::cacheChainLength());
+
+  q.append(std::move(bufFrame));
+  pipeline.read(q);
+  EXPECT_EQ(called, 0);
+
+  q.append(std::move(bufData));
+  pipeline.read(q);
+  EXPECT_EQ(called, 1);
+}
+
+TEST(CodecTest, LengthFieldFrameDecoderStripPrePostHeaderFrameInclHeader) {
+  ChannelPipeline<IOBufQueue&, std::unique_ptr<IOBuf>> pipeline;
+  int called = 0;
+
+  pipeline
+    .addBack(LengthFieldBasedFrameDecoder(2, 10, 2, -2, 4))
+    .addBack(FrameTester([&](std::unique_ptr<IOBuf> buf) {
+        auto sz = buf->computeChainDataLength();
+        called++;
+        EXPECT_EQ(sz, 3);
+      }))
+    .finalize();
+
+  auto bufFrame = IOBuf::create(6);
+  bufFrame->append(6);
+  RWPrivateCursor c(bufFrame.get());
+  c.write((uint16_t)100); // pre header
+  c.writeBE((uint16_t)5); // frame size
+  c.write((uint16_t)100); // post header
+  auto bufData = IOBuf::create(1);
+  bufData->append(1);
+
+  IOBufQueue q(IOBufQueue::cacheChainLength());
+
+  q.append(std::move(bufFrame));
+  pipeline.read(q);
+  EXPECT_EQ(called, 0);
+
+  q.append(std::move(bufData));
+  pipeline.read(q);
+  EXPECT_EQ(called, 1);
+}
---
+++ folly/wangle/codec/README.md
@@ -0,0 +1,5 @@
+Codecs are modeled after netty's codecs:
+
+https://github.com/netty/netty/tree/master/codec/src/main/java/io/netty/handler/codec
+
+Most of the changes are due to differing memory allocation strategies.
