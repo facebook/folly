f22baf0e9594d15052564d8fe9823d457af10377
Louis Brandy <ldbrandy@fb.com>
2015-04-10 17:36:56 -0700

    parse flags in unit tests
    Summary: This code has useful logging when turned up high enough (e.g. --v=11). But we need to parse the flags to see it.
    
    Test Plan: trial & error
    
    Reviewed By: yfeldblum@fb.com
    
    Subscribers: enis, folly-diffs@, jsedgwick, yfeldblum, darshan, chalfant
    
    FB internal diff: D1985965
    
    Signature: t1:1985965:1428713065:4607610ec438416fffb5fced1867efa13544eef0

--- folly/futures/test/main.cpp
+++ folly/futures/test/main.cpp
@@ -15,8 +15,10 @@
  */
 
 #include <gtest/gtest.h>
+#include <gflags/gflags.h>
 
 int main(int argc, char** argv) {
   ::testing::InitGoogleTest(&argc, argv);
+  ::google::ParseCommandLineFlags(&argc, &argv, true);
   return RUN_ALL_TESTS();
 }
