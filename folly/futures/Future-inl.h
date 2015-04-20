ad932c32057925606acb5156dd58e2626a8cccc3
James Sedgwick <jsedgwick@fb.com>
2015-04-20 12:20:01 -0700

    revert D1985475, clang still borked
    Summary:
    This reverts commit bb08e2405dc68e6dab1f0066b15faa2821ad3dc7.
    
    Test Plan: revert-hammer
    
    Reviewed By: hans@fb.com
    
    Subscribers: mathieubaudet, folly-diffs@, jsedgwick, yfeldblum, chalfant
    
    FB internal diff: D2006283
    
    Signature: t1:2006283:1429556243:41e6b3189ce2da5a0f1a32a663ff8761628ca574

--- folly/futures/Future-inl.h
+++ folly/futures/Future-inl.h
@@ -236,16 +236,6 @@ Future<T>::then(R(Caller::*func)(Args...), Caller *instance) {
 }
 
 template <class T>
-template <class... Args>
-auto Future<T>::then(Executor* x, Args&&... args)
-  -> decltype(this->then(std::forward<Args>(args)...))
-{
-  auto oldX = getExecutor();
-  setExecutor(x);
-  return this->then(std::forward<Args>(args)...).via(oldX);
-}
-
-template <class T>
 Future<void> Future<T>::then() {
   return then([] (Try<T>&& t) {});
 }
