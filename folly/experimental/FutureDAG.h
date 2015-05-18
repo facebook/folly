f51b046693ba4e381b19e1f1f4c1e0b8efdbb287
James Sedgwick <jsedgwick@fb.com>
2015-05-18 08:42:50 -0700

    RFC: FutureDAG
    Summary:
    See task. Set up a DAG of Future-returning tasks (optionally with executors) and eventually kick them off.
    One big question is ownership. Currently the user would be responsible for ensuring that the FutureDAG outlives its own completion. This requirement could go away with shared_from_this magic maybe
    
    Test Plan: unit. I didn't bother to test via() functionality because it's too much work for now - the functionality is trivial. Same for "true-async" dags...
    
    Reviewed By: hans@fb.com
    
    Subscribers: folly-diffs@, jsedgwick, yfeldblum, chalfant
    
    FB internal diff: D2073481
    
    Signature: t1:2073481:1431961131:82a8898502d5308f6ab3cc8cc5b84b016d3998fe

---
+++ folly/experimental/FutureDAG.h
@@ -0,0 +1,151 @@
+/*
+ * Copyright 2015 Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#pragma once
+
+#include <folly/futures/Future.h>
+#include <folly/futures/SharedPromise.h>
+
+namespace folly {
+
+class FutureDAG : public std::enable_shared_from_this<FutureDAG> {
+ public:
+  static std::shared_ptr<FutureDAG> create() {
+    return std::shared_ptr<FutureDAG>(new FutureDAG());
+  }
+
+  typedef size_t Handle;
+  typedef std::function<Future<void>()> FutureFunc;
+
+  Handle add(FutureFunc func, Executor* executor = nullptr) {
+    nodes.emplace_back(std::move(func), executor);
+    return nodes.size() - 1;
+  }
+
+  void dependency(Handle a, Handle b) {
+    nodes[b].dependencies.push_back(a);
+    nodes[a].hasDependents = true;
+  }
+
+  Future<void> go() {
+    if (hasCycle()) {
+      return makeFuture<void>(std::runtime_error("Cycle in FutureDAG graph"));
+    }
+    std::vector<Handle> rootNodes;
+    std::vector<Handle> leafNodes;
+    for (Handle handle = 0; handle < nodes.size(); handle++) {
+      if (nodes[handle].dependencies.empty()) {
+        rootNodes.push_back(handle);
+      }
+      if (!nodes[handle].hasDependents) {
+        leafNodes.push_back(handle);
+      }
+    }
+
+    auto sinkHandle = add([] { return Future<void>(); });
+    for (auto handle : leafNodes) {
+      dependency(handle, sinkHandle);
+    }
+
+    auto sourceHandle = add(nullptr);
+    for (auto handle : rootNodes) {
+      dependency(sourceHandle, handle);
+    }
+
+    for (Handle handle = 0; handle < nodes.size() - 1; handle++) {
+      std::vector<Future<void>> dependencies;
+      for (auto depHandle : nodes[handle].dependencies) {
+        dependencies.push_back(nodes[depHandle].promise.getFuture());
+      }
+
+      collect(dependencies)
+        .via(nodes[handle].executor)
+        .then([this, handle] {
+          nodes[handle].func()
+            .then([this, handle] (Try<void>&& t) {
+              nodes[handle].promise.setTry(std::move(t));
+            });
+        })
+        .onError([this, handle] (exception_wrapper ew) {
+          nodes[handle].promise.setException(std::move(ew));
+        });
+    }
+
+    nodes[sourceHandle].promise.setValue();
+    auto that = shared_from_this();
+    return nodes[sinkHandle].promise.getFuture().ensure([that]{});
+  }
+
+ private:
+  FutureDAG() = default;
+
+  bool hasCycle() {
+    // Perform a modified topological sort to detect cycles
+    std::vector<std::vector<Handle>> dependencies;
+    for (auto& node : nodes) {
+      dependencies.push_back(node.dependencies);
+    }
+
+    std::vector<size_t> dependents(nodes.size());
+    for (auto& dependencyEdges : dependencies) {
+      for (auto handle : dependencyEdges) {
+        dependents[handle]++;
+      }
+    }
+
+    std::vector<Handle> handles;
+    for (Handle handle = 0; handle < nodes.size(); handle++) {
+      if (!nodes[handle].hasDependents) {
+        handles.push_back(handle);
+      }
+    }
+
+    while (!handles.empty()) {
+      auto handle = handles.back();
+      handles.pop_back();
+      while (!dependencies[handle].empty()) {
+        auto dependency = dependencies[handle].back();
+        dependencies[handle].pop_back();
+        if (--dependents[dependency] == 0) {
+          handles.push_back(dependency);
+        }
+      }
+    }
+
+    for (auto& dependencyEdges : dependencies) {
+      if (!dependencyEdges.empty()) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  struct Node {
+    Node(FutureFunc&& funcArg, Executor* executorArg) :
+      func(std::move(funcArg)), executor(executorArg) {}
+
+    FutureFunc func{nullptr};
+    Executor* executor{nullptr};
+    SharedPromise<void> promise;
+    std::vector<Handle> dependencies;
+    bool hasDependents{false};
+    bool visited{false};
+  };
+
+  std::vector<Node> nodes;
+};
+
+} // folly
