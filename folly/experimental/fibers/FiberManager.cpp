3f274113c79f2677547d6a1d849bf11c1f54dd73
Orvid King <blah38621@gmail.com>
2015-07-29 14:44:59 -0700

    Switch a couple of uses of __thread to FOLLY_TLS
    Summary: Because MSVC doesn't support `__thread`, and `FOLLY_TLS` is already setup to use the MSVC syntax when needed.
    Closes #269
    
    Reviewed By: @yfeldblum
    
    Differential Revision: D2283919
    
    Pulled By: @sgolemon

--- folly/experimental/fibers/FiberManager.cpp
+++ folly/experimental/fibers/FiberManager.cpp
@@ -28,7 +28,7 @@
 
 namespace folly { namespace fibers {
 
-__thread FiberManager* FiberManager::currentFiberManager_ = nullptr;
+FOLLY_TLS FiberManager* FiberManager::currentFiberManager_ = nullptr;
 
 FiberManager::FiberManager(std::unique_ptr<LoopController> loopController,
                            Options options) :
