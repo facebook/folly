42581035875b25d5fc001b32ad7ec016e4e6affe
Orvid King <blah38621@gmail.com>
2015-09-13 16:06:37 -0700

    Use .string() not .native() when working with boost::filesystem
    Summary: Because .native() produces a wstring under MSVC.
    Closes #306
    
    Reviewed By: @yfeldblum
    
    Differential Revision: D2419043
    
    Pulled By: @JoelMarcey

--- folly/experimental/NestedCommandLineApp.cpp
+++ folly/experimental/NestedCommandLineApp.cpp
@@ -30,7 +30,7 @@ namespace {
 // Guess the program name as basename(executable)
 std::string guessProgramName() {
   try {
-    return fs::executable_path().filename().native();
+    return fs::executable_path().filename().string();
   } catch (const std::exception&) {
     return "UNKNOWN";
   }
@@ -181,7 +181,7 @@ auto NestedCommandLineApp::findCommand(const std::string& name) const
 
 int NestedCommandLineApp::run(int argc, const char* const argv[]) {
   if (programName_.empty()) {
-    programName_ = fs::path(argv[0]).filename().native();
+    programName_ = fs::path(argv[0]).filename().string();
   }
   return run(std::vector<std::string>(argv + 1, argv + argc));
 }
