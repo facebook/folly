321542683a01c3f334047531e9b487f047129775
Mark Isaacson <markisaa@fb.com>
2016-02-12 12:56:41 -0800

    folly copyright 2015 -> copyright 2016
    Summary: Update copyright for 2016
    
    Reviewed By: igorsugak
    
    Differential Revision: D2828047
    
    fb-gh-sync-id: 8638188aeb694c3ca2a615cc2bc3bc9d251e388d
    shipit-source-id: 8638188aeb694c3ca2a615cc2bc3bc9d251e388d

--- folly/io/async/AsyncTimeout.h
+++ folly/io/async/AsyncTimeout.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 Facebook, Inc.
+ * Copyright 2016 Facebook, Inc.
  *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements. See the NOTICE file
