b363d9f7a37c23ad345861025ed07cf437adedaa
Michael Lee <mzlee@fb.com>
2016-03-01 07:48:18 -0800

    Remove unecessary int main from MemoryTest
    Summary: We do not need this main function, so remove it.
    
    Reviewed By: markisaa
    
    Differential Revision: D2992000
    
    fb-gh-sync-id: 30bc0734af0f61e047276dab9639ae29cc999bbb
    shipit-source-id: 30bc0734af0f61e047276dab9639ae29cc999bbb

--- folly/test/MemoryTest.cpp
+++ folly/test/MemoryTest.cpp
@@ -93,11 +93,3 @@ TEST(rebind_allocator, sanity_check) {
   s.reset();
   ASSERT_EQ(nullptr, s.get());
 }
-
-int main(int argc, char **argv) {
-  FLAGS_logtostderr = true;
-  google::InitGoogleLogging(argv[0]);
-  testing::InitGoogleTest(&argc, argv);
-
-  return RUN_ALL_TESTS();
-}
