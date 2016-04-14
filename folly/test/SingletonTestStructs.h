7c17b38a91cd376968a8d85c23dc86a9460cc105
Den Raskovalov <denplusplus@fb.com>
2016-04-13 19:46:08 -0700

    remove duplicate licence prolog
    Reviewed By: yfeldblum
    
    Differential Revision: D3174112
    
    fb-gh-sync-id: 00a01f64fb43d2333818097bb2212e26e4dd5775
    fbshipit-source-id: 00a01f64fb43d2333818097bb2212e26e4dd5775

--- folly/test/SingletonTestStructs.h
+++ folly/test/SingletonTestStructs.h
@@ -15,24 +15,10 @@
  */
 #pragma once
 
-/*
- * Copyright 2016 Facebook, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 #include <memory>
+#include <stdexcept>
 #include <vector>
+
 #include <glog/logging.h>
 #include <gtest/gtest.h>
 
@@ -40,6 +26,7 @@
 // subclasses are created, and the ordering.  Also tracks a global
 // unique counter for each object.
 std::atomic<size_t> global_counter(19770326);
+
 struct Watchdog {
   static std::vector<Watchdog*>& creation_order() {
     static std::vector<Watchdog*> ret;
