f61c4a49b38f1f38bd236d1db4c1e831e96e3ec8
Guobao Sun <gsun@fb.com>
2016-08-12 21:45:45 -0700

    Fix typos in FBVector's comments
    Summary:
    Found this nit while trying to understand how fbvector works. 1) grwoing -> growing 2) remove that unnecessary "and"
    
    Not sure about reviewers' choosing so I included the author and whoever phabricator suggested to me.
    
    Reviewed By: meyering, yfeldblum
    
    Differential Revision: D3714166
    
    fbshipit-source-id: 8bf575c3f2954feb9e92611acd0aa23c5eee9a0a

--- folly/FBVector.h
+++ folly/FBVector.h
@@ -1139,9 +1139,9 @@ private:
   // fbvector grows differently on two counts:
   //
   // (1) initial size
-  //     Instead of grwoing to size 1 from empty, and fbvector allocates at
-  //     least 64 bytes. You may still use reserve to reserve a lesser amount
-  //     of memory.
+  //     Instead of growing to size 1 from empty, fbvector allocates at least
+  //     64 bytes. You may still use reserve to reserve a lesser amount of
+  //     memory.
   // (2) 1.5x
   //     For medium-sized vectors, the growth strategy is 1.5x. See the docs
   //     for details.
