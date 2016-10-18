b74eb888a2090eb64a552f89cbea730bc9b150cb
Phil Willoughby <philwill@fb.com>
2016-10-18 15:42:55 -0700

    Fix for ExceptionWrapperTest
    Summary:
    Some platforms do not output anything when `std::terminate` is called; this
    fixes the test on those platforms.
    
    Reviewed By: yfeldblum, jsedgwick
    
    Differential Revision: D4001052
    
    fbshipit-source-id: 671fbca9d6d22e372189b6554440c268c0cff60b

--- folly/test/ExceptionWrapperTest.cpp
+++ folly/test/ExceptionWrapperTest.cpp
@@ -229,13 +229,9 @@ TEST(ExceptionWrapper, getExceptionPtr_test) {
   EXPECT_THROW(std::rethrow_exception(eptr), IntException);
 
   // Test with empty ew.
-
-  // Disabling pending fix for fbandroid: t13822116
-  /*
   exception_wrapper empty_ew;
   eptr = empty_ew.getExceptionPtr();
-  EXPECT_DEATH(std::rethrow_exception(eptr), "exception");
-  */
+  EXPECT_FALSE(eptr);
 }
 
 TEST(ExceptionWrapper, with_exception_deduction) {
