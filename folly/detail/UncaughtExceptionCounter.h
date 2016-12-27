1e03974395c7007e75ed7847ad7d28b36a1c3a1b
Adrian Hamza <adriah@fb.com>
2016-12-27 14:18:20 -0800

    Make UncaughtExceptionCounter public
    Summary: Make UncaughtExceptionCounter public by moving it from folly/detail to folly. I am adding a scope performance counter that works in a similar way to SCOPE_EXIT/SCOPE_ERROR and I need the UncaughtExceptionCounter functionality.
    
    Reviewed By: yfeldblum, ericniebler
    
    Differential Revision: D4342783
    
    fbshipit-source-id: a1848e89cbb6340e2ac48adabf7bf76cece1b86d

--- folly/detail/UncaughtExceptionCounter.h
+++ folly/detail/UncaughtExceptionCounter.h
@@ -18,27 +18,7 @@
 
 #include <exception>
 
-#if defined(__GNUG__) || defined(__CLANG__)
-#define FOLLY_EXCEPTION_COUNT_USE_CXA_GET_GLOBALS
-namespace __cxxabiv1 {
-// forward declaration (originally defined in unwind-cxx.h from from libstdc++)
-struct __cxa_eh_globals;
-// declared in cxxabi.h from libstdc++-v3
-extern "C" __cxa_eh_globals* __cxa_get_globals() noexcept;
-}
-#elif defined(_MSC_VER) && (_MSC_VER >= 1400) && \
-    (_MSC_VER < 1900) // MSVC++ 8.0 or greater
-#define FOLLY_EXCEPTION_COUNT_USE_GETPTD
-// forward declaration (originally defined in mtdll.h from MSVCRT)
-struct _tiddata;
-extern "C" _tiddata* _getptd(); // declared in mtdll.h from MSVCRT
-#elif defined(_MSC_VER) && (_MSC_VER >= 1900) // MSVC++ 2015
-#define FOLLY_EXCEPTION_COUNT_USE_STD
-#else
-// Raise an error when trying to use this on unsupported platforms.
-#error "Unsupported platform, don't include this header."
-#endif
-
+#include <folly/UncaughtExceptions.h>
 
 namespace folly { namespace detail {
 
@@ -54,41 +34,17 @@ namespace folly { namespace detail {
 class UncaughtExceptionCounter {
  public:
   UncaughtExceptionCounter() noexcept
-      : exceptionCount_(getUncaughtExceptionCount()) {}
+      : exceptionCount_(folly::uncaught_exceptions()) {}
 
   UncaughtExceptionCounter(const UncaughtExceptionCounter& other) noexcept
       : exceptionCount_(other.exceptionCount_) {}
 
   bool isNewUncaughtException() noexcept {
-    return getUncaughtExceptionCount() > exceptionCount_;
+    return folly::uncaught_exceptions() > exceptionCount_;
   }
 
  private:
-  int getUncaughtExceptionCount() noexcept;
-
   int exceptionCount_;
 };
-
-/**
- * Returns the number of uncaught exceptions.
- *
- * This function is based on Evgeny Panasyuk's implementation from here:
- * http://fburl.com/15190026
- */
-inline int UncaughtExceptionCounter::getUncaughtExceptionCount() noexcept {
-#if defined(FOLLY_EXCEPTION_COUNT_USE_CXA_GET_GLOBALS)
-  // __cxa_get_globals returns a __cxa_eh_globals* (defined in unwind-cxx.h).
-  // The offset below returns __cxa_eh_globals::uncaughtExceptions.
-  return *(reinterpret_cast<unsigned int*>(static_cast<char*>(
-      static_cast<void*>(__cxxabiv1::__cxa_get_globals())) + sizeof(void*)));
-#elif defined(FOLLY_EXCEPTION_COUNT_USE_GETPTD)
-  // _getptd() returns a _tiddata* (defined in mtdll.h).
-  // The offset below returns _tiddata::_ProcessingThrow.
-  return *(reinterpret_cast<int*>(static_cast<char*>(
-      static_cast<void*>(_getptd())) + sizeof(void*) * 28 + 0x4 * 8));
-#elif defined(FOLLY_EXCEPTION_COUNT_USE_STD)
-  return std::uncaught_exceptions();
-#endif
-}
 
 }} // namespaces
