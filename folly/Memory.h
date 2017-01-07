c5e6d87b2adc858072885ec321968c973372a7ec
Yedidya Feldblum <yfeldblum@fb.com>
2017-01-06 16:43:48 -0800

    Skip defined checks in folly/Memory.h
    Summary:
    [Folly] Skip `defined` checks in `folly/Memory.h`.
    
    Undefined symbols, when evaluated by the preprocessor in preprocessor integer contexts, evaluate to `0`. That works for these checks.
    
    Reviewed By: ericniebler
    
    Differential Revision: D4385026
    
    fbshipit-source-id: 3162f8b83431ebab2de53f9ba202e7ea8c339210

--- folly/Memory.h
+++ folly/Memory.h
@@ -36,10 +36,8 @@ namespace folly {
  * @author Xu Ning (xning@fb.com)
  */
 
-#if __cplusplus >= 201402L ||                                              \
-    (defined __cpp_lib_make_unique && __cpp_lib_make_unique >= 201304L) || \
-    (defined __ANDROID__ && __cplusplus >= 201300L) ||                     \
-    (defined(_MSC_VER) && _MSC_VER >= 1900)
+#if __cplusplus >= 201402L || __cpp_lib_make_unique >= 201304L || \
+    (__ANDROID__ && __cplusplus >= 201300L) || _MSC_VER >= 1900
 
 /* using override */ using std::make_unique;
 
