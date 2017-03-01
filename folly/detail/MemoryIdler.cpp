94a3d5d31413b318d8a978b003e0f93ddf44a617
Andrii Grynenko <andrii@fb.com>
2017-03-01 10:35:36 -0800

    Disable MemoryIdler::unmapUnusedStack with ASAN
    Summary: It doesn't work nicely with fake stacks (which are used with detect_stack_use_after_return).
    
    Reviewed By: yfeldblum
    
    Differential Revision: D4631995
    
    fbshipit-source-id: f139041314f8f64c0cf0a15874d6a4710fb2a5b6

--- folly/detail/MemoryIdler.cpp
+++ folly/detail/MemoryIdler.cpp
@@ -80,7 +80,7 @@ void MemoryIdler::flushLocalMallocCaches() {
 // and arithmetic (and bug compatibility) are not portable.  The set of
 // platforms could be increased if it was useful.
 #if (FOLLY_X64 || FOLLY_PPC64) && defined(_GNU_SOURCE) && \
-    defined(__linux__) && !FOLLY_MOBILE
+    defined(__linux__) && !FOLLY_MOBILE && !FOLLY_SANITIZE_ADDRESS
 
 static FOLLY_TLS uintptr_t tls_stackLimit;
 static FOLLY_TLS size_t tls_stackSize;
