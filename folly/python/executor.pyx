1d3ca26b6dd5e42d56f97c7b8ce20bbf21bf162e
Jason Fried <fried@fb.com>
2017-03-20 11:48:49 -0700

    Folly Futures to Python Asyncio Futures Bridge
    Summary:
    folly/python/futures.h provides some helper functions for bridging folly::future to asyncio.Future.
    
    folly/python/NotificationQueueExecutor.h is a Driveable executor that has a fileno() method that can be monitored using (select, epoll) to determine if the drive method should be called.
    
    folly/python/executor.pyx is an implementation of a "driver" for the NotificationQueueExecutor from the python asyncio side. It tracks also keeps track of asyncio eventloops to Executor mappings.
    
    the getExecutor() from folly/python/futures.h uses that mapping to return the correct executor for this python thread.
    
    Reviewed By: andriigrynenko, yfeldblum
    
    Differential Revision: D4687029
    
    fbshipit-source-id: e79314606ffa18cb6933fe6b749991bfea646cde

---
+++ folly/python/executor.pyx
@@ -0,0 +1,34 @@
+import asyncio
+from folly cimport cFollyExecutor
+from folly.executor cimport cNotificationQueueExecutor
+from libcpp.memory cimport make_unique, unique_ptr
+from cython.operator cimport dereference as deref
+
+#asynico Loops to NotificationQueueExecutor
+loop_to_q = {}
+
+
+cdef class NotificationQueueExecutor:
+   def __cinit__(self):
+       self.cQ = make_unique[cNotificationQueueExecutor]();
+
+   def fileno(NotificationQueueExecutor self):
+       return deref(self.cQ).fileno()
+
+   def drive(NotificationQueueExecutor self):
+       deref(self.cQ).drive()
+
+   def __dealloc__(NotificationQueueExecutor self):
+       # We drive it one last time
+       deref(self.cQ).drive()
+
+
+cdef cFollyExecutor* get_executor():
+   loop = asyncio.get_event_loop()
+   try:
+       Q = <NotificationQueueExecutor>(loop_to_q[loop])
+   except KeyError:
+       Q = NotificationQueueExecutor()
+       loop.add_reader(Q.fileno(), Q.drive)
+       loop_to_q[loop] = Q
+   return Q.cQ.get()
