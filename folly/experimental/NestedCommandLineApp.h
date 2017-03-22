3c0df74643b83f1362b1053082c5e27a96f2c6e3
Brian Smith <bcsmith@fb.com>
2017-03-21 20:26:02 -0700

    Add additional points to configure help output
    Summary: Sometimes tools need to output a little more help context before or after help.
    
    Reviewed By: yfeldblum
    
    Differential Revision: D4719112
    
    fbshipit-source-id: e6f59312d722e0669053644d862c81758855b538

--- folly/experimental/NestedCommandLineApp.h
+++ folly/experimental/NestedCommandLineApp.h
@@ -67,6 +67,8 @@ class NestedCommandLineApp {
   explicit NestedCommandLineApp(
       std::string programName = std::string(),
       std::string version = std::string(),
+      std::string programHeading = std::string(),
+      std::string programHelpFooter = std::string(),
       InitFunction initFunction = InitFunction());
 
   /**
@@ -142,6 +144,8 @@ class NestedCommandLineApp {
       const std::vector<std::string>& args);
 
   std::string programName_;
+  std::string programHeading_;
+  std::string programHelpFooter_;
   std::string version_;
   InitFunction initFunction_;
   boost::program_options::options_description globalOptions_;
