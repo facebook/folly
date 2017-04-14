a411c5f39eae10019582d3c19375ee8816f2e4ce
Yedidya Feldblum <yfeldblum@fb.com>
2017-04-14 00:22:47 -0700

    Initialize the singleton-vault early in init()
    Summary:
    Initialize the singleton-vault early in `init()` to avoid ordering problems with other parts of `init()`.
    
    Initialization is done by calling `SingletonVault::singleton()->registrationComplete()`. Do that before (almost) everything, in particular, before parsing args and initializing logging.
    
    Note that this just marks it okay for singletons to be instantiated, but does not by itself instantiate any singletons. However, this is useful if, in some way, parsing args or initializing logging should happen to use singletons.
    
    Reviewed By: andriigrynenko
    
    Differential Revision: D4889777
    
    fbshipit-source-id: 6817db22d38a498cd20e361fc574a146029432ec

--- folly/init/Init.cpp
+++ folly/init/Init.cpp
@@ -36,6 +36,10 @@ void init(int* argc, char*** argv, bool removeFlags) {
   google::InstallFailureSignalHandler();
 #endif
 
+  // Move from the registration phase to the "you can actually instantiate
+  // things now" phase.
+  folly::SingletonVault::singleton()->registrationComplete();
+
   gflags::ParseCommandLineFlags(argc, argv, removeFlags);
 
   auto programName = argc && argv && *argc > 0 ? (*argv)[0] : "unknown";
@@ -44,13 +48,7 @@ void init(int* argc, char*** argv, bool removeFlags) {
 #ifdef FOLLY_USE_SYMBOLIZER
   // Don't use glog's DumpStackTraceAndExit; rely on our signal handler.
   google::InstallFailureFunction(abort);
-#endif
 
-  // Move from the registration phase to the "you can actually instantiate
-  // things now" phase.
-  folly::SingletonVault::singleton()->registrationComplete();
-
-#ifdef FOLLY_USE_SYMBOLIZER
   // Actually install the callbacks into the handler.
   folly::symbolizer::installFatalSignalCallbacks();
 #endif
