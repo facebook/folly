51e345da33505616db9804d98145a33f5eaf930c
Christopher Dykes <cdykes@fb.com>
2017-06-21 11:22:23 -0700

    Shift the exception tracer benchmark and test into the test directory
    Summary: Tests and benchmarks don't belong in the same directory as source code.
    
    Reviewed By: yfeldblum
    
    Differential Revision: D5288080
    
    fbshipit-source-id: 865c6dfbeff751bda49870e5284e168d33aff256

--- folly/experimental/exception_tracer/test/ExceptionTracerTest.cpp
+++ folly/experimental/exception_tracer/test/ExceptionTracerTest.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 Facebook, Inc.
+ * Copyright 2017-present Facebook, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,8 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-
 #include <iostream>
 #include <stdexcept>
 
