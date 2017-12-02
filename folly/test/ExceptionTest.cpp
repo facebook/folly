20272b4ec1c1ac41cc6bd408b8c454794873d4fb
Adam Simpkins <simpkins@fb.com>
2017-12-01 18:05:54 -0800

    add makeSystemError*() helper functions
    Summary:
    Add makeSystemError() helper functions that are similar to the existing
    throwSystemError() functions but just return the exception rather than throwing
    it.  This is helpful for callers using folly::Expected or folly::Future, where
    they need to return an exception type rather than throwing it.
    
    This also includes comments about the fact that this code is using the wrong
    error category.  The C++ standard indicates that std::generic_category() for
    POSIX errno values.  I am not fixing this as part of this diff, since this
    change has the potential to break existing users, and would need a fair amount
    of testing first.
    
    Reviewed By: yfeldblum
    
    Differential Revision: D6456771
    
    fbshipit-source-id: 4724b51b8d4a7f513ae70ea1b0c4f0516cfc205f

--- folly/test/ExceptionTest.cpp
+++ folly/test/ExceptionTest.cpp
@@ -90,5 +90,34 @@ TEST(ExceptionTest, Simple) {
                       EIO, "hello world");
 }
 
+TEST(ExceptionTest, makeSystemError) {
+  errno = ENOENT;
+  auto ex = makeSystemErrorExplicit(EDEADLK, "stuck");
+  EXPECT_EQ(EDEADLK, ex.code().value());
+  EXPECT_EQ(std::system_category(), ex.code().category());
+  EXPECT_TRUE(StringPiece{ex.what()}.contains("stuck"))
+      << "what() string missing input message: " << ex.what();
+
+  ex = makeSystemErrorExplicit(EDOM, 300, " is bigger than max=", 255);
+  EXPECT_EQ(EDOM, ex.code().value());
+  EXPECT_EQ(std::system_category(), ex.code().category());
+  EXPECT_TRUE(StringPiece{ex.what()}.contains("300 is bigger than max=255"))
+      << "what() string missing input message: " << ex.what();
+
+  errno = EINVAL;
+  ex = makeSystemError("bad argument ", 1234, ": bogus");
+  EXPECT_EQ(EINVAL, ex.code().value());
+  EXPECT_EQ(std::system_category(), ex.code().category());
+  EXPECT_TRUE(StringPiece{ex.what()}.contains("bad argument 1234: bogus"))
+      << "what() string missing input message: " << ex.what();
+
+  errno = 0;
+  ex = makeSystemError("unexpected success");
+  EXPECT_EQ(0, ex.code().value());
+  EXPECT_EQ(std::system_category(), ex.code().category());
+  EXPECT_TRUE(StringPiece{ex.what()}.contains("unexpected success"))
+      << "what() string missing input message: " << ex.what();
+}
+
 } // namespace test
 } // namespace folly
