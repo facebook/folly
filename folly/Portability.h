0a1444e778c15825978bf917f087367406cfd6f9
Yedidya Feldblum <yfeldblum@fb.com>
2017-12-13 14:53:49 -0800

    Remove unused #include <string.h> from folly/Portability.h
    Summary: [Folly] Remove unused `#include <string.h>` from `folly/Portability.h`.
    
    Reviewed By: Orvid
    
    Differential Revision: D6557374
    
    fbshipit-source-id: 369ef0b622dc057e882ed0f4ad25b47fa3f05100

--- folly/Portability.h
+++ folly/Portability.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 Facebook, Inc.
+ * Copyright 2017-present Facebook, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,8 +15,6 @@
  */
 
 #pragma once
-
-#include <string.h>
 
 #include <cstddef>
 #include <type_traits>
