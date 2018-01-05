64c5a21374fa65e4179b3f165c269f4bf1534dba
Mingtao Yang <mingtao@fb.com>
2018-01-05 12:54:46 -0800

    Clarify folly::ssl::init documentation
    Summary: Adds wording indicating that it is safe to call this function multiple times.
    
    Reviewed By: yfeldblum
    
    Differential Revision: D6625530
    
    fbshipit-source-id: 2305f7b8e3451a6663003d23230b22c6f65a3729

--- folly/ssl/Init.h
+++ folly/ssl/Init.h
@@ -23,8 +23,10 @@ namespace folly {
 namespace ssl {
 /**
  * Initializes openssl. This should be invoked once, during the start of an
- * application. For OpenSSL < 1.1.0, any lock types should be set with
- * setLockTypes prior to the call to folly::ssl::init()
+ * application. Subsequent calls to this function are no-ops.
+ *
+ * For OpenSSL < 1.1.0, any lock types should be set with setLockTypes prior to
+ * the call to folly::ssl::init()
  */
 void init();
 
