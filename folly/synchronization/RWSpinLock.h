5cb23840adceb1c5f4d960967c9a66374a858b9e
Yedidya Feldblum <yfeldblum@fb.com>
2018-01-04 23:08:09 -0800

    Cut glog include from folly/synchronization/RWSpinLock.h
    Summary: [Folly] Cut `glog` include from `folly/synchronization/RWSpinLock.h`.
    
    Reviewed By: meyering
    
    Differential Revision: D6659300
    
    fbshipit-source-id: 0f1b79d98be0656694f57667252a16acd54e2414

--- folly/synchronization/RWSpinLock.h
+++ folly/synchronization/RWSpinLock.h
@@ -157,10 +157,7 @@ pthread_rwlock_t Read        728698     24us       101ns     7.28ms     194us
 
 #include <algorithm>
 #include <atomic>
-#include <string>
 #include <thread>
-
-#include <glog/logging.h>
 
 #include <folly/Likely.h>
 
