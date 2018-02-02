754ee1bca48edd4d47b8a419d7832ab2ecc31525
Adam Simpkins <simpkins@fb.com>
2018-02-02 14:26:13 -0800

    add constexpr_strcmp()
    Summary: Add a constexpr implementation of `strcmp()`
    
    Reviewed By: yfeldblum
    
    Differential Revision: D6881816
    
    fbshipit-source-id: 08a9e5ab9ef4a422ff6d004ec13b8ec8c71b0d6f

--- folly/portability/Constexpr.h
+++ folly/portability/Constexpr.h
@@ -33,6 +33,13 @@ constexpr size_t constexpr_strlen_internal(const Char* s, size_t len) {
 static_assert(
     constexpr_strlen_internal("123456789", 0) == 9,
     "Someone appears to have broken constexpr_strlen...");
+
+template <typename Char>
+constexpr int constexpr_strcmp_internal(const Char* s1, const Char* s2) {
+  return (*s1 == '\0' || *s1 != *s2)
+      ? (static_cast<int>(*s1 - *s2))
+      : constexpr_strcmp_internal(s1 + 1, s2 + 1);
+}
 } // namespace detail
 
 template <typename Char>
@@ -50,6 +57,24 @@ constexpr size_t constexpr_strlen(const char* s) {
   return std::strlen(s);
 #else
   return detail::constexpr_strlen_internal(s, 0);
+#endif
+}
+
+template <typename Char>
+constexpr int constexpr_strcmp(const Char* s1, const Char* s2) {
+  return detail::constexpr_strcmp_internal(s1, s2);
+}
+
+template <>
+constexpr int constexpr_strcmp(const char* s1, const char* s2) {
+#if FOLLY_HAS_FEATURE(cxx_constexpr_string_builtins)
+  // clang provides a constexpr builtin
+  return __builtin_strcmp(s1, s2);
+#elif defined(__GNUC__) && !defined(__clang__)
+  // strcmp() happens to already be constexpr under gcc
+  return std::strcmp(s1, s2);
+#else
+  return detail::constexpr_strcmp_internal(s1, s2);
 #endif
 }
 } // namespace folly
