d75c47478eec1fd8ccadd05b501817f74e3f4f0e
Neel Goyal <ngoyal@fb.com>
2018-02-26 11:13:57 -0800

    Add uint32 type flags
    Summary: Add DECLARE/DEFINE_uint32 to the portability layer
    
    Reviewed By: yfeldblum
    
    Differential Revision: D7085534
    
    fbshipit-source-id: 3f6f1bf79371b052706f4b3a03dca1d503b879da

--- folly/portability/GFlags.h
+++ folly/portability/GFlags.h
@@ -37,6 +37,7 @@
 #define DECLARE_double(_name) FOLLY_DECLARE_FLAG(double, D, _name)
 #define DECLARE_int32(_name) FOLLY_DECLARE_FLAG(int, I, _name)
 #define DECLARE_int64(_name) FOLLY_DECLARE_FLAG(long long, I64, _name)
+#define DECLARE_uint32(_name) FOLLY_DECLARE_FLAG(unsigned long, U32, _name)
 #define DECLARE_uint64(_name) FOLLY_DECLARE_FLAG(unsigned long long, U64, _name)
 #define DECLARE_string(_name) FOLLY_DECLARE_FLAG(std::string, S, _name)
 
@@ -54,6 +55,8 @@
   FOLLY_DEFINE_FLAG(int, I, _name, _default)
 #define DEFINE_int64(_name, _default, _description) \
   FOLLY_DEFINE_FLAG(long long, I64, _name, _default)
+#define DEFINE_uint32(_name, _default, _description) \
+  FOLLY_DEFINE_FLAG(unsigned long, U32, _name, _default)
 #define DEFINE_uint64(_name, _default, _description) \
   FOLLY_DEFINE_FLAG(unsigned long long, U64, _name, _default)
 #define DEFINE_string(_name, _default, _description) \
