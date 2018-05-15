2e29c4715e0957e44e8e3fdf8e37ca7f4526b6f3
Simon_CQK <cqk0100@gmail.com>
2018-05-14 18:18:55 -0700

    Replace std::unique_lock with std::lock_guard.
    Summary:
    std::lock_guard has a better performance than std::unique_lock in scoped-lock.
    Closes https://github.com/facebook/folly/pull/840
    
    Reviewed By: djwatson
    
    Differential Revision: D7888672
    
    Pulled By: yfeldblum
    
    fbshipit-source-id: 391082aa79ada67a3906e828d68f8cc2a29bf22c

--- folly/synchronization/ParkingLot.h
+++ folly/synchronization/ParkingLot.h
@@ -59,7 +59,7 @@ struct WaitNodeBase {
   }
 
   void wake() {
-    std::unique_lock<std::mutex> nodeLock(mutex_);
+    std::lock_guard<std::mutex> nodeLock(mutex_);
     signaled_ = true;
     cond_.notify_one();
   }
