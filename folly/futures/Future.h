9e7138402b637ba5cd247ecbcf711e3723e2a570
Alex Lambert <alexlambert@fb.com>
2018-06-16 08:16:33 -0700

    Add descriptive comment to FutureAlreadyContinued
    Summary:
    A comment to `FutureAlreadyContinued` to explain to readers in what circumstance it is thrown.
    
    (Note: this ignores all push blocking failures!)
    
    Reviewed By: yfeldblum
    
    Differential Revision: D8460097
    
    fbshipit-source-id: 49657e030efe72c026c1c3eea3612952746bb0e1

--- folly/futures/Future.h
+++ folly/futures/Future.h
@@ -57,6 +57,11 @@ class FOLLY_EXPORT FutureInvalid : public FutureException {
   FutureInvalid() : FutureException("Future invalid") {}
 };
 
+/// At most one continuation may be attached to any given Future.
+///
+/// If a continuation is attached to a future to which another continuation has
+/// already been attached, then an instance of FutureAlreadyContinued will be
+/// thrown instead.
 class FOLLY_EXPORT FutureAlreadyContinued : public FutureException {
  public:
   FutureAlreadyContinued() : FutureException("Future already continued") {}
