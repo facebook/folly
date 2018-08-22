f8afdfaa0db855b88d70f76ec1a97542c46a3c8a
Ajanthan Asogamoorthy <aasogamo@fb.com>
2018-08-22 13:09:27 -0700

    Link in fizz to wangle and wangle's dependencies
    Summary: Update cmake configurations + legocastle jobs in order to add fizz as a dependency to wangle
    
    Reviewed By: reanimus
    
    Differential Revision: D9337956
    
    fbshipit-source-id: 40f25694c2b3fd8aa37d254bc63a664f4c8ee526

--- build/fbcode_builder/specs/fbthrift.py
+++ build/fbcode_builder/specs/fbthrift.py
@@ -5,6 +5,8 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 import specs.folly as folly
+import specs.fizz as fizz
+import specs.sodium as sodium
 import specs.wangle as wangle
 import specs.zstd as zstd
 
@@ -22,7 +24,7 @@ def fbcode_builder_spec(builder):
     )
     builder.add_option('krb5/krb5:git_hash', 'krb5-1.16.1-final')
     return {
-        'depends_on': [folly, wangle, zstd],
+        'depends_on': [folly, fizz, sodium, wangle, zstd],
         'steps': [
             # This isn't a separete spec, since only fbthrift uses mstch.
             builder.github_project_workdir('no1msd/mstch', 'build'),
--- build/fbcode_builder/specs/proxygen.py
+++ build/fbcode_builder/specs/proxygen.py
@@ -5,12 +5,14 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 import specs.folly as folly
+import specs.fizz as fizz
+import specs.sodium as sodium
 import specs.wangle as wangle
 
 
 def fbcode_builder_spec(builder):
     return {
-        'depends_on': [folly, wangle],
+        'depends_on': [folly, wangle, fizz, sodium],
         'steps': [
             builder.fb_github_autoconf_install('proxygen/proxygen'),
         ],
