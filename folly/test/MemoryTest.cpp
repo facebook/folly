1a32e2cf3b4c991c3cf272d3367e93a93254d667
Dan Zimmerman <danzimm@fb.com>
2018-08-26 18:44:37 -0700

    Split out folly::enable_shared_from_this from Memory.h
    Summary:
    This doesn't need to be in Memory.h and is one of the reasons Memory.h fails to compile with -fno-exceptions, so split it out
    
    I include EnableSharedFromThis.h because I'm not sure how else to ensure backward compatibility any other way (and I have the macro so we can include Memory.h without including EnableSharedFromThis.h)
    
    Reviewed By: yfeldblum
    
    Differential Revision: D9385709
    
    fbshipit-source-id: bf2c3a757ae4eefba69e6191309d64b347becf12

--- folly/test/MemoryTest.cpp
+++ folly/test/MemoryTest.cpp
@@ -324,52 +324,6 @@ TEST(AllocatorObjectLifecycleTraits, compiles) {
       !folly::AllocatorHasDefaultObjectDestroy<TestAlloc5<S>, S>::value, "");
 }
 
-template <typename C>
-static void test_enable_shared_from_this(std::shared_ptr<C> sp) {
-  ASSERT_EQ(1l, sp.use_count());
-
-  // Test shared_from_this().
-  std::shared_ptr<C> sp2 = sp->shared_from_this();
-  ASSERT_EQ(sp, sp2);
-
-  // Test weak_from_this().
-  std::weak_ptr<C> wp = sp->weak_from_this();
-  ASSERT_EQ(sp, wp.lock());
-  sp.reset();
-  sp2.reset();
-  ASSERT_EQ(nullptr, wp.lock());
-
-  // Test shared_from_this() and weak_from_this() on object not owned by a
-  // shared_ptr. Undefined in C++14 but well-defined in C++17. Also known to
-  // work with libstdc++ >= 20150123. Feel free to add other standard library
-  // versions where the behavior is known.
-#if __cplusplus >= 201700L || __GLIBCXX__ >= 20150123L
-  C stack_resident;
-  ASSERT_THROW(stack_resident.shared_from_this(), std::bad_weak_ptr);
-  ASSERT_TRUE(stack_resident.weak_from_this().expired());
-#endif
-}
-
-TEST(enable_shared_from_this, compatible_with_std_enable_shared_from_this) {
-  // Compile-time compatibility.
-  class C_std : public std::enable_shared_from_this<C_std> {};
-  class C_folly : public folly::enable_shared_from_this<C_folly> {};
-  static_assert(
-      noexcept(std::declval<C_std>().shared_from_this()) ==
-          noexcept(std::declval<C_folly>().shared_from_this()),
-      "");
-  static_assert(
-      noexcept(std::declval<C_std const>().shared_from_this()) ==
-          noexcept(std::declval<C_folly const>().shared_from_this()),
-      "");
-  static_assert(noexcept(std::declval<C_folly>().weak_from_this()), "");
-  static_assert(noexcept(std::declval<C_folly const>().weak_from_this()), "");
-
-  // Runtime compatibility.
-  test_enable_shared_from_this(std::make_shared<C_folly>());
-  test_enable_shared_from_this(std::make_shared<C_folly const>());
-}
-
 template <typename T>
 struct ExpectingAlloc {
   using value_type = T;
