07f3b3df05f5b7fb7a9a42cf0bceaf0305fcafa5
Yunus Rahbar <yrahbar@fb.com>
2018-11-09 09:50:12 -0800

    Add copyright headers to fbcode builder source files
    Reviewed By: simpkins
    
    Differential Revision: D12990230
    
    fbshipit-source-id: 58d82299a8fe6aed75115008fe5dfb0f46d847c6

--- build/fbcode_builder/specs/fbthrift.py
+++ build/fbcode_builder/specs/fbthrift.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# Copyright (c) Facebook, Inc. and its affiliates.
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
