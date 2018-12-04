1cdb03423607ea958496563c51f896e35eebe195
Andrii Grynenko <andrii@fb.com>
2018-12-03 18:09:36 -0800

    Drop updates if value didn't change
    Summary: If observer generator function returned the same shared_ptr it's safe to not propagate such an update.
    
    Reviewed By: yfeldblum
    
    Differential Revision: D13249675
    
    fbshipit-source-id: 334c33e791b664eaec3c2698009f138bef04f8a6

--- folly/experimental/observer/test/ObserverTest.cpp
+++ folly/experimental/observer/test/ObserverTest.cpp
@@ -380,3 +380,31 @@ TEST(Observer, WaitForAllUpdates) {
 
   folly::observer_detail::ObserverManager::waitForAllUpdates();
 }
+
+TEST(Observer, IgnoreUpdates) {
+  int callbackCalled = 0;
+  folly::observer::SimpleObservable<int> observable(42);
+  auto observer =
+      folly::observer::makeObserver([even = std::make_shared<bool>(true),
+                                     odd = std::make_shared<bool>(false),
+                                     observer = observable.getObserver()] {
+        if (**observer % 2 == 0) {
+          return even;
+        }
+        return odd;
+      });
+  auto callbackHandle = observer.addCallback([&](auto) { ++callbackCalled; });
+  EXPECT_EQ(1, callbackCalled);
+
+  observable.setValue(43);
+  folly::observer_detail::ObserverManager::waitForAllUpdates();
+  EXPECT_EQ(2, callbackCalled);
+
+  observable.setValue(45);
+  folly::observer_detail::ObserverManager::waitForAllUpdates();
+  EXPECT_EQ(2, callbackCalled);
+
+  observable.setValue(46);
+  folly::observer_detail::ObserverManager::waitForAllUpdates();
+  EXPECT_EQ(3, callbackCalled);
+}
