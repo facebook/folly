af3040b33a25b7d13025650ef9008fc27395a68b
Caleb Marchent <cmarchent@fb.com>
2019-01-16 19:06:52 +0000

    Use find_program looking for Cython

--- CMake/FindCython.cmake
+++ CMake/FindCython.cmake
@@ -1,18 +1,23 @@
-
-execute_process(COMMAND cython --version
-                RESULT_VARIABLE _cython_retcode
-                OUTPUT_VARIABLE _cython_output
-                ERROR_VARIABLE _cython_output
-                OUTPUT_STRIP_TRAILING_WHITESPACE)
-
+find_program(_cython
+             NAMES cython cython3)
 unset(CYTHON_FOUND)
-if (${_cython_retcode} EQUAL 0)
-  separate_arguments(_cython_output)
-  list(GET _cython_output -1 CYTHON_VERSION_STRING)
-  message(STATUS "Found Cython Version ${CYTHON_VERSION_STRING}")
-  set(CYTHON_FOUND True)
+if (_cython)
+  execute_process(COMMAND ${_cython} --version
+                  RESULT_VARIABLE _cython_retcode
+                  OUTPUT_VARIABLE _cython_output
+                  ERROR_VARIABLE _cython_output
+                  OUTPUT_STRIP_TRAILING_WHITESPACE)
+
+  if (${_cython_retcode} EQUAL 0)
+    separate_arguments(_cython_output)
+    list(GET _cython_output -1 CYTHON_VERSION_STRING)
+    message(STATUS "Found Cython Version ${CYTHON_VERSION_STRING}")
+    set(CYTHON_FOUND True)
+  else ()
+    message(STATUS "Failed to get Cython version")
+  endif ()
 else ()
-  message(STATUS "Found Cython NOT FOUND")
+  message(STATUS "Cython not found")
 endif ()
 
 include(FindPackageHandleStandardArgs)
