821d5083fdf05a332f44b6a07b56ed9a533cc3f5
Wez Furlong <wez@fb.com>
2019-01-25 12:57:02 -0800

    rsocket: add fbcode builder bits
    Summary:
    This should allow more direct testing of changes without waiting for things to land on github first.
    
    As part of this, to avoid a conflict between the deps that bistro downloads and those used by
    the rest of the fbcode builder CI, I've updated the version of gtest used by bistro.
    
    Reviewed By: zertosh
    
    Differential Revision: D13802637
    
    fbshipit-source-id: fd71bfabd2a85f4f63c21b44a1e868f2d293180a

--- build/fbcode_builder/specs/fbthrift.py
+++ build/fbcode_builder/specs/fbthrift.py
@@ -7,6 +7,7 @@ from __future__ import unicode_literals
 
 import specs.folly as folly
 import specs.fizz as fizz
+import specs.rsocket as rsocket
 import specs.sodium as sodium
 import specs.wangle as wangle
 import specs.zstd as zstd
@@ -20,25 +21,15 @@ def fbcode_builder_spec(builder):
         'no1msd/mstch:git_hash',
         ShellQuoted('$(git describe --abbrev=0 --tags)')
     )
-    builder.add_option(
-        'rsocket/rsocket-cpp/build:cmake_defines', {'BUILD_TESTS': 'OFF'}
-    )
     builder.add_option('krb5/krb5:git_hash', 'krb5-1.16.1-final')
     return {
-        'depends_on': [folly, fizz, sodium, wangle, zstd],
+        'depends_on': [folly, fizz, sodium, rsocket, wangle, zstd],
         'steps': [
             # This isn't a separete spec, since only fbthrift uses mstch.
             builder.github_project_workdir('no1msd/mstch', 'build'),
             builder.cmake_install('no1msd/mstch'),
             builder.github_project_workdir('krb5/krb5', 'src'),
             builder.autoconf_install('krb5/krb5'),
-            builder.github_project_workdir(
-                'rsocket/rsocket-cpp', 'build'
-            ),
-            builder.step('configuration for rsocket', [
-                builder.cmake_configure('rsocket/rsocket-cpp/build'),
-            ]),
-            builder.cmake_install('rsocket/rsocket-cpp'),
             builder.fb_github_cmake_install('fbthrift/thrift'),
         ],
     }
