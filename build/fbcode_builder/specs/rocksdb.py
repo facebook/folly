a8422464dcc981f114fb4e5a61a8c2f26bb00199
Caleb Marchent <cmarchent@fb.com>
2019-03-27 13:38:26 -0700

    Allow LogDevice to build without Submodules (#71)
    Summary:
    As a step in the plan to migrate LogDevice to using fbcode_builder as
    the primary build method, allow LogDevice to build without submodules.
    In place of the self compiled modules, the CMake scripts will look in
    the system (or provided prefix locations) and use precompiled modules
    there. Users can then use fbcode_builder to ensure those required
    modules are built and available.
    
    * Use new 'folly_test_util' library rather than referencing sources
    directly
    Pull Request resolved: https://github.com/facebookincubator/LogDevice/pull/71
    
    Reviewed By: AhmedSoliman
    
    Differential Revision: D14578119
    
    Pulled By: calebmarchent
    
    fbshipit-source-id: 36400d41d3f1906919706d20d0fb53195fec2e63

---
+++ build/fbcode_builder/specs/rocksdb.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# Copyright (c) Facebook, Inc. and its affiliates.
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+
+def fbcode_builder_spec(builder):
+    builder.add_option("rocksdb/_build:cmake_defines", {
+        "USE_RTTI": "1",
+        "PORTABLE": "ON",
+    })
+    return {
+        "steps": [
+            builder.fb_github_cmake_install("rocksdb/_build"),
+        ],
+    }
