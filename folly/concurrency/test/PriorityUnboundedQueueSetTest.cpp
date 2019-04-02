74f36602c3b9d1104d33f18bf6b42f4be1fde551
Yedidya Feldblum <yfeldblum@fb.com>
2019-04-02 00:52:39 -0700

    PriorityUnboundedQueueSet, wrapping UnboundedQueue
    Summary: [Folly] `PriorityUnboundedQueueSet`, wrapping `UnboundedQueue`.
    
    Reviewed By: andriigrynenko
    
    Differential Revision: D14719777
    
    fbshipit-source-id: 529ddd8de5879908e36f6fc26e7a091230cb03da

---
+++ folly/concurrency/test/PriorityUnboundedQueueSetTest.cpp
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2019-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <vector>
+
+#include <folly/concurrency/PriorityUnboundedQueueSet.h>
+#include <folly/container/Enumerate.h>
+#include <folly/portability/GTest.h>
+#include <folly/synchronization/Baton.h>
+
+using namespace folly;
+
+class PriorityUnboundedQueueSetTest : public testing::Test {};
+
+TEST_F(PriorityUnboundedQueueSetTest, try_dequeue) {
+  PriorityUSPSCQueueSet<int, false> q(3);
+  q.at_priority(1).enqueue(42);
+  EXPECT_EQ(42, q.try_dequeue().value());
+}
+
+TEST_F(PriorityUnboundedQueueSetTest, try_peek) {
+  PriorityUSPSCQueueSet<int, false> q(3);
+  q.at_priority(1).enqueue(42);
+  EXPECT_EQ(42, *q.try_peek());
+  q.at_priority(1).enqueue(42);
+  EXPECT_EQ(42, q.try_dequeue().value());
+}
+
+TEST_F(PriorityUnboundedQueueSetTest, size_empty) {
+  PriorityUSPSCQueueSet<int, false> q(3);
+  EXPECT_TRUE(q.empty());
+  EXPECT_EQ(0, q.size());
+
+  q.at_priority(1).enqueue(42);
+  EXPECT_FALSE(q.empty());
+  EXPECT_EQ(1, q.size());
+
+  EXPECT_EQ(42, *q.try_peek());
+  EXPECT_FALSE(q.empty());
+  EXPECT_EQ(1, q.size());
+
+  EXPECT_EQ(42, q.try_dequeue().value());
+  EXPECT_TRUE(q.empty());
+  EXPECT_EQ(0, q.size());
+}
+
+TEST_F(PriorityUnboundedQueueSetTest, priority_order) {
+  PriorityUSPSCQueueSet<int, false> q(3);
+  EXPECT_EQ(0, q.size());
+  q.at_priority(1).enqueue(55);
+  q.at_priority(2).enqueue(42);
+  q.at_priority(0).enqueue(12);
+  q.at_priority(1).enqueue(27);
+  EXPECT_EQ(4, q.size());
+  EXPECT_EQ(12, q.try_dequeue().value());
+  EXPECT_EQ(55, q.try_dequeue().value());
+  EXPECT_EQ(27, q.try_dequeue().value());
+  EXPECT_EQ(42, q.try_dequeue().value());
+  EXPECT_EQ(0, q.size());
+}
