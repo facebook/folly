ad5d6ff528555c375eb31ae08c126ec4a7de7c31
Joe Loser <joeloser93@gmail.com>
2019-04-25 15:12:55 -0700

    Replace boost::is_member_pointer with std::is_member_pointer (#1085)
    Summary:
    :
    - Replace a call site in `DynamicParser-inl.h` using `boost::is_member_pointer`
      with the standard library equivalent.
    Pull Request resolved: https://github.com/facebook/folly/pull/1085
    
    Reviewed By: meyering
    
    Differential Revision: D14638847
    
    Pulled By: yfeldblum
    
    fbshipit-source-id: 8eb95607dc7134b9bbc6974052094a92623fe776

--- folly/experimental/DynamicParser-inl.h
+++ folly/experimental/DynamicParser-inl.h
@@ -24,7 +24,8 @@
  */
 #pragma once
 
-#include <boost/function_types/is_member_pointer.hpp>
+#include <type_traits>
+
 #include <boost/function_types/parameter_types.hpp>
 #include <boost/mpl/equal.hpp>
 #include <boost/mpl/pop_front.hpp>
@@ -48,9 +49,7 @@ class IdentifyCallable {
 
  private:
   template <typename Fn>
-  using IsMemFn =
-      typename boost::function_types::template is_member_pointer<decltype(
-          &Fn::operator())>;
+  using IsMemFn = std::is_member_pointer<decltype(&Fn::operator())>;
   template <typename Fn>
   constexpr static typename std::enable_if<IsMemFn<Fn>::value, Kind>::type test(
       IsMemFn<Fn>*) {
