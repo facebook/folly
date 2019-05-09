fd4e016588aa57ab8bb0c3123c605c0c586aaee8
Yuhan Hao <yuhanhao@fb.com>
2019-05-09 12:15:05 -0700

    remove mstch from fbthrift's fbcode_builder spec
    Summary: mstch is no longer required for fbthrift, we can remove it from oss build spec
    
    Reviewed By: vitaut
    
    Differential Revision: D15280267
    
    fbshipit-source-id: 5008e54af9c927f23b0d6acbf0d9beb6e004eee1

--- build/fbcode_builder/specs/fbthrift.py
+++ build/fbcode_builder/specs/fbthrift.py
@@ -13,21 +13,11 @@ import specs.sodium as sodium
 import specs.wangle as wangle
 import specs.zstd as zstd
 
-from shell_quoting import ShellQuoted
-
 
 def fbcode_builder_spec(builder):
-    # This API should change rarely, so build the latest tag instead of master.
-    builder.add_option(
-        'no1msd/mstch:git_hash',
-        ShellQuoted('$(git describe --abbrev=0 --tags)')
-    )
     return {
         'depends_on': [folly, fizz, fmt, sodium, rsocket, wangle, zstd],
         'steps': [
-            # This isn't a separete spec, since only fbthrift uses mstch.
-            builder.github_project_workdir('no1msd/mstch', 'build'),
-            builder.cmake_install('no1msd/mstch'),
             builder.fb_github_cmake_install('fbthrift/thrift'),
         ],
     }
