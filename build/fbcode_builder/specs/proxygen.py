8e85e992140a26f061bf2801c9cbe03c8a177bb0
Luca Niccolini <lniccolini@fb.com>
2019-06-08 09:09:55 -0700

    Back out "[proxygen][oss] enable CMake build (with HTTP/3)"
    Summary: Original commit changeset: 5e101fc38417
    
    Reviewed By: snarkmaster
    
    Differential Revision: D15726457
    
    fbshipit-source-id: 001ee9ebaee81ebc60996e5fb95c1870121b2dec

--- build/fbcode_builder/specs/proxygen.py
+++ build/fbcode_builder/specs/proxygen.py
@@ -7,26 +7,14 @@ from __future__ import unicode_literals
 
 import specs.folly as folly
 import specs.fizz as fizz
-import specs.mvfst as mvfst
 import specs.sodium as sodium
 import specs.wangle as wangle
-import specs.zstd as zstd
 
 
 def fbcode_builder_spec(builder):
-    builder.add_option(
-        'proxygen/proxygen:cmake_defines',
-        {
-            'BUILD_QUIC': 'ON',
-        }
-    )
     return {
-        'depends_on': [folly, wangle, fizz, sodium, zstd, mvfst],
+        'depends_on': [folly, wangle, fizz, sodium],
         'steps': [
-            # CMake build with QUIC/HTTP3
-            builder.fb_github_cmake_install(
-                'proxygen/proxygen', '..'),
-            # Legacy build with no QUIC/HTTP3
             builder.fb_github_autoconf_install('proxygen/proxygen'),
         ],
     }
