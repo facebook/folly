93b9af641decff4e64f5c58eccac195eeae4836e
Luca Niccolini <lniccolini@fb.com>
2019-06-25 02:56:06 -0700

    enable CMake build (with HTTP/3) -- take 2
    Reviewed By: snarkmaster
    
    Differential Revision: D15730421
    
    fbshipit-source-id: 51eaac2ad443aadbab3ba772e651287c6b330408

---
+++ build/fbcode_builder/specs/proxygen_quic.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# Copyright (c) Facebook, Inc. and its affiliates.
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import specs.folly as folly
+import specs.fizz as fizz
+import specs.mvfst as mvfst
+import specs.sodium as sodium
+import specs.wangle as wangle
+import specs.zstd as zstd
+
+
+def fbcode_builder_spec(builder):
+    builder.add_option("proxygen/proxygen:cmake_defines", {"BUILD_QUIC": "ON"})
+    return {
+        "depends_on": [folly, wangle, fizz, sodium, zstd, mvfst],
+        "steps": [builder.fb_github_cmake_install("proxygen/proxygen", "..")],
+    }
