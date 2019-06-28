0161dd5ec714fc2e6dfed9bcb57389b241672341
Christos Stratopoulos <c.stratopoulos@gmail.com>
2019-06-28 16:05:29 -0400

    guard noexcept specializations with feature test macro

--- folly/detail/PolyDetail.h
+++ folly/detail/PolyDetail.h
@@ -404,6 +404,7 @@ struct SignatureOf_<R (C::*)(As...) const, I> {
   using type = Ret<R, I> (*)(Data const&, Arg<As, I>...);
 };
 
+#ifdef __cpp_noexcept_function_type
 template <class R, class C, class... As, class I>
 struct SignatureOf_<R(C::*)(As...) noexcept, I> {
 	using type = std::add_pointer_t<Ret<R, I>(Data&, Arg<As, I>...) noexcept>;
@@ -413,6 +414,7 @@ template <class R, class C, class... As, class I>
 struct SignatureOf_<R(C::*)(As...) const noexcept, I> {
 	using type = std::add_pointer_t<Ret<R, I>(Data const&, Arg<As, I>...) noexcept>;
 };
+#endif
 
 template <class R, class This, class... As, class I>
 struct SignatureOf_<R (*)(This&, As...), I> {
@@ -435,10 +437,12 @@ struct ArgTypes_<User, I, Ret (*)(Data, Args...)> {
   using type = TypeList<Args...>;
 };
 
+#ifdef __cpp_noexcept_function_type
 template <FOLLY_AUTO User, class I, class Ret, class Data, class... Args>
 struct ArgTypes_<User, I, Ret (*)(Data, Args...) noexcept> {
   using type = TypeList<Args...>;
 };
+#endif
 
 template <FOLLY_AUTO User, class I>
 using ArgTypes = _t<ArgTypes_<User, I>>;
