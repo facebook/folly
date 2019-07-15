f18d8adbc9d06fa4bec68d7766638af2ce05d9f8
Nanshu Chen <nanshu@fb.com>
2019-07-15 11:05:16 -0700

    coro::Task bridge
    Reviewed By: andriigrynenko
    
    Differential Revision: D14637661
    
    fbshipit-source-id: 0f8852e30ff554e9f0125e2d918054e4bc1e5af5

---
+++ folly/python/test/simplecoro.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2019-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#pragma once
+#include <folly/experimental/coro/Task.h>
+#include <cstdint>
+
+namespace folly {
+namespace python {
+namespace test {
+
+folly::coro::Task<uint64_t> coro_getValueX5(uint64_t val) {
+  if (val == 0) {
+    throw std::invalid_argument("0 is not allowed");
+  }
+  co_return val * 5;
+}
+} // namespace test
+} // namespace python
+} // namespace folly
