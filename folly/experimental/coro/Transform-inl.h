600a1046cbe927d76ce913f91cd60ec7bf664926
Kirk Shoop <kirkshoop@fb.com>
2019-09-03 12:40:25 -0700

    Add folly::coro::transform() algorithm
    Summary: The `transform()` algorithm uses the provided function to transform each value in the provided input stream and produces each transformed value on the returned output stream.
    
    Reviewed By: lewissbaker
    
    Differential Revision: D16774893
    
    fbshipit-source-id: 7ab33706c667b1706c0cb0b1c0ecb9046f47a325

---
+++ folly/experimental/coro/Transform-inl.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2019-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <folly/experimental/coro/Transform.h>
+
+namespace folly {
+namespace coro {
+
+template <typename TransformFn, typename Reference, typename Value>
+AsyncGenerator<invoke_result_t<TransformFn&, Reference>> transform(
+    AsyncGenerator<Reference, Value> source,
+    TransformFn transformFn) {
+  while (auto item = co_await source.next()) {
+    co_yield invoke(transformFn, std::move(item).value());
+  }
+}
+
+} // namespace coro
+} // namespace folly
