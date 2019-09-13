0c5ceda68a92447a9c47d94649ad4631f1b4a38d
Hasnain Lakhani <mhl@fb.com>
2019-09-12 23:33:50 -0700

    Add CMake option to fetch deps statically
    Summary:
    We would like to build a version of proxygen that has minimal
    dependencies on dynamic libraries.
    
    Reviewed By: yfeldblum
    
    Differential Revision: D17228181
    
    fbshipit-source-id: cfd61afdfa978c49a536184f426502196241fb8a

---
+++ build/fbcode_builder/CMake/FBBuildOptions.cmake
@@ -0,0 +1,13 @@
+function (fb_activate_static_library_option)
+  option(USE_STATIC_DEPS_ON_UNIX
+    "If enabled, use static dependencies on unix systems. This is generally discouraged."
+    OFF
+  )
+  # Mark USE_STATIC_DEPS_ON_UNIX as an "advanced" option, since enabling it
+  # is generally discouraged.
+  mark_as_advanced(USE_STATIC_DEPS_ON_UNIX)
+
+  if(UNIX AND USE_STATIC_DEPS_ON_UNIX)
+    SET(CMAKE_FIND_LIBRARY_SUFFIXES ".a" PARENT_SCOPE)
+  endif()
+endfunction()
