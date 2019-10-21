ab636ee7e19c3a84be9d1c2f2f9fe11696497673
Rosen Penev <rosenp@gmail.com>
2019-10-21 16:58:45 -0700

    [clang-tidy] Use ranged based for loops
    Found with modernize-loop-convert
    
    Signed-off-by: Rosen Penev <rosenp@gmail.com>

--- folly/Fingerprint.cpp
+++ folly/Fingerprint.cpp
@@ -98,10 +98,10 @@ constexpr poly_table<Deg> make_poly_table() {
   for (uint16_t x = 0; x < 256; x++) {
     FingerprintPolynomial<Deg> t;
     t.setHigh8Bits(uint8_t(x));
-    for (int i = 0; i < 8; i++) {
+    for (auto & i : table) {
       t.mulXkmod(8, poly);
       for (size_t j = 0; j < poly_size(Deg); ++j) {
-        table[i][x][j] = t.get(j);
+        i[x][j] = t.get(j);
       }
     }
   }
