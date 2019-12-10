4427c1a8321bb138f667758172d4e2f47bb0675b
Lee Howes <lwh@fb.com>
2019-12-10 15:01:57 -0800

    Add weak symbol-based async logging calls
    Summary: Stub interface for logging functions to integrate with the GlobalExecutor and Futures.
    
    Reviewed By: andriigrynenko
    
    Differential Revision: D18229089
    
    fbshipit-source-id: ad57de08a8bd6a80e2d0af9a9792de86a8267176

---
+++ folly/detail/test/AsyncTraceTest.cpp
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <folly/detail/AsyncTrace.h>
+#include <folly/portability/GTest.h>
+
+TEST(FollyCountersTest, Trivial) {
+  folly::Executor* exec = nullptr;
+  folly::IOExecutor* io_exec = nullptr;
+  folly::async_tracing::logSetGlobalCPUExecutor(exec);
+  folly::async_tracing::logGetGlobalCPUExecutor(exec);
+  folly::async_tracing::logSetGlobalIOExecutor(io_exec);
+  folly::async_tracing::logGetGlobalIOExecutor(io_exec);
+  folly::async_tracing::logSetGlobalCPUExecutorToImmutable();
+  folly::async_tracing::logGetImmutableCPUExecutor(exec);
+  folly::async_tracing::logGetImmutableIOExecutor(io_exec);
+
+  folly::Executor* lastExec = nullptr;
+  folly::async_tracing::logSemiFutureVia(lastExec, exec);
+  folly::async_tracing::logFutureVia(lastExec, exec);
+
+  folly::async_tracing::logSemiFutureVia(lastExec, exec);
+  folly::async_tracing::logFutureVia(lastExec, exec);
+}
