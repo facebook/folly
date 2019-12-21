5edd05b578394a23ecfd4fc371d4e10bc150bae2
Yedidya Feldblum <yfeldblum@fb.com>
2019-12-21 03:19:19 -0800

    Register with AtFork without an object in BufferedRandomDevice
    Summary: [Folly] Register with `AtFork` without an object in `BufferedRandomDevice`, since no object is actually needed and since it is possible for the object passed in to be destroyed.
    
    Reviewed By: luciang
    
    Differential Revision: D19207385
    
    fbshipit-source-id: 7c0e2793d9b438e576bf26bc3b3b459b372aec6d

--- folly/Random.cpp
+++ folly/Random.cpp
@@ -115,9 +115,9 @@ BufferedRandomDevice::BufferedRandomDevice(size_t bufferSize)
     : bufferSize_(bufferSize),
       buffer_(new unsigned char[bufferSize]),
       ptr_(buffer_.get() + bufferSize) { // refill on first use
-  call_once(flag, [this]() {
+  call_once(flag, [] {
     detail::AtFork::registerHandler(
-        this,
+        nullptr,
         /*prepare*/ []() { return true; },
         /*parent*/ []() {},
         /*child*/
