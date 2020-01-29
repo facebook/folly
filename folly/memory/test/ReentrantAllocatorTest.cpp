05fe3c7b16b4189f5ae47d62624a101334efbe5a
Yedidya Feldblum <yfeldblum@fb.com>
2020-01-29 13:40:42 -0800

    reentrant_allocator
    Summary:
    [Folly] `reentrant_allocator`, a multi-thread-safe and async-signal-safe allocator.
    
    Based on `mmap` for dynamic allocation. While, technically, `mmap` is not documented to be async-signal-safe, in practice it is so at least on Linux. Take advantage.
    
    Details:
    * Large allocations are handled directly by `mmap` and deallocations by `munmap`, where large-size is 2^12 (1 page) by default; they are not tracked; they are aligned only to page boundaries.
    * Small allocations are handled by an `mmap`-backed refcounted arena list. Arena sections are block-size bytes less overhead by default, where block-size is 2^16 (16 pages) by default; they are allocated on demand; and they are linked together to allow for `munmap` when the allocator refcounted arena list is destroyed.
    
    Reviewed By: nbronson, luciang
    
    Differential Revision: D19222635
    
    fbshipit-source-id: adf30580c1bf3dd7f8dab13b1d4ac1e10b2f5c52

---
+++ folly/memory/test/ReentrantAllocatorTest.cpp
@@ -0,0 +1,110 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <folly/memory/ReentrantAllocator.h>
+
+#include <memory>
+#include <thread>
+#include <tuple>
+#include <vector>
+
+#include <folly/Utility.h>
+#include <folly/portability/GTest.h>
+
+class ReentrantAllocatorTest : public testing::Test {};
+
+TEST_F(ReentrantAllocatorTest, equality) {
+  folly::reentrant_allocator<void> a{folly::reentrant_allocator_options{}};
+  folly::reentrant_allocator<void> b{folly::reentrant_allocator_options{}};
+  EXPECT_TRUE(a == a);
+  EXPECT_FALSE(a == b);
+  EXPECT_FALSE(a != a);
+  EXPECT_TRUE(a != b);
+
+  folly::reentrant_allocator<int> a2{a};
+  folly::reentrant_allocator<int> b2{b};
+  EXPECT_TRUE(a2 == a);
+  EXPECT_FALSE(a2 == b);
+  EXPECT_TRUE(a2 == a2);
+  EXPECT_FALSE(a2 == b2);
+  EXPECT_FALSE(a2 != a);
+  EXPECT_TRUE(a2 != b);
+  EXPECT_FALSE(a2 != a2);
+  EXPECT_TRUE(a2 != b2);
+}
+
+TEST_F(ReentrantAllocatorTest, small) {
+  folly::reentrant_allocator<void> alloc{folly::reentrant_allocator_options{}};
+  std::vector<size_t, folly::reentrant_allocator<size_t>> vec{alloc};
+  for (auto i = 0u; i < (1u << 16); ++i) {
+    vec.push_back(i);
+  }
+  for (auto i = 0u; i < (1u << 16); ++i) {
+    EXPECT_EQ(i, vec.at(i));
+  }
+}
+
+TEST_F(ReentrantAllocatorTest, large) {
+  constexpr size_t const large_size_lg = 6;
+  struct alignas(1u << large_size_lg) type : std::tuple<size_t> {};
+  auto const opts = folly::reentrant_allocator_options{} //
+                        .large_size_lg(large_size_lg);
+  folly::reentrant_allocator<void> alloc{opts};
+  std::vector<type, folly::reentrant_allocator<type>> vec{alloc};
+  for (auto i = 0u; i < (1u << 16); ++i) {
+    vec.push_back({i});
+  }
+  for (auto i = 0u; i < (1u << 16); ++i) {
+    EXPECT_EQ(i, std::get<0>(vec.at(i)));
+  }
+}
+
+TEST_F(ReentrantAllocatorTest, zero) {
+  folly::reentrant_allocator<int> a{folly::reentrant_allocator_options{}};
+  a.deallocate(nullptr, 0);
+  auto ptr = a.allocate(0);
+  EXPECT_NE(nullptr, ptr);
+  a.deallocate(ptr, 0);
+}
+
+TEST_F(ReentrantAllocatorTest, self_assignment) {
+  folly::reentrant_allocator<int> a{folly::reentrant_allocator_options{}};
+  auto& i = *a.allocate(1);
+  ::new (&i) int(7);
+  EXPECT_EQ(7, i);
+  a = folly::as_const(a);
+  EXPECT_EQ(7, i);
+  a.deallocate(&i, 1);
+}
+
+TEST_F(ReentrantAllocatorTest, stress) {
+  struct alignas(256) big {};
+  folly::reentrant_allocator<void> a{folly::reentrant_allocator_options{}};
+  std::vector<std::thread> threads{4};
+  std::atomic<bool> done{false};
+  for (auto& th : threads) {
+    th = std::thread([&done, a] {
+      while (!done.load(std::memory_order_relaxed)) {
+        std::allocate_shared<big>(a);
+      }
+    });
+  }
+  /* sleep override */ std::this_thread::sleep_for(std::chrono::seconds(1));
+  done.store(true, std::memory_order_relaxed);
+  for (auto& th : threads) {
+    th.join();
+  }
+}
