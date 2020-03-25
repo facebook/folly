96149148fac1408c51dacc9fe40b7d90b6bad7c8
Adam Retter <adam.retter@googlemail.com>
2020-03-25 17:51:38 +0100

    Check for s390x not s390

--- folly/Portability.h
+++ folly/Portability.h
@@ -107,10 +107,10 @@ constexpr bool kHasUnalignedAccess = false;
 #define FOLLY_PPC64 0
 #endif
 
-#if defined(__s390__)
-#define FOLLY_S390 1
+#if defined(__s390x__)
+#define FOLLY_S390X 1
 #else
-#define FOLLY_S390 0
+#define FOLLY_S390X 0
 #endif
 
 namespace folly {
@@ -118,7 +118,7 @@ constexpr bool kIsArchArm = FOLLY_ARM == 1;
 constexpr bool kIsArchAmd64 = FOLLY_X64 == 1;
 constexpr bool kIsArchAArch64 = FOLLY_AARCH64 == 1;
 constexpr bool kIsArchPPC64 = FOLLY_PPC64 == 1;
-constexpr bool kIsArchS390 = FOLLY_S390 == 1;
+constexpr bool kIsArchS390X = FOLLY_S390X == 1;
 } // namespace folly
 
 namespace folly {
