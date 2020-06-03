7e211332d715b0791b5a0d448fadfbe6df94a98d
Victor Zverovich <viz@fb.com>
2020-06-03 09:46:04 -0700

    Update fmt version to 6.2.1
    Summary:
    Update fmt version to 6.2.1 for better compatibility with the version used in fbcode. Among other things this fixes fbthrift build failure on Travis:
    
    ```
    /home/fbthrift/thrift/lib/cpp2/async/RocketClientChannel.cpp:70:67:   required from here
    /home/install/include/fmt/core.h:492:3: error: static assertion failed: don't know how to format the type, include fmt/ostream.h if it provides an operator<< that should be used
    ```
    which is caused by trying to format an enum class without a formatter - only supported as of 6.0.
    
    Reviewed By: stevegury, avalonalex
    
    Differential Revision: D21860076
    
    fbshipit-source-id: 1857ab65822956b005980b8dfff7a967508f507c

--- build/fbcode_builder/specs/fmt.py
+++ build/fbcode_builder/specs/fmt.py
@@ -7,7 +7,7 @@ from __future__ import unicode_literals
 
 
 def fbcode_builder_spec(builder):
-    builder.add_option('fmtlib/fmt:git_hash', '5.3.0')
+    builder.add_option('fmtlib/fmt:git_hash', '6.2.1')
     return {
         'steps': [
             builder.github_project_workdir('fmtlib/fmt', 'build'),
