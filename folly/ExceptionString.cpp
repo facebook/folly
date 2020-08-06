ec55993dded00cfe6f6083db0a6c58ce505da553
Yedidya Feldblum <yfeldblum@fb.com>
2020-08-06 10:01:54 -0700

    Let exceptionStr peek into unknown exception types
    Summary: [Folly] Let `exceptionStr` peek into unknown exception types when building with libstdc++ via `std::exception_ptr::__cxa_exception_type()`.
    
    Reviewed By: ot, luciang
    
    Differential Revision: D22521812
    
    fbshipit-source-id: b75811ed4159df81e651c5df1386c406f6ac6d60

--- folly/ExceptionString.cpp
+++ folly/ExceptionString.cpp
@@ -18,6 +18,9 @@
 
 #include <utility>
 
+#include <folly/CPortability.h>
+#include <folly/CppAttributes.h>
+#include <folly/functional/Invoke.h>
 #include <folly/lang/Exception.h>
 
 namespace folly {
@@ -37,17 +40,44 @@ fbstring exceptionStr(const std::exception& e) {
   return rv;
 }
 
+namespace {
+
+FOLLY_CREATE_MEMBER_INVOKER(invoke_cxa_exception_type_fn, __cxa_exception_type);
+
+struct fallback_cxa_exception_type_fn {
+  FOLLY_MAYBE_UNUSED FOLLY_ERASE_HACK_GCC std::type_info const* operator()(
+      std::exception_ptr const&) const noexcept {
+    return nullptr;
+  }
+};
+
+using invoke_or_fallback_cxa_exception_type_fn = std::conditional_t<
+    is_invocable_r_v<
+        std::type_info const*,
+        invoke_cxa_exception_type_fn,
+        std::exception_ptr const&>,
+    invoke_cxa_exception_type_fn,
+    fallback_cxa_exception_type_fn>;
+
+FOLLY_INLINE_VARIABLE constexpr invoke_or_fallback_cxa_exception_type_fn
+    invoke_or_fallback_cxa_exception_type;
+
+} // namespace
+
 fbstring exceptionStr(std::exception_ptr ep) {
   if (!kHasExceptions) {
     return "Exception (catch unavailable)";
   }
+  auto type = invoke_or_fallback_cxa_exception_type(ep);
   return catch_exception(
       [&]() -> fbstring {
         return catch_exception<std::exception const&>(
             [&]() -> fbstring { std::rethrow_exception(std::move(ep)); },
             [](auto&& e) { return exceptionStr(e); });
       },
-      []() -> fbstring { return "<unknown exception>"; });
+      [&]() -> fbstring {
+        return type ? demangle(*type) : "<unknown exception>";
+      });
 }
 
 } // namespace folly
