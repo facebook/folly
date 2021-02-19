36eb535c56a68a5836e10d8b8510c069861d5475
Yedidya Feldblum <yfeldblum@fb.com>
2021-02-18 22:21:59 -0800

    in uncaught_exceptions, if windows, assume c++17
    Summary: This permits removal of the `_getptd()`-based implementation of `folly::uncaught_exceptions` sometimes used on windows, in favor of forwarding to `std::uncaught_exceptions`.
    
    Reviewed By: akrieger
    
    Differential Revision: D26509011
    
    fbshipit-source-id: b8fd14df91fcd849420d3c52328cb080b528245b

--- folly/lang/UncaughtExceptions.h
+++ folly/lang/UncaughtExceptions.h
@@ -53,12 +53,6 @@ inline int uncaught_exceptions() noexcept {
   return *(reinterpret_cast<unsigned int*>(
       static_cast<char*>(static_cast<void*>(__cxxabiv1::__cxa_get_globals())) +
       sizeof(void*)));
-#elif defined(FOLLY_EXCEPTION_COUNT_USE_GETPTD)
-  // _getptd() returns a _tiddata* (defined in mtdll.h).
-  // The offset below returns _tiddata::_ProcessingThrow.
-  return *(reinterpret_cast<int*>(
-      static_cast<char*>(static_cast<void*>(_getptd())) + sizeof(void*) * 28 +
-      0x4 * 8));
 #elif defined(FOLLY_EXCEPTION_COUNT_USE_STD)
   return std::uncaught_exceptions();
 #endif
