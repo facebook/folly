696d65f700a5aaa92240a8c90383bda3a00e7371
Ruslan Sayfutdinov <sayfutdinov@fb.com>
2021-03-05 13:18:27 -0800

    Define S_IRXO, S_ISREG in SysStat.h
    Reviewed By: yfeldblum
    
    Differential Revision: D26848785
    
    fbshipit-source-id: 441aeac55f51ee8ec5e2316288f2c1f778a60dda

--- folly/portability/SysStat.h
+++ folly/portability/SysStat.h
@@ -34,8 +34,10 @@
 #define S_IROTH S_IRUSR
 #define S_IRWXU (S_IRUSR | S_IWUSR | S_IXUSR)
 #define S_IRWXG (S_IRGRP | S_IWGRP | S_IXGRP)
+#define S_IRWXO (S_IROTH | S_IWOTH | S_IXOTH)
 
 #define S_ISDIR(mode) (((mode) & (_S_IFDIR)) == (_S_IFDIR) ? 1 : 0)
+#define S_ISREG(mode) (((mode) & (_S_IFREG)) == (_S_IFREG) ? 1 : 0)
 
 // This isn't defined anywhere, so give a sane value.
 #define MAXSYMLINKS 255
