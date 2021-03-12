9b6c06fa9d19fcf531b07d628d155c301c919138
Christian Watson <christianwatson@fb.com>
2021-03-11 22:45:22 -0800

    Revert D26612326: A new NotificationQueue without read limit for server responses
    Differential Revision:
    D26612326 (https://github.com/facebook/folly/commit/28799ed1d609bb1aca82464be8ad9dbf4e9f1b39)
    
    Original commit changeset: d56c892c87ba
    
    fbshipit-source-id: 77f8a7c58bfced197558ed29912ace457a089baa

--- folly/io/async/EventBase.cpp
+++ folly/io/async/EventBase.cpp
@@ -151,6 +151,14 @@ class ExecutionObserverScopeGuard {
 
 namespace folly {
 
+class EventBase::FuncRunner {
+ public:
+  void operator()(Func&& func) noexcept {
+    func();
+    func = nullptr;
+  }
+};
+
 /*
  * EventBase methods
  */
