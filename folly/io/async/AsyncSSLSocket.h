59dcfc1f24aa3b6d80975ba33cf13e66a3c8ae88
Tristan Rice <tristanr@fb.com>
2021-03-18 13:54:29 -0700

    Back out "Remove setZeroCopy() override"
    Summary:
    Original commit changeset: ec402ad2badb
    
    reverting D27044720 (https://github.com/facebook/folly/commit/bda140dc1f4be1846a19e708b7c2a0aa6215c730) to S225748
    
    This check was added in D22272411 (https://github.com/facebook/folly/commit/0ff36eaf684ee7c1eeac0e4592e47c92f2440323) as a fix for S204433. This is very likely the cause for S225748 since the behavior is very similar to the original sev
    
    Reviewed By: clrfb
    
    Differential Revision: D27166845
    
    fbshipit-source-id: c0a3f44690cd7c0df5d2a67f433419b961922dec

--- folly/io/async/AsyncSSLSocket.h
+++ folly/io/async/AsyncSSLSocket.h
@@ -832,6 +832,9 @@ class AsyncSSLSocket : public AsyncSocket {
     asyncOperationFinishCallback_ = std::move(cb);
   }
 
+  // zero copy is not supported by openssl.
+  bool setZeroCopy(bool /*enable*/) override { return false; }
+
  private:
   /**
    * Handle the return from invoking SSL_accept
