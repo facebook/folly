58a889d7beffef8bbaf011d04af988c93c029e35
Yedidya Feldblum <yfeldblum@fb.com>
2021-04-22 20:15:24 -0700

    NativeSemaphore
    Summary:
    A semaphore type which wraps the platform native semaphore type.
    
    On Windows, this is the Windows semaphore type. On Apple, this is the semaphore type from libdispatch. Otherwise, this is the POSIX semaphore type, which is deprecated on Apple and unavailable on Windows.
    
    Reviewed By: Orvid
    
    Differential Revision: D27951114
    
    fbshipit-source-id: e6a870f656eecd3ba41c2efed038b916c124d0e8

---
+++ folly/synchronization/test/NativeSemaphoreTest.cpp
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <folly/synchronization/NativeSemaphore.h>
+
+#include <folly/portability/GTest.h>
+
+using folly::NativeSemaphore;
+
+class NativeSemaphoreTest : public testing::Test {};
+
+TEST_F(NativeSemaphoreTest, empty) {
+  NativeSemaphore sem;
+  EXPECT_FALSE(sem.try_wait());
+  EXPECT_FALSE(sem.try_wait());
+
+  sem.post();
+  EXPECT_TRUE(sem.try_wait());
+  EXPECT_FALSE(sem.try_wait());
+
+  sem.post();
+  sem.wait();
+  EXPECT_FALSE(sem.try_wait());
+}
