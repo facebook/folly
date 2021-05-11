96b303638fc0b10bcaa029703990b2954f9ee0ff
Chris Keeline <ckeeline@fb.com>
2021-05-10 17:38:25 -0700

    Fix dev-tsan error
    Summary:
    TSAN catches this as a data race, though it's fairly benign. In the
    assert we read non-atomic `state` without holding the lock. As a fix, acquire
    the lock in the assert. The assert is skipped anyway in opt builds.
    
    Reviewed By: yfeldblum
    
    Differential Revision: D28302491
    
    fbshipit-source-id: b92ca9f3d321d34301d96e59fab363d999bddb30

--- folly/fibers/TimedMutex-inl.h
+++ folly/fibers/TimedMutex-inl.h
@@ -173,7 +173,10 @@ void TimedRWMutexImpl<ReaderPriority, BatonType>::lock_shared() {
     read_waiters_.push_back(waiter);
     ulock.unlock();
     waiter.baton.wait();
-    assert(state_ == State::READ_LOCKED);
+    if (folly::kIsDebug) {
+      std::unique_lock<folly::SpinLock> assertLock{lock_};
+      assert(state_ == State::READ_LOCKED);
+    }
     return;
   }
   assert(
