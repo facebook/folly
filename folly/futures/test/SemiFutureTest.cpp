6fd2b2c4dedfc0f74ee201bf6ab987a7d19d063a
JTJL <ch.papaw@gmail.com>
2021-06-14 20:43:04 +0800

    remove semicolons at the end of macros

--- folly/futures/test/SemiFutureTest.cpp
+++ folly/futures/test/SemiFutureTest.cpp
@@ -727,13 +727,13 @@ TEST(SemiFuture, onError) {
   do {                    \
     EXPECT_TRUE(theFlag); \
     theFlag = false;      \
-  } while (0);
+  } while (0)
 
 #define EXPECT_NO_FLAG()   \
   do {                     \
     EXPECT_FALSE(theFlag); \
     theFlag = false;       \
-  } while (0);
+  } while (0)
 
   // By reference
   {
