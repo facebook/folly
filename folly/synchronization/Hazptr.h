273a2cc81956b879b2fbf00e58f0703c621e17c4
Kian Ostad <c@kianostad.com>
2021-06-29 14:38:54 +0200

    Fix typo in Hazptr

--- folly/synchronization/Hazptr.h
+++ folly/synchronization/Hazptr.h
@@ -59,7 +59,7 @@
 ///   protected by hazard pointers.
 /// - The essential components of the hazptr API are:
 ///   o hazptr_holder: Class that owns and manages a hazard pointer.
-///   o protect: Mmember function of hazptr_holder. Protects
+///   o protect: Member function of hazptr_holder. Protects
 ///     an object pointed to by an atomic source (if not null).
 ///       T* protect(const atomic<T*>& src);
 ///   o hazptr_obj_base<T>: Base class for protected objects.
