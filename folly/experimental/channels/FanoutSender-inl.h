7f69cb31243e4a328b2927a53c798a4bf2253df2
Andrew Smith <ands@fb.com>
2021-08-06 11:06:32 -0700

    Add FanoutSender abstraction to channels framework
    Summary:
    The existing FanoutChannel construct takes in an input receiver, allows the creation of output receivers, and broadcasts all received values to the output receivers. It is good for the scenario where you already have an input receiver that you want to forward to multiple output receivers.
    
    However, for the scenario for which there is no existing input receiver, it is a bit heavy weight. To use it in this scenario, you have to create a separate channel, pass its receiver as the input receiver to the FanoutChannel, and write values to the sender.
    
    In addition to having to create a separate input channel that is otherwise unnecessary, it is not optimized for the case where there is a single output receiver. In many cases, there is typically only one output receiver. It would be ideal to have a construct that uses the same amount of memory as a simple channel, in the case where there is only one output receiver.
    
    FanoutSender solves both of these problems. FanoutSender does not require an input channel, and instead allows one to directly write values to all output receivers. In addition, in the case where there is only one output receiver, it uses exactly the same amount of memory as a simple channel.
    
    Reviewed By: aary
    
    Differential Revision: D29896797
    
    fbshipit-source-id: 18178f42364d53e84fb07424ed3e8cc0497e21e1

---
+++ folly/experimental/channels/FanoutSender-inl.h
@@ -0,0 +1,195 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace folly {
+namespace channels {
+
+template <typename ValueType>
+FanoutSender<ValueType>::FanoutSender()
+    : senders_(static_cast<detail::ChannelBridge<ValueType>*>(nullptr)) {}
+
+template <typename ValueType>
+FanoutSender<ValueType>::FanoutSender(FanoutSender&& other) noexcept
+    : senders_(std::move(other.senders_)) {}
+
+template <typename ValueType>
+FanoutSender<ValueType>& FanoutSender<ValueType>::operator=(
+    FanoutSender&& other) noexcept {
+  if (&other == this) {
+    return *this;
+  }
+  std::move(*this).close();
+  senders_ = std::move(senders_);
+  return *this;
+}
+
+template <typename ValueType>
+FanoutSender<ValueType>::~FanoutSender() {
+  std::move(*this).close();
+}
+
+template <typename ValueType>
+Receiver<ValueType> FanoutSender<ValueType>::subscribe(
+    std::vector<ValueType> initialValues) {
+  clearSendersWithClosedReceivers();
+  auto [newReceiver, newSender] = Channel<ValueType>::create();
+  for (auto& initialValue : initialValues) {
+    newSender.write(std::move(initialValue));
+  }
+  if (!anyReceivers()) {
+    // There are currently no output receivers. Store the new output receiver.
+    senders_.set(detail::senderGetBridge(newSender).release());
+  } else if (!hasSenderSet()) {
+    // There is currently exactly one output receiver. Convert to a sender set.
+    auto senderSet = std::make_unique<
+        folly::F14FastSet<detail::ChannelBridgePtr<ValueType>>>();
+    senderSet->insert(detail::ChannelBridgePtr<ValueType>(getSingleSender()));
+    senderSet->insert(std::move(detail::senderGetBridge(newSender)));
+    senders_.set(senderSet.release());
+  } else {
+    // There are currently more than one output receivers. Add the new receiver
+    // to the existing sender set.
+    auto* senderSet = getSenderSet();
+    senderSet->insert(std::move(detail::senderGetBridge(newSender)));
+  }
+  return std::move(newReceiver);
+}
+
+template <typename ValueType>
+bool FanoutSender<ValueType>::anyReceivers() {
+  clearSendersWithClosedReceivers();
+  return hasSenderSet() || getSingleSender() != nullptr;
+}
+
+template <typename ValueType>
+template <typename U>
+void FanoutSender<ValueType>::write(U&& element) {
+  clearSendersWithClosedReceivers();
+  if (!anyReceivers()) {
+    // There are currently no output receivers to write to.
+    return;
+  } else if (!hasSenderSet()) {
+    // There is exactly one output receiver. Write the value to that receiver.
+    getSingleSender()->senderPush(std::forward<U>(element));
+  } else {
+    // There are more than one output receivers. Write the value to all
+    // receivers.
+    for (auto& senderBridge : *getSenderSet()) {
+      senderBridge->senderPush(element);
+    }
+  }
+}
+
+template <typename ValueType>
+void FanoutSender<ValueType>::close(folly::exception_wrapper ex) && {
+  clearSendersWithClosedReceivers();
+  if (!anyReceivers()) {
+    // There are no output receivers to close.
+    return;
+  } else if (!hasSenderSet()) {
+    // There is exactly one output receiver to close.
+    if (ex) {
+      getSingleSender()->senderClose(ex);
+    } else {
+      getSingleSender()->senderClose();
+    }
+    // Delete the output receiver.
+    (detail::ChannelBridgePtr<ValueType>(getSingleSender()));
+    senders_.set(static_cast<detail::ChannelBridge<ValueType>*>(nullptr));
+  } else {
+    // There are more than one output receivers to close.
+    auto senderSet =
+        std::unique_ptr<F14FastSet<detail::ChannelBridgePtr<ValueType>>>(
+            getSenderSet());
+    senders_.set(static_cast<detail::ChannelBridge<ValueType>*>(nullptr));
+    for (auto& senderBridge : *senderSet) {
+      if (ex) {
+        senderBridge->senderClose(ex);
+      } else {
+        senderBridge->senderClose();
+      }
+    }
+  }
+}
+
+template <typename ValueType>
+bool FanoutSender<ValueType>::hasSenderSet() {
+  return senders_.index() == 1;
+}
+
+template <typename ValueType>
+detail::ChannelBridge<ValueType>* FanoutSender<ValueType>::getSingleSender() {
+  return senders_.get(folly::tag_t<detail::ChannelBridge<ValueType>>{});
+}
+
+template <typename ValueType>
+F14FastSet<detail::ChannelBridgePtr<ValueType>>*
+FanoutSender<ValueType>::getSenderSet() {
+  return senders_.get(
+      folly::tag_t<folly::F14FastSet<detail::ChannelBridgePtr<ValueType>>>{});
+}
+
+template <typename ValueType>
+void FanoutSender<ValueType>::clearSendersWithClosedReceivers() {
+  if (hasSenderSet()) {
+    // There are currently more than one output receivers. Check all of them to
+    // see if any have been cancelled.
+    auto* senderSet = getSenderSet();
+    for (auto it = senderSet->begin(); it != senderSet->end();) {
+      auto values = it->get()->senderGetValues();
+      if (!values.empty()) {
+        it->get()->senderClose();
+        it = senderSet->erase(it);
+      } else {
+        ++it;
+      }
+    }
+    if (senderSet->empty()) {
+      // After erasing all cancelled output receivers, there are no remaining
+      // receivers.
+      (std::unique_ptr<F14FastSet<detail::ChannelBridgePtr<ValueType>>>(
+          senderSet));
+      senders_.set(static_cast<detail::ChannelBridge<ValueType>*>(nullptr));
+      senderSet = nullptr;
+    } else if (senderSet->size() == 1) {
+      // After erasing all cancelled output receivers, there is exactly one
+      // remaining receiver. Move it out of the set, and store it as a single
+      // receiver.
+      auto senderSetUniqPtr =
+          std::unique_ptr<F14FastSet<detail::ChannelBridgePtr<ValueType>>>(
+              senderSet);
+      senderSetUniqPtr->eraseInto(senderSet->begin(), [&](auto&& senderBridge) {
+        senders_.set(senderBridge.release());
+      });
+      senderSet = nullptr;
+    }
+  } else {
+    auto* bridge = getSingleSender();
+    if (bridge != nullptr) {
+      // There is currently exactly one output receiver. Check to see if it has
+      // been cancelled.
+      auto values = bridge->senderGetValues();
+      if (!values.empty()) {
+        bridge->senderClose();
+        senders_.set(static_cast<detail::ChannelBridge<ValueType>*>(nullptr));
+        // Delete the output receiver.
+        (detail::ChannelBridgePtr<ValueType>(bridge));
+      }
+    }
+  }
+}
+} // namespace channels
+} // namespace folly
---
+++ folly/experimental/channels/FanoutSender.h
@@ -0,0 +1,94 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <folly/container/F14Set.h>
+#include <folly/experimental/channels/Channel.h>
+#include <folly/experimental/channels/detail/PointerVariant.h>
+
+namespace folly {
+namespace channels {
+
+/**
+ * A FanoutSender allows fanning out updates to multiple output receivers.
+ * Values can be written as with a normal Sender. When there is only one output
+ * receiver, the memory used by a FanoutSender (and the corresponding output
+ * receiver) is the same as the memory used by a normal channel.
+ *
+ * When a new output receiver is added, an optional vector of initial values
+ * can be provided. These initial values will only be sent to the new receiver.
+ *
+ * Memory used by closed receivers is reclaimed lazily (when iterating over
+ * receivers).
+ *
+ * Example:
+ *
+ *  FanoutSender<int> fanoutSender;
+ *  auto receiver1 = fanoutSender.getNewReceiver();
+ *  auto receiver2 = fanoutSender.getNewReceiver();
+ *  auto receiver3 = fanoutSender.getNewReceiver({1, 2, 3});
+ *  std::move(fanoutSender).close();
+ */
+template <typename ValueType>
+class FanoutSender {
+ public:
+  FanoutSender();
+  FanoutSender(FanoutSender&& other) noexcept;
+  FanoutSender& operator=(FanoutSender&& other) noexcept;
+  ~FanoutSender();
+
+  /**
+   * Returns a new output receiver that will receive all values written to the
+   * FanoutSender. If the initialValues parameter is provided, the given values
+   * will (only) go to the new output receiver.
+   */
+  Receiver<ValueType> subscribe(std::vector<ValueType> initialValues = {});
+
+  /**
+   * Returns whether this fanout sender has any active output receivers.
+   */
+  bool anyReceivers();
+
+  /**
+   * Sends the given value to all corresponding receivers.
+   */
+  template <typename U = ValueType>
+  void write(U&& element);
+
+  /**
+   * Closes the fanout sender.
+   */
+  void close(folly::exception_wrapper ex = folly::exception_wrapper()) &&;
+
+ private:
+  bool hasSenderSet();
+
+  detail::ChannelBridge<ValueType>* getSingleSender();
+
+  folly::F14FastSet<detail::ChannelBridgePtr<ValueType>>* getSenderSet();
+
+  void clearSendersWithClosedReceivers();
+
+  detail::PointerVariant<
+      detail::ChannelBridge<ValueType>,
+      folly::F14FastSet<detail::ChannelBridgePtr<ValueType>>>
+      senders_;
+};
+} // namespace channels
+} // namespace folly
+
+#include <folly/experimental/channels/FanoutSender-inl.h>
