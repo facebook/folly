fb5c25d96c76360b3e20c0369225ca0ec122aef0
Jolene Tan <lene@fb.com>
2021-09-14 15:14:25 -0700

    Deprecate folly::vformat
    Reviewed By: vitaut
    
    Differential Revision: D30676171
    
    fbshipit-source-id: f820b1c08750c4d844925a82d5fd710bc8a1ca5c

--- folly/Format.h
+++ folly/Format.h
@@ -286,7 +286,11 @@ inline std::string sformat(StringPiece fmt, Args&&... args) {
  * but the latter is cleaner.
  */
 template <class Container>
-Formatter<true, Container> vformat(StringPiece fmt, Container&& container) {
+[[deprecated(
+    "Use fmt::format instead of folly::vformat for better performance, build "
+    "times and compatibility with std::format")]] //
+Formatter<true, Container>
+vformat(StringPiece fmt, Container&& container) {
   return Formatter<true, Container>(fmt, std::forward<Container>(container));
 }
 
