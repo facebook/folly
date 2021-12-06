a4f059fbe04aa5b7e8bba58e16747ff9a67dfd63
Yedidya Feldblum <yfeldblum@fb.com>
2021-12-06 15:24:18 -0800

    let AsyncBase build even without eventfd available
    Differential Revision: D32633369
    
    fbshipit-source-id: a4a1df211647a49e0f9e69dac4902f4ae9a5ee27

--- folly/experimental/io/AsyncBase.cpp
+++ folly/experimental/io/AsyncBase.cpp
@@ -16,7 +16,6 @@
 
 #include <folly/experimental/io/AsyncBase.h>
 
-#include <sys/eventfd.h>
 #include <cerrno>
 #include <ostream>
 #include <stdexcept>
@@ -30,6 +29,10 @@
 #include <folly/Likely.h>
 #include <folly/String.h>
 #include <folly/portability/Unistd.h>
+
+#if __has_include(<sys/eventfd.h>)
+#include <sys/eventfd.h>
+#endif
 
 namespace folly {
 
@@ -93,8 +96,12 @@ AsyncBase::AsyncBase(size_t capacity, PollMode pollMode) : capacity_(capacity) {
   CHECK_GT(capacity_, 0);
   completed_.reserve(capacity_);
   if (pollMode == POLLABLE) {
+#if __has_include(<sys/eventfd.h>)
     pollFd_ = eventfd(0, EFD_NONBLOCK);
     checkUnixError(pollFd_, "AsyncBase: eventfd creation failed");
+#else
+    // fallthrough to not-pollable, observed as: pollFd() == -1
+#endif
   }
 }
 
