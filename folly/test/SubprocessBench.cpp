4e0cbf3c0738a6aa45fde53c24e146b9657264c9
Shaun Ruffell <sruffell@fb.com>
2021-12-16 06:28:24 -0800

    Add subprocess_benchmark
    Summary:
    Create a benchmark to measure subprocess creation time.
    
    This can be used as a baseline to measure future changes to process creation time. Specifically I am interested in optimizing the clean-up of open file descriptors in spawned process.
    
    Reviewed By: yfeldblum
    
    Differential Revision: D33128497
    
    fbshipit-source-id: fa9b69877e6ee77a4a0e3e612e875c88d6bf4e88

---
+++ folly/test/SubprocessBench.cpp
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <vector>
+
+#include <folly/Benchmark.h>
+#include <folly/File.h>
+#include <folly/Subprocess.h>
+
+using namespace folly;
+
+BENCHMARK(spawn_with_close_fds, iters) {
+  auto options = Subprocess::Options().closeOtherFds();
+  std::vector<std::string> cmd{"/bin/true"};
+  for (size_t i = 0; i < iters; ++i) {
+    Subprocess proc{cmd, options};
+    proc.wait();
+  }
+}
+
+BENCHMARK(spawn_without_close_fds, iters) {
+  std::vector<std::string> cmd{"/bin/true"};
+  for (size_t i = 0; i < iters; ++i) {
+    Subprocess proc{cmd};
+    proc.wait();
+  }
+}
+
+int main(int argc, char** argv) {
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+
+  // Create 512 descriptors
+  int rootfd = ::open("/", O_RDONLY);
+  assert(rootfd != -1);
+  std::vector<folly::File> descriptors;
+  const bool kTakeOwnership = true;
+  for (int i = 0; i < 512; ++i) {
+    descriptors.emplace_back(::dup(rootfd), kTakeOwnership);
+  }
+
+  folly::runBenchmarks();
+}
