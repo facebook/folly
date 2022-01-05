0ec02d4830c9d67987d7c71e52f8283217071c35
Ilya Albrecht <ialbrecht@fb.com>
2022-01-05 12:42:07 -0800

    asm memset implementation
    Summary: asm memset implementation based on the https://github.com/nadavrot/memset_benchmark implementation by Nadav Rotem
    
    Reviewed By: luciang, magedm
    
    Differential Revision: D32888087
    
    fbshipit-source-id: 3678950ed5178d2a46ba833502d1a6aef4f2c731

---
+++ folly/test/MemsetBenchmark.cpp
@@ -0,0 +1,104 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <folly/FollyMemset.h>
+
+#include <stdint.h>
+#include <stdlib.h>
+#include <deque>
+#include <string>
+#include <fmt/core.h>
+#include <folly/Benchmark.h>
+#include <folly/Preprocessor.h>
+#include <folly/portability/GFlags.h>
+
+DEFINE_uint32(min_size, 1, "Minimum size to benchmark");
+DEFINE_uint32(max_size, 32768, "Maximum size to benchmark");
+DEFINE_bool(linear, false, "Test all sizes [min_size, max_size]");
+DEFINE_uint32(step, 1, "Test sizes step");
+DEFINE_uint32(page_offset, 0, "Buffer offset from page aligned size");
+
+uint8_t* temp_buf;
+
+size_t getPow2(size_t v) {
+  assert(v != 0);
+  return 1UL << (sizeof(size_t) * 8 - __builtin_clzl(v) - 1);
+}
+
+template <void* memset_impl(void*, int, size_t)>
+void bmMemset(void* buf, size_t length, size_t iters) {
+  __asm__ volatile(".align 64\n");
+#pragma unroll(1)
+  for (size_t i = 0; i < iters; ++i) {
+    memset_impl(buf, 0xFF, length);
+  }
+}
+
+template <void* memset_impl(void*, int, size_t)>
+void addMemsetBenchmark(const std::string& name) {
+  static std::deque<std::string> names;
+
+  auto addBech = [&](size_t size) {
+    names.emplace_back(fmt::format("{}: size={}", name, size));
+    folly::addBenchmark(__FILE__, names.back().c_str(), [=](unsigned iters) {
+      bmMemset<memset_impl>(temp_buf + FLAGS_page_offset, size, iters);
+      return iters;
+    });
+  };
+
+  if (FLAGS_linear) {
+    for (size_t size = FLAGS_min_size; size <= FLAGS_max_size;
+         size += FLAGS_step) {
+      addBech(size);
+    }
+  } else {
+    for (size_t size = getPow2(FLAGS_min_size); size <= getPow2(FLAGS_max_size);
+         size <<= 1) {
+      addBech(size);
+    }
+  }
+
+  /* Draw line. */
+  folly::addBenchmark(__FILE__, "-", []() { return 0; });
+}
+
+int main(int argc, char** argv) {
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+  google::InitGoogleLogging(argv[0]);
+
+  assert(FLAGS_min_size <= FLAGS_max_size);
+  assert(FLAGS_page_offset < 4096);
+  assert(FLAGS_step > 0);
+
+  size_t totalBufSize = (FLAGS_max_size + FLAGS_page_offset + 4095) & ~4095;
+  temp_buf = (uint8_t*)aligned_alloc(4096, totalBufSize);
+  // Make sure all pages are allocated
+  for (size_t i = 0; i < totalBufSize; i++) {
+    temp_buf[i] = 0;
+  }
+
+#define BENCHMARK_MEMSET(MEMSET) \
+  addMemsetBenchmark<MEMSET>(FOLLY_PP_STRINGIZE(MEMSET));
+
+  BENCHMARK_MEMSET(memset);
+  BENCHMARK_MEMSET(folly::__folly_memset);
+
+  folly::runBenchmarks();
+
+  free(temp_buf);
+
+  return 0;
+}
---
+++ folly/test/MemsetTest.cpp
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <stdlib.h>
+#include <cstddef>
+
+#include <folly/FollyMemset.h>
+
+#include <folly/portability/GTest.h>
+
+constexpr size_t kPageSize = 4096;
+constexpr size_t kMaxSize = 2 * kPageSize;
+constexpr uint8_t kBufEnd = 0xDB;
+
+// memset implementation test with 0xFF pattern
+// buf must have an extra byte to be filled with magic constant
+void testMemsetImpl(uint8_t* buf, size_t maxLen) {
+  for (size_t len = 0; len < maxLen; len++) {
+    for (size_t i = 0; i < maxLen; i++) {
+      buf[i] = 0x0;
+    }
+    buf[len] = kBufEnd;
+    auto* p = folly::__folly_memset(buf, 0xFF, len);
+    EXPECT_EQ(buf, reinterpret_cast<uint8_t*>(p));
+    bool isEq = true;
+    for (size_t i = 0; i < len; i++) {
+      isEq &= buf[i] == 0xFF;
+    }
+    EXPECT_TRUE(isEq);
+    EXPECT_EQ(buf[len], kBufEnd);
+  }
+}
+
+TEST(MemsetAsmTest, alignedBuffer) {
+  uint8_t* buf = reinterpret_cast<uint8_t*>(
+      aligned_alloc(kPageSize, kMaxSize + 2 * kPageSize));
+  // Get buffer aligned power of 2 from 16 all the way up to a page size
+  for (size_t alignment = 16; alignment <= kPageSize; alignment <<= 1) {
+    testMemsetImpl(buf + (alignment % kPageSize), kMaxSize);
+  }
+  free(buf);
+}
+
+TEST(MemsetAsmTest, unalignedBuffer) {
+  uint8_t* buf = reinterpret_cast<uint8_t*>(
+      aligned_alloc(kPageSize, kMaxSize + 2 * kPageSize));
+  for (size_t alignment = 1; alignment <= 192; alignment++) {
+    testMemsetImpl(buf + alignment, kMaxSize);
+  }
+  free(buf);
+}
