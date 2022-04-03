1ec393f392d2759974a5ddd1633557fc8532c933
msaf1980 <msaf1980@gmail.com>
2022-04-03 07:34:41 +0500

    fix gcc11 build: passing ‘const fmt::v7::formatter<folly::Range<const char*> >’ as ‘this’ argument discards qualifiers [-fpermissive]

--- folly/Range.h
+++ folly/Range.h
@@ -1554,7 +1554,7 @@ struct formatter<folly::StringPiece> : private formatter<string_view> {
   using formatter<string_view>::parse;
 
   template <typename Context>
-  typename Context::iterator format(folly::StringPiece s, Context& ctx) const {
+  typename Context::iterator format(folly::StringPiece s, Context& ctx) {
     return formatter<string_view>::format({s.data(), s.size()}, ctx);
   }
 };
