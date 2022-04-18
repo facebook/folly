9d2b3ae18f8492e237f63a04ff6a66d1b79e1e91
Orvid King <orvid@fb.com>
2022-04-18 15:51:20 -0700

    Expand lang/CheckedMath.h
    Summary: This uses builtins to check against the overflow flag on supported compilers, and also implements checked_mul for GCC, clang and MSVC, with a fallback for unsupported architectures. This also adds a couple of utility methods, including a checked_add for pointers that just does the right thing.
    
    Reviewed By: Gownta
    
    Differential Revision: D32483197
    
    fbshipit-source-id: b5ea6b195c32fd91dc3ab6bd188e4d5aa49948ae

--- folly/lang/CheckedMath.h
+++ folly/lang/CheckedMath.h
@@ -17,15 +17,23 @@
 #pragma once
 
 #include <cassert>
+#include <cstddef>
+#include <cstdint>
 #include <limits>
 #include <type_traits>
 
+#ifdef _MSC_VER
+#include <intrin.h>
+#endif
+
+#include <folly/CPortability.h>
 #include <folly/Likely.h>
 
 namespace folly {
+namespace detail {
+
 template <typename T, typename = std::enable_if_t<std::is_unsigned<T>::value>>
-bool checked_add(T* result, T a, T b) {
-  assert(result != nullptr);
+bool generic_checked_add(T* result, T a, T b) {
   if (FOLLY_LIKELY(a < std::numeric_limits<T>::max() - b)) {
     *result = a + b;
     return true;
@@ -33,5 +41,181 @@ bool checked_add(T* result, T a, T b) {
     *result = {};
     return false;
   }
+}
+
+template <typename T, typename = std::enable_if_t<std::is_unsigned<T>::value>>
+bool generic_checked_small_mul(T* result, T a, T b) {
+  static_assert(sizeof(T) < sizeof(uint64_t));
+  uint64_t res = static_cast<uint64_t>(a) * static_cast<uint64_t>(b);
+  constexpr uint64_t overflowMask = ~((1ULL << (sizeof(T) * 8)) - 1);
+  if (FOLLY_UNLIKELY((res & overflowMask) != 0)) {
+    *result = {};
+    return false;
+  }
+  *result = static_cast<T>(res);
+  return true;
+}
+
+template <typename T, typename = std::enable_if_t<std::is_unsigned<T>::value>>
+std::enable_if_t<sizeof(T) < sizeof(uint64_t), bool> generic_checked_mul(
+    T* result, T a, T b) {
+  return generic_checked_small_mul(result, a, b);
+}
+
+template <typename T, typename = std::enable_if_t<std::is_unsigned<T>::value>>
+std::enable_if_t<sizeof(T) == sizeof(uint64_t), bool> generic_checked_mul(
+    T* result, T a, T b) {
+  constexpr uint64_t halfBits = 32;
+  constexpr uint64_t halfMask = (1ULL << halfBits) - 1ULL;
+  uint64_t lhs_high = a >> halfBits;
+  uint64_t lhs_low = a & halfMask;
+  uint64_t rhs_high = b >> halfBits;
+  uint64_t rhs_low = b & halfMask;
+
+  if (FOLLY_LIKELY(lhs_high == 0 && rhs_high == 0)) {
+    *result = lhs_low * rhs_low;
+    return true;
+  }
+
+  if (FOLLY_UNLIKELY(lhs_high != 0 && rhs_high != 0)) {
+    *result = {};
+    return false;
+  }
+
+  uint64_t mid_bits1 = lhs_low * rhs_high;
+  if (FOLLY_UNLIKELY(mid_bits1 >> halfBits != 0)) {
+    *result = {};
+    return false;
+  }
+
+  uint64_t mid_bits2 = lhs_high * rhs_low;
+  if (FOLLY_UNLIKELY(mid_bits2 >> halfBits != 0)) {
+    *result = {};
+    return false;
+  }
+
+  uint64_t mid_bits = mid_bits1 + mid_bits2;
+  if (FOLLY_UNLIKELY(mid_bits >> halfBits != 0)) {
+    *result = {};
+    return false;
+  }
+
+  uint64_t bot_bits = lhs_low * rhs_low;
+  if (FOLLY_UNLIKELY(
+          !generic_checked_add(result, bot_bits, mid_bits << halfBits))) {
+    *result = {};
+    return false;
+  }
+  return true;
+}
+} // namespace detail
+
+template <typename T, typename = std::enable_if_t<std::is_unsigned<T>::value>>
+bool checked_add(T* result, T a, T b) {
+#if FOLLY_HAS_BUILTIN(__builtin_add_overflow)
+  if (FOLLY_LIKELY(!__builtin_add_overflow(a, b, result))) {
+    return true;
+  }
+  *result = {};
+  return false;
+#else
+  return detail::generic_checked_add(result, a, b);
+#endif
+}
+
+template <typename T, typename = std::enable_if_t<std::is_unsigned<T>::value>>
+bool checked_add(T* result, T a, T b, T c) {
+  T tmp{};
+  if (FOLLY_UNLIKELY(!checked_add(&tmp, a, b))) {
+    *result = {};
+    return false;
+  }
+  if (FOLLY_UNLIKELY(!checked_add(&tmp, tmp, c))) {
+    *result = {};
+    return false;
+  }
+  *result = tmp;
+  return true;
+}
+
+template <typename T, typename = std::enable_if_t<std::is_unsigned<T>::value>>
+bool checked_add(T* result, T a, T b, T c, T d) {
+  T tmp{};
+  if (FOLLY_UNLIKELY(!checked_add(&tmp, a, b))) {
+    *result = {};
+    return false;
+  }
+  if (FOLLY_UNLIKELY(!checked_add(&tmp, tmp, c))) {
+    *result = {};
+    return false;
+  }
+  if (FOLLY_UNLIKELY(!checked_add(&tmp, tmp, d))) {
+    *result = {};
+    return false;
+  }
+  *result = tmp;
+  return true;
+}
+
+template <typename T, typename = std::enable_if_t<std::is_unsigned<T>::value>>
+bool checked_mul(T* result, T a, T b) {
+  assert(result != nullptr);
+#if FOLLY_HAS_BUILTIN(__builtin_mul_overflow)
+  if (FOLLY_LIKELY(!__builtin_mul_overflow(a, b, result))) {
+    return true;
+  }
+  *result = {};
+  return false;
+#elif _MSC_VER
+  static_assert(sizeof(T) <= sizeof(unsigned __int64));
+  if (sizeof(T) < sizeof(uint64_t)) {
+    return detail::generic_checked_small_mul(result, a, b);
+  } else {
+    unsigned __int64 high;
+    unsigned __int64 low = _umul128(a, b, &high);
+    // unsigned right shift is guaranteed to be 0
+    // if sizeof(T) == sizeof(unsigned __int64)
+    if (FOLLY_UNLIKELY(low >> (sizeof(T) * 8) != 0)) {
+      *result = {};
+      return false;
+    }
+    if (FOLLY_LIKELY(high == 0)) {
+      *result = low;
+      return true;
+    }
+    *result = {};
+    return false;
+  }
+#else
+  return detail::generic_checked_mul(result, a, b);
+#endif
+}
+
+template <typename T, typename = std::enable_if_t<std::is_unsigned<T>::value>>
+bool checked_muladd(T* result, T base, T mul, T add) {
+  T tmp{};
+  if (FOLLY_UNLIKELY(!checked_mul(&tmp, base, mul))) {
+    *result = {};
+    return false;
+  }
+  if (FOLLY_UNLIKELY(!checked_add(&tmp, tmp, add))) {
+    *result = {};
+    return false;
+  }
+  *result = tmp;
+  return true;
+}
+
+template <
+    typename T,
+    typename T2,
+    typename = std::enable_if_t<std::is_pointer<T>::value>,
+    typename = std::enable_if_t<std::is_unsigned<T2>::value>>
+bool checked_add(T* result, T a, T2 b) {
+  return checked_muladd(
+      reinterpret_cast<size_t*>(result),
+      size_t(b),
+      sizeof(std::remove_pointer_t<T>),
+      size_t(a));
 }
 } // namespace folly
--- folly/lang/test/CheckedMathTest.cpp
+++ folly/lang/test/CheckedMathTest.cpp
@@ -16,7 +16,9 @@
 
 #include <folly/lang/CheckedMath.h>
 
+#include <cstdint>
 #include <limits>
+#include <random>
 
 #include <folly/portability/GTest.h>
 
@@ -35,10 +37,258 @@ TEST(CheckedMath, checked_add_overflow) {
   EXPECT_EQ(a, {});
 }
 
-TEST(CheckedMath, checked_add_size_t_overflow) {
-  size_t a;
+TEST(CheckedMath, checked_add_uint64_t_overflow) {
+  uint64_t a;
 
-  EXPECT_FALSE(folly::checked_add<size_t>(
-      &a, std::numeric_limits<size_t>::max() - 7, 9));
+  EXPECT_FALSE(folly::checked_add<uint64_t>(
+      &a, std::numeric_limits<uint64_t>::max() - 7, 9));
   EXPECT_EQ(a, {});
+}
+
+TEST(CheckedMath, checked_add_3_no_overflow) {
+  uint64_t a;
+
+  EXPECT_TRUE(folly::checked_add<uint64_t>(&a, 5, 7, 9));
+  EXPECT_EQ(a, 21);
+}
+
+TEST(CheckedMath, checked_add_3_overflow) {
+  uint64_t a;
+
+  EXPECT_FALSE(folly::checked_add<uint64_t>(
+      &a, 5, std::numeric_limits<uint64_t>::max() - 7, 9));
+  EXPECT_EQ(a, {});
+}
+
+TEST(CheckedMath, checked_add_3_overflow2) {
+  uint64_t a;
+
+  EXPECT_FALSE(folly::checked_add<uint64_t>(
+      &a, 5, 7, std::numeric_limits<uint64_t>::max() - 7));
+  EXPECT_EQ(a, {});
+}
+
+TEST(CheckedMath, checked_add_4_no_overflow) {
+  uint64_t a;
+
+  EXPECT_TRUE(folly::checked_add<uint64_t>(&a, 5, 7, 9, 11));
+  EXPECT_EQ(a, 32);
+}
+
+TEST(CheckedMath, checked_add_4_overflow) {
+  uint64_t a;
+
+  EXPECT_FALSE(folly::checked_add<uint64_t>(
+      &a, 5, std::numeric_limits<uint64_t>::max() - 7, 9, 11));
+  EXPECT_EQ(a, {});
+}
+
+TEST(CheckedMath, checked_add_4_overflow2) {
+  uint64_t a;
+
+  EXPECT_FALSE(folly::checked_add<uint64_t>(
+      &a, 5, 7, std::numeric_limits<uint64_t>::max() - 7, 11));
+  EXPECT_EQ(a, {});
+}
+
+TEST(CheckedMath, checked_add_4_overflow3) {
+  uint64_t a;
+
+  EXPECT_FALSE(folly::checked_add<uint64_t>(
+      &a, 5, 7, 9, std::numeric_limits<uint64_t>::max() - 7));
+  EXPECT_EQ(a, {});
+}
+
+TEST(CheckedMath, generic_checked_add_no_overflow) {
+  unsigned int a;
+
+  EXPECT_TRUE(folly::detail::generic_checked_add(&a, 5u, 4u));
+  EXPECT_EQ(a, 9);
+}
+
+TEST(CheckedMath, generic_checked_add_overflow) {
+  unsigned int a;
+
+  EXPECT_FALSE(folly::detail::generic_checked_add(
+      &a, std::numeric_limits<unsigned int>::max(), 4u));
+  EXPECT_EQ(a, {});
+}
+
+TEST(CheckedMath, generic_checked_add_uint64_t_overflow) {
+  uint64_t a;
+
+  EXPECT_FALSE(folly::detail::generic_checked_add<uint64_t>(
+      &a, std::numeric_limits<uint64_t>::max() - 7, 9));
+  EXPECT_EQ(a, {});
+}
+
+TEST(CheckedMath, checked_mul_no_overflow) {
+  unsigned int a;
+
+  EXPECT_TRUE(folly::checked_mul(&a, 5u, 4u));
+  EXPECT_EQ(a, 20);
+}
+
+TEST(CheckedMath, checked_mul_overflow) {
+  unsigned int a;
+
+  EXPECT_FALSE(
+      folly::checked_mul(&a, std::numeric_limits<unsigned int>::max(), 4u));
+  EXPECT_EQ(a, {});
+}
+
+TEST(CheckedMath, checked_mul_uint64_t_overflow) {
+  uint64_t a;
+
+  EXPECT_FALSE(folly::checked_mul<uint64_t>(
+      &a, std::numeric_limits<uint64_t>::max() - 7, 9));
+  EXPECT_EQ(a, {});
+}
+
+TEST(CheckedMath, generic_checked_mul_no_overflow) {
+  unsigned int a;
+
+  EXPECT_TRUE(folly::detail::generic_checked_mul(&a, 5u, 4u));
+  EXPECT_EQ(a, 20);
+}
+
+TEST(CheckedMath, generic_checked_mul_overflow) {
+  unsigned int a;
+
+  EXPECT_FALSE(folly::detail::generic_checked_mul(
+      &a, std::numeric_limits<unsigned int>::max(), 4u));
+  EXPECT_EQ(a, {});
+}
+
+TEST(CheckedMath, generic_checked_mul_uint64_t_overflow) {
+  uint64_t a;
+
+  EXPECT_FALSE(folly::detail::generic_checked_mul<uint64_t>(
+      &a, std::numeric_limits<uint64_t>::max() - 7, 9));
+  EXPECT_EQ(a, {});
+}
+
+TEST(CheckedMath, generic_checked_mul_uint64_t_overflow_1) {
+  uint64_t a;
+
+  // lhs_high != 0 && rhs_high != 0
+  EXPECT_FALSE(folly::detail::generic_checked_mul<uint64_t>(
+      &a, 0x1'0000'0000, 0x1'0000'0000));
+  EXPECT_EQ(a, {});
+}
+
+TEST(CheckedMath, generic_checked_mul_uint64_t_overflow_2) {
+  uint64_t a;
+
+  // lhs_low * rhs_high overflows
+  EXPECT_FALSE(folly::detail::generic_checked_mul<uint64_t>(
+      &a, 0xFFFF'FFFF, 0xF'0000'0000));
+  EXPECT_EQ(a, {});
+}
+
+TEST(CheckedMath, generic_checked_mul_uint64_t_overflow_3) {
+  uint64_t a;
+
+  // lhs_high * rhs_low overflows
+  EXPECT_FALSE(folly::detail::generic_checked_mul<uint64_t>(
+      &a, 0xF'0000'0000, 0xFFFF'FFFF));
+  EXPECT_EQ(a, {});
+}
+
+TEST(CheckedMath, generic_checked_mul_uint64_t_overflow_4) {
+  uint64_t a;
+
+  // mid_bits1 + mid_bits2 overflows
+  EXPECT_FALSE(folly::detail::generic_checked_mul<uint64_t>(
+      &a, 0x2'7FFF'FFFF, 0x2'7FFF'FFFF));
+  EXPECT_EQ(a, {});
+}
+
+TEST(CheckedMath, generic_checked_mul_uint64_t_overflow_5) {
+  uint64_t a;
+
+  // (lhs_low * rhs_low) + (mid_bits << 32) overflows
+  EXPECT_FALSE(folly::detail::generic_checked_mul<uint64_t>(
+      &a, 0x2'0000'0203, 0x7FFF'FFFF));
+  EXPECT_EQ(a, {});
+}
+
+// __builtin_mul_overflow has the most straight-forward interface for doing
+// this check, and the generic code will be the same regardless of the compiler.
+#if FOLLY_HAS_BUILTIN(__builtin_mul_overflow)
+TEST(CheckedMath, generic_checked_mul_vs_builtin) {
+  std::mt19937_64 mt{std::random_device()()};
+
+  constexpr size_t IterationCount = 1'000'000;
+  size_t overflowCount = 0;
+  for (size_t i = 0; i < IterationCount; i++) {
+    // There's probably a distribution that produces values on the
+    // edge of likely overflow, but I don't know the name, so use
+    // the full range for now and just drop bits from a and b to
+    // get us down to only ~90% overflow, which is what we care the
+    // most about testing.
+    // 70 bits of random data across a & b nets 90% overflow
+    // 69 bits of data nets 86% overflow
+    // 68 bits of data nets 76% overflow
+    uint64_t a = mt() >> 16;
+    uint64_t b = mt() >> 43;
+    uint64_t genRes = 0;
+    uint64_t builtinRes = 0;
+    bool genOverflow =
+        folly::detail::generic_checked_mul<uint64_t>(&genRes, a, b);
+    bool builtinOverflow = !__builtin_mul_overflow(a, b, &builtinRes);
+    EXPECT_EQ(genOverflow, builtinOverflow);
+
+    if (genOverflow && builtinOverflow) {
+      // __builtin doesn't guarantee the value of builtinRes when an overflow
+      // happens, but generic_checked_* guarantees it's zero.
+      EXPECT_EQ(genRes, builtinRes);
+    } else {
+      overflowCount++;
+    }
+  }
+
+  EXPECT_NE(overflowCount, IterationCount);
+  EXPECT_NE(overflowCount, 0);
+}
+#endif
+
+TEST(CheckedMath, checked_muladd_no_overflow) {
+  unsigned int a;
+
+  EXPECT_TRUE(folly::checked_muladd(&a, 5u, 4u, 1u));
+  EXPECT_EQ(a, 21);
+}
+
+TEST(CheckedMath, checked_muladd_overflow) {
+  unsigned int a;
+
+  EXPECT_FALSE(folly::checked_muladd(
+      &a, 5u, std::numeric_limits<unsigned int>::max(), 1u));
+  EXPECT_EQ(a, {});
+}
+
+TEST(CheckedMath, checked_muladd_overflow2) {
+  unsigned int a;
+
+  EXPECT_FALSE(folly::checked_muladd(
+      &a, 5u, 4u, std::numeric_limits<unsigned int>::max()));
+  EXPECT_EQ(a, {});
+}
+
+TEST(CheckedMath, checked_ptr_add_no_overflow) {
+  unsigned int buf[4];
+  unsigned int* a;
+
+  EXPECT_TRUE(folly::checked_add(&a, &buf[0], 1u));
+  EXPECT_EQ(a, &buf[1]);
+}
+
+TEST(CheckedMath, checked_ptr_add_overflow) {
+  unsigned int buf[4];
+  unsigned int* a{nullptr};
+
+  EXPECT_FALSE(folly::checked_add(
+      &a, &buf[0], std::numeric_limits<uint64_t>::max() - 7));
+  EXPECT_EQ(a, nullptr);
 }
