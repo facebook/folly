1476b0c0bcfa83a7b79169f1bcca7eac93cc225d
Chad Austin <chadaustin@fb.com>
2022-05-13 12:08:30 -0700

    threadname: add support for SetThreadDescription
    Summary:
    Windows 10, starting with 1607, has a public API for setting thread
    names. Profilers can use this information even when the process does
    not have a debugger attached.
    
    Try calling SetThreadDescription before falling back to the MSVC
    debugger interrupt protocol.
    
    Reviewed By: yfeldblum
    
    Differential Revision: D36338834
    
    fbshipit-source-id: 799fcda711d6c6d2b46bd6fd24beb6981b705ccc

--- folly/system/ThreadName.cpp
+++ folly/system/ThreadName.cpp
@@ -19,9 +19,14 @@
 #include <type_traits>
 
 #include <folly/Portability.h>
+#include <folly/ScopeGuard.h>
 #include <folly/Traits.h>
 #include <folly/portability/PThread.h>
 #include <folly/portability/Windows.h>
+
+#ifdef _WIN32
+#include <strsafe.h> // @manual
+#endif
 
 // Android only, prctl is only used when pthread_setname_np
 // and pthread_getname_np are not avilable.
@@ -154,13 +159,70 @@ Optional<std::string> getCurrentThreadName() {
 #endif
 }
 
-bool setThreadName(std::thread::id tid, StringPiece name) {
+namespace {
 #ifdef _WIN32
-  static_assert(
-      sizeof(unsigned int) == sizeof(std::thread::id),
-      "This assumes std::thread::id is a thin wrapper around "
-      "the thread id as an unsigned int, but that doesn't appear to be true.");
 
+typedef HRESULT(__stdcall* SetThreadDescriptionFn)(HANDLE, PCWSTR);
+
+SetThreadDescriptionFn getSetThreadDescription() {
+  // GetModuleHandle does not increment the module's reference count,
+  // but kernelbase.dll won't be unloaded.
+  auto proc = GetProcAddress(
+      GetModuleHandleW(L"kernelbase.dll"), "SetThreadDescription");
+  return reinterpret_cast<SetThreadDescriptionFn>(proc);
+}
+
+bool setThreadNameWindowsViaDescription(DWORD id, StringPiece name) noexcept {
+  // This whole function is noexcept.
+
+  static const auto setThreadDescription = getSetThreadDescription();
+  if (!setThreadDescription) {
+    return false;
+  }
+
+  HANDLE thread = id == GetCurrentThreadId()
+      ? GetCurrentThread()
+      : OpenThread(THREAD_SET_LIMITED_INFORMATION, FALSE, id);
+  if (!thread) {
+    return false;
+  }
+
+  SCOPE_EXIT {
+    if (id != GetCurrentThreadId()) {
+      CloseHandle(thread);
+    }
+  };
+
+  // Limit thread name length so the UTF-16 output buffer can be fixed-length.
+  // Pthreads limits thread names to 15, but that's pretty tight in practice. 64
+  // should be plenty.
+  constexpr size_t kMaximumThreadNameLength = 64;
+  if (name.size() > kMaximumThreadNameLength) {
+    name = name.subpiece(0, kMaximumThreadNameLength);
+  }
+
+  // For valid UTF-8, code points in [0, 0xFFFF] fit in one UTF-16 code unit.
+  // Code points that require two UTF-16 code units are always encoded with four
+  // UTF-8 bytes. Therefore, the fixed output buffer can be the maximum name
+  // length in WCHARs, plus one for the terminating zero. That said, who knows
+  // what MultiByteToWideChar does with invalid UTF-8 or if it attempts to
+  // compose or decompose forms, so double-check the output anyway.
+  WCHAR wname[kMaximumThreadNameLength + 1];
+  int written = MultiByteToWideChar(
+      CP_UTF8, 0, name.data(), name.size(), wname, kMaximumThreadNameLength);
+  if (written <= 0 || static_cast<size_t>(written) >= std::size(wname)) {
+    // Conversion failed or somehow it didn't fit.
+    return false;
+  }
+  wname[written] = 0;
+
+  if (FAILED(setThreadDescription(thread, wname))) {
+    return false;
+  }
+
+  return true;
+}
+bool setThreadNameWindowsViaDebugger(DWORD id, StringPiece name) noexcept {
 // http://msdn.microsoft.com/en-us/library/xcb2z8hs.aspx
 #pragma pack(push, 8)
   struct THREADNAME_INFO {
@@ -177,26 +239,55 @@ bool setThreadName(std::thread::id tid, StringPiece name) {
 
   static constexpr DWORD kMSVCException = 0x406D1388;
 
-  // std::thread::id is a thin wrapper around an integral thread id,
-  // so just extract the ID.
-  unsigned int id;
+  char trimmed[kMaxThreadNameLength];
+  if (STRSAFE_E_INVALID_PARAMETER ==
+      StringCchCopyNA(trimmed, sizeof(trimmed), name.data(), name.size())) {
+    return false;
+  }
+  // Intentionally ignore STRSAFE_E_INSUFFICIENT_BUFFER: the buffer now contains
+  // a truncated, zero-terminated string, and that's desirable here.
+
+  TNIUnion tniUnion = {0x1000, trimmed, id, 0};
+
+  // SEH requires no use of C++ object destruction semantics in this stack
+  // frame.
+  __try {
+    RaiseException(kMSVCException, 0, 4, tniUnion.upArray);
+  } __except (
+      GetExceptionCode() == kMSVCException ? EXCEPTION_CONTINUE_EXECUTION
+                                           : EXCEPTION_EXECUTE_HANDLER) {
+    // Swallow the exception when a debugger isn't attached.
+  }
+  return true;
+}
+
+bool setThreadNameWindows(std::thread::id tid, StringPiece name) {
+  static_assert(
+      sizeof(DWORD) == sizeof(std::thread::id),
+      "This assumes std::thread::id is a thin wrapper around "
+      "the Win32 thread id, but that doesn't appear to be true.");
+
+  // std::thread::id is a thin wrapper around the Windows thread ID,
+  // so just extract it.
+  DWORD id;
   std::memcpy(&id, &tid, sizeof(id));
 
-  auto trimmedName = name.subpiece(0, kMaxThreadNameLength - 1).str();
-
-  TNIUnion tniUnion = {0x1000, trimmedName.data(), id, 0};
-  // This has to be in a separate stack frame from trimmedName, which requires
-  // C++ object destruction semantics.
-  return [&]() {
-    __try {
-      RaiseException(kMSVCException, 0, 4, tniUnion.upArray);
-    } __except (
-        GetExceptionCode() == kMSVCException ? EXCEPTION_CONTINUE_EXECUTION
-                                             : EXCEPTION_EXECUTE_HANDLER) {
-      // Swallow the exception when a debugger isn't attached.
-    }
+  // First, try the Windows 10 1607 SetThreadDescription call.
+  if (setThreadNameWindowsViaDescription(id, name)) {
     return true;
-  }();
+  }
+
+  // Otherwise, fall back to the older SEH approach, which is primarily
+  // useful in debuggers.
+  return setThreadNameWindowsViaDebugger(id, name);
+}
+
+#endif
+} // namespace
+
+bool setThreadName(std::thread::id tid, StringPiece name) {
+#ifdef _WIN32
+  return setThreadNameWindows(tid, name);
 #else
   return setThreadName(stdTidToPthreadId(tid), name);
 #endif
