2d9a3bc35fa7e9e1ded68e7bd7b3d8d743a6441a
Mohamed Albashir <malbashir@fb.com>
2022-06-07 13:54:15 -0700

    add FOLLY_CONSTEVAL macro
    Summary: using consteval when building using platform 10 or newer. and defaulting to constexpr when older
    
    Reviewed By: yfeldblum
    
    Differential Revision: D36948803
    
    fbshipit-source-id: 4dab633e7958cc7eaea18d437d2d5b10c9484408

--- folly/Portability.h
+++ folly/Portability.h
@@ -633,3 +633,10 @@ constexpr auto kCpplibVer = 0;
 #else
 #define FOLLY_HAS_STRING_VIEW 0
 #endif
+
+// C++20 consteval
+#if FOLLY_CPLUSPLUS >= 202002L
+#define FOLLY_CONSTEVAL consteval
+#else
+#define FOLLY_CONSTEVAL constexpr
+#endif
