72391b18f736914870ef44a85b0137be0370a101
Johnson Lam <johnsonlam@fb.com>
2022-07-16 08:43:52 -0700

    Back out "Improve IOBuf::create smart-combine behavior"
    Summary:
    Original commit: rFBS47a319bded0a3d56e06b6e85205138e942a6a3bc
    Original diff: D37759017 (https://github.com/facebook/folly/commit/1ccd0e707f947a6683d826ee99b7619e948162a3)
    
    Reviewed By: Prowindy
    
    Differential Revision: D37890008
    
    fbshipit-source-id: 6573aaa06a3fea639e7d3dbd0050e9357bccd49e

--- folly/io/IOBuf.cpp
+++ folly/io/IOBuf.cpp
@@ -336,27 +336,16 @@ unique_ptr<IOBuf> IOBuf::create(std::size_t capacity) {
     return createCombined(capacity);
   }
 
-  // If we have nallocx, we want to opportunistically combine allocations where
-  // doing so doesn't increase the buffer allocation size.
+  // if we have nallocx, we want to allocate the capacity and the overhead in
+  // a single allocation only if we do not cross into the next allocation class
+  // for some buffer sizes, this can use about 25% extra memory
   if (canNallocx()) {
     auto mallocSize = goodMallocSize(capacity);
-
-    // Best case: all 3 objects fit in the minimum allocation.
-    size_t combinedSize = offsetof(HeapFullStorage, align) + capacity;
-    if (mallocSize >= combinedSize) {
+    // round capacity to a multiple of 8
+    size_t minSize = ((capacity + 7) & ~7) + sizeof(SharedInfo);
+    if (mallocSize >= minSize) {
       return createCombined(capacity);
     }
-
-    // Middle case: SharedInfo fits in the buffer allocation but IOBuf doesn't.
-    size_t partiallyCombinedSize = capacity + sizeof(SharedInfo);
-    if (mallocSize >= partiallyCombinedSize) {
-      return createSeparate(capacity);
-    }
-
-    // Worst case: have to allocate buffer separately, but will allocate IOBuf
-    // and SharedInfo together (this is what takeOwnership does).
-    auto* buf = checkedMalloc(mallocSize);
-    return takeOwnership(SIZED_FREE, buf, mallocSize, 0, 0);
   }
 
   return createSeparate(capacity);
