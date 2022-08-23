df2fca4c391d395e54cab4147c64c71c292c5bf5
Logan Evans <lpe@fb.com>
2022-08-22 17:26:23 -0700

    Add multithreaded benchmarks for TDigest and QuantileEstimator
    Summary:
    Add a benchmark for adding values to a multithreaded TDigest using a
    DigestBuilder.
    
    This makes it easier to compare the performance against other algorithms, such
    as the CPUShardedQuantileHistogram.
    
    This benchmark is designed to mimic the performance of TDigest as used via
    DigestBuider, BufferedStat, and QuantileEstimator. As a consequence, the buffer
    space used is rather huge -- 0.5 MB for a 64-core machine.
    
    Reviewed By: Gownta
    
    Differential Revision: D38880483
    
    fbshipit-source-id: 1b858ff891bfa3a522a6107edbd0ce2e21b252cd

---
+++ folly/stats/test/QuantileEstimatorBenchmark.cpp
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <folly/stats/QuantileEstimator.h>
+
+#include <algorithm>
+#include <atomic>
+#include <chrono>
+#include <random>
+
+#include <folly/Benchmark.h>
+#include <folly/portability/GFlags.h>
+
+void addValueMultithreaded(unsigned int iters, size_t nThreads) {
+  folly::SimpleQuantileEstimator qest;
+
+  constexpr int kNumValues = 512;
+  std::vector<std::vector<double>> valuesPerThread;
+
+  BENCHMARK_SUSPEND {
+    std::default_random_engine generator;
+    std::uniform_real_distribution<double> dist(0.0, 1.0);
+
+    valuesPerThread.reserve(nThreads);
+    for (size_t threadIndex = 0; threadIndex < nThreads; threadIndex++) {
+      valuesPerThread.push_back(std::vector<double>{});
+      auto& values = valuesPerThread.back();
+      values.reserve(kNumValues);
+      for (size_t i = 0; i < kNumValues; i++) {
+        values.push_back(dist(generator));
+      }
+    }
+  }
+
+  std::atomic<int> remainingBatches{static_cast<int>(iters / kNumValues)};
+  std::vector<std::thread> threads(nThreads);
+  for (size_t threadIndex = 0; threadIndex < nThreads; threadIndex++) {
+    threads[threadIndex] = std::thread(
+        [&](size_t index) {
+          while (remainingBatches.fetch_sub(1, std::memory_order_acq_rel) > 0) {
+            for (const auto v : valuesPerThread[index]) {
+              qest.addValue(v);
+            }
+          }
+        },
+        threadIndex);
+  }
+
+  for (auto& th : threads) {
+    th.join();
+  }
+}
+
+BENCHMARK_NAMED_PARAM(addValueMultithreaded, 1, 1)
+BENCHMARK_NAMED_PARAM(addValueMultithreaded, 2, 2)
+BENCHMARK_NAMED_PARAM(addValueMultithreaded, 4, 4)
+BENCHMARK_NAMED_PARAM(addValueMultithreaded, 8, 8)
+BENCHMARK_NAMED_PARAM(addValueMultithreaded, 16, 16)
+BENCHMARK_NAMED_PARAM(addValueMultithreaded, 32, 32)
+
+int main(int argc, char* argv[]) {
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+  folly::runBenchmarks();
+  return 0;
+}
--- folly/stats/test/TDigestBenchmark.cpp
+++ folly/stats/test/TDigestBenchmark.cpp
@@ -14,6 +14,7 @@
  * limitations under the License.
  */
 
+#include <folly/stats/DigestBuilder.h>
 #include <folly/stats/TDigest.h>
 
 #include <algorithm>
@@ -24,6 +25,51 @@
 #include <folly/portability/GFlags.h>
 
 using folly::TDigest;
+
+void addValueMultithreaded(unsigned int iters, size_t nThreads) {
+  // This is a rather huge buffer size because it will allocate over 8k bytes
+  // per CPU. However, it seems to be the typical size used by ODS. The TDigest
+  // algorithm does see substantial speedups by having a large number of
+  // unsorted values to process in a batch.
+  folly::DigestBuilder<TDigest> digestBuilder(
+      /*bufferSize=*/1000, /*digestSize=*/100);
+
+  constexpr int kNumValues = 512;
+  std::vector<std::vector<double>> valuesPerThread;
+
+  BENCHMARK_SUSPEND {
+    std::default_random_engine generator;
+    std::uniform_real_distribution<double> dist(0.0, 1.0);
+
+    valuesPerThread.reserve(nThreads);
+    for (size_t threadIndex = 0; threadIndex < nThreads; threadIndex++) {
+      valuesPerThread.push_back(std::vector<double>{});
+      auto& values = valuesPerThread.back();
+      values.reserve(kNumValues);
+      for (size_t i = 0; i < kNumValues; i++) {
+        values.push_back(dist(generator));
+      }
+    }
+  }
+
+  std::atomic<int> remainingBatches{static_cast<int>(iters / kNumValues)};
+  std::vector<std::thread> threads(nThreads);
+  for (size_t threadIndex = 0; threadIndex < nThreads; threadIndex++) {
+    threads[threadIndex] = std::thread(
+        [&](size_t index) {
+          while (remainingBatches.fetch_sub(1, std::memory_order_acq_rel) > 0) {
+            for (const auto v : valuesPerThread[index]) {
+              digestBuilder.append(v);
+            }
+          }
+        },
+        threadIndex);
+  }
+
+  for (auto& th : threads) {
+    th.join();
+  }
+}
 
 void merge(unsigned int iters, size_t maxSize, size_t bufSize) {
   TDigest digest(maxSize);
@@ -104,17 +150,27 @@ void estimateQuantile(unsigned int iters, size_t maxSize, double quantile) {
   }
 }
 
+BENCHMARK_NAMED_PARAM(addValueMultithreaded, 1, 1)
+BENCHMARK_NAMED_PARAM(addValueMultithreaded, 2, 2)
+BENCHMARK_NAMED_PARAM(addValueMultithreaded, 4, 4)
+BENCHMARK_NAMED_PARAM(addValueMultithreaded, 8, 8)
+BENCHMARK_NAMED_PARAM(addValueMultithreaded, 16, 16)
+BENCHMARK_NAMED_PARAM(addValueMultithreaded, 32, 32)
+
+BENCHMARK_DRAW_LINE();
 BENCHMARK_NAMED_PARAM(merge, 100x1, 100, 100)
 BENCHMARK_RELATIVE_NAMED_PARAM(merge, 100x5, 100, 500)
 BENCHMARK_RELATIVE_NAMED_PARAM(merge, 100x10, 100, 1000)
 BENCHMARK_RELATIVE_NAMED_PARAM(merge, 1000x1, 1000, 1000)
 BENCHMARK_RELATIVE_NAMED_PARAM(merge, 1000x5, 1000, 5000)
 BENCHMARK_RELATIVE_NAMED_PARAM(merge, 1000x10, 1000, 10000)
+
 BENCHMARK_DRAW_LINE();
 BENCHMARK_NAMED_PARAM(mergeDigests, 100x10, 100, 10)
 BENCHMARK_RELATIVE_NAMED_PARAM(mergeDigests, 100x30, 100, 30)
 BENCHMARK_RELATIVE_NAMED_PARAM(mergeDigests, 100x60, 100, 60)
 BENCHMARK_RELATIVE_NAMED_PARAM(mergeDigests, 1000x60, 1000, 60)
+
 BENCHMARK_DRAW_LINE();
 BENCHMARK_NAMED_PARAM(estimateQuantile, 100x1_p001, 100, 0.001)
 BENCHMARK_RELATIVE_NAMED_PARAM(estimateQuantile, 100_p01, 100, 0.01)
@@ -123,6 +179,7 @@ BENCHMARK_RELATIVE_NAMED_PARAM(estimateQuantile, 100_p50, 100, 0.5)
 BENCHMARK_RELATIVE_NAMED_PARAM(estimateQuantile, 100_p75, 100, 0.75)
 BENCHMARK_RELATIVE_NAMED_PARAM(estimateQuantile, 100_p99, 100, 0.99)
 BENCHMARK_RELATIVE_NAMED_PARAM(estimateQuantile, 100_p999, 100, 0.999)
+
 BENCHMARK_DRAW_LINE();
 BENCHMARK_RELATIVE_NAMED_PARAM(estimateQuantile, 1000_p001, 1000, 0.001)
 BENCHMARK_RELATIVE_NAMED_PARAM(estimateQuantile, 1000_p01, 1000, 0.01)
