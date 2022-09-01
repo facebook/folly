c7fbfac9dc5344342422a86f560703250f26dff6
Joey Harrison <joeyh021@fb.com>
2022-09-01 02:04:48 -0700

    Added documentation for IPAddressv4
    Summary:
    Tidied up the documentation for this class.
    
    (Note: this ignores all push blocking failures!)
    
    Reviewed By: Gownta
    
    Differential Revision: D38866604
    
    fbshipit-source-id: e84d8a52e9b58dadababaf279c671f6b236144ee

--- folly/IPAddressV4.h
+++ folly/IPAddressV4.h
@@ -14,6 +14,14 @@
  * limitations under the License.
  */
 
+/**
+ * A representation of an IPv4 address
+ *
+ * @class folly::IPAddressV4
+ * @see IPAddress
+ * @see IPAddressV6
+ */
+
 #pragma once
 
 #include <cstring>
@@ -41,76 +49,84 @@ class IPAddressV6;
 typedef std::pair<IPAddressV4, uint8_t> CIDRNetworkV4;
 
 /**
- * Specialization for IPv4 addresses
+ * Specialization of `std::array` for IPv4 addresses
  */
 typedef std::array<uint8_t, 4> ByteArray4;
 
-/**
- * IPv4 variation of IPAddress.
- *
- * Added methods: toLong, toLongHBO and createIPv6
- *
- * @note toLong/fromLong deal in network byte order, use toLongHBO/fromLongHBO
- * if working in host byte order.
- *
- * @see IPAddress
- */
 class IPAddressV4 {
  public:
-  // Max size of std::string returned by toFullyQualified.
+  /**
+   * Max size of std::string returned by toFullyQualified()
+   */
   static constexpr size_t kMaxToFullyQualifiedSize =
       4 /*words*/ * 3 /*max chars per word*/ + 3 /*separators*/;
 
-  // returns true iff the input string can be parsed as an ipv4-address
+  /**
+   * Returns true if the input string can be parsed as an IP address.
+   */
   static bool validate(StringPiece ip) noexcept;
 
-  // create an IPAddressV4 instance from a uint32_t (network byte order)
+  /**
+   * Create an IPAddressV4 instance from a uint32_t, using network byte
+   * order
+   */
   static IPAddressV4 fromLong(uint32_t src);
-  // same as above but host byte order
+  /**
+   * Create an IPAddressV4 instance from a uint32_t, using host byte
+   * order
+   */
   static IPAddressV4 fromLongHBO(uint32_t src);
 
   /**
-   * Create a new IPAddress instance from the provided binary data.
+   * Create a new IPAddressV4 from the provided ByteRange.
+   *
    * @throws IPAddressFormatException if the input length is not 4 bytes.
    */
   static IPAddressV4 fromBinary(ByteRange bytes);
 
   /**
-   * Non-throwing version of fromBinary().
-   * On failure returns IPAddressFormatError.
+   * Create a new IPAddressV4 from the provided ByteRange.
+   *
+   * Returns an IPAddressFormatError if the input length is not 4 bytes.
    */
   static Expected<IPAddressV4, IPAddressFormatError> tryFromBinary(
       ByteRange bytes) noexcept;
 
   /**
-   * Tries to create a new IPAddressV4 instance from provided string and
-   * returns it on success. Returns IPAddressFormatError on failure.
+   * Create a new IPAddressV4 from the provided string.
+   *
+   * Returns an IPAddressFormatError if the string is not a valid IP.
    */
   static Expected<IPAddressV4, IPAddressFormatError> tryFromString(
       StringPiece str) noexcept;
 
   /**
-   * Returns the address as a Range.
+   * Returns the address as a ByteRange.
    */
   ByteRange toBinary() const {
     return ByteRange((const unsigned char*)&addr_.inAddr_.s_addr, 4);
   }
 
   /**
-   * Create a new IPAddress instance from the in-addr.arpa representation.
+   * Create a new IPAddressV4 from a `in-addr.arpa` representation of an IP
+   * address.
+   *
    * @throws IPAddressFormatException if the input is not a valid in-addr.arpa
    * representation
    */
   static IPAddressV4 fromInverseArpaName(const std::string& arpaname);
 
   /**
-   * Convert a IPv4 address string to a long in network byte order.
-   * @param [in] ip the address to convert
-   * @return the long representation of the address
+   * Convert a IPv4 address string to a long, in network byte order.
    */
+
   static uint32_t toLong(StringPiece ip);
-  // Same as above, but in host byte order.
-  // This is slightly slower than toLong.
+
+  /**
+   * Convert a IPv4 address string to a long, in host byte order.
+   *
+   * This is slightly slower than toLong()
+   */
   static uint32_t toLongHBO(StringPiece ip);
 
   /**
@@ -120,94 +136,182 @@ class IPAddressV4 {
    */
   IPAddressV4();
 
-  // Create an IPAddressV4 from a string
-  // @throws IPAddressFormatException
+  /**
+   * Construct an IPAddressV4 from a string.
+   *
+   * @throws IPAddressFormatException if the string is not a valid IPv4
+   * address.
+   */
   explicit IPAddressV4(StringPiece addr);
 
-  // ByteArray4 constructor
+  /**
+   * Construct an IPAddressV4 from a ByteArray4, in network byte order.
+   */
   explicit IPAddressV4(const ByteArray4& src) noexcept;
 
-  // in_addr constructor
+  /**
+   * Construct an IPAddressV4 from an `in_addr` representation of an IPV4
+   * address
+   */
   explicit IPAddressV4(const in_addr src) noexcept;
 
-  // Return the V6 mapped representation of the address.
+  /**
+   * Return the IPV6 mapped representation of the address.
+   */
   IPAddressV6 createIPv6() const;
 
   /**
-   * Return a V6 address in the format of an 6To4 address.
+   * Return an IPV6 address in the format of a 6To4 address.
    */
   IPAddressV6 getIPv6For6To4() const;
 
-  // Return the long (network byte order) representation of the address.
+  /**
+   * Return the uint32_t representation of the address, in network byte order.
+   */
   uint32_t toLong() const { return toAddr().s_addr; }
 
-  // Return the long (host byte order) representation of the address.
-  // This is slightly slower than toLong.
+  /**
+   * Return the uint32_t representation of the address, in host byte order.
+   */
   uint32_t toLongHBO() const { return ntohl(toLong()); }
 
   /**
-   * @see IPAddress#bitCount
+   * Returns the number of bits in the IP address.
+   *
    * @returns 32
    */
   static constexpr size_t bitCount() { return 32; }
 
   /**
-   * @See IPAddress#toJson
+   * Get a json representation of the IP address.
+   *
+   * This prints a string representation of the address, for human consumption
+   * or logging. The string will take the form of a JSON object that looks like:
+   *  `{family:'AF_INET', addr:'address', hash:long}`.
    */
   std::string toJson() const;
 
+  /**
+   * Returns a hash of the IP address.
+   */
   size_t hash() const {
     static const uint32_t seed = AF_INET;
     uint32_t hashed = hash::fnv32_buf(&addr_, 4);
     return hash::hash_combine(seed, hashed);
   }
 
-  // @see IPAddress#inSubnet
-  // @throws IPAddressFormatException if string doesn't contain a V4 address
+  /**
+   * @overloadbrief Check if the IP address is found in the specified CIDR
+   * netblock.
+   *
+   * @throws IPAddressFormatException if no /mask
+   *
+   * @note This is slower than the other inSubnet() overload. If perf is
+   * important use the other overload, or inSubnetWithMask().
+   * @param [in] cidrNetwork address in "192.168.1.0/24" format
+   * @return true if address is part of specified subnet with cidr
+   */
   bool inSubnet(StringPiece cidrNetwork) const;
 
-  // return true if address is in subnet
+  /**
+   * Check if an IPAddressV4 belongs to a subnet.
+   * @param [in] subnet Subnet to check against (e.g. 192.168.1.0)
+   * @param [in] cidr   CIDR for subnet (e.g. 24 for /24)
+   * @return true if address is part of specified subnet with cidr
+   */
   bool inSubnet(const IPAddressV4& subnet, uint8_t cidr) const {
     return inSubnetWithMask(subnet, fetchMask(cidr));
   }
+
+  /**
+   * Check if an IPAddressV4 belongs to the subnet with the given mask.
+   *
+   * This is the same as inSubnet but the mask is provided instead of looked up
+   * from the cidr.
+   * @param [in] subnet Subnet to check against
+   * @param [in] mask   The netmask for the subnet
+   * @return true if address is part of the specified subnet with mask
+   */
   bool inSubnetWithMask(const IPAddressV4& subnet, const ByteArray4 mask) const;
 
-  // @see IPAddress#isLoopback
+  /**
+   * Return true if the IP address qualifies as localhost.
+   */
   bool isLoopback() const;
 
-  // @see IPAddress#isLinkLocal
+  /**
+   * Return true if the IP address qualifies as link local
+   */
   bool isLinkLocal() const;
 
-  // @see IPAddress#isNonroutable
+  /**
+   * Return true if the IP address is a special purpose address, as defined per
+   * RFC 6890 (i.e. 0.0.0.0).
+   *
+   */
   bool isNonroutable() const;
-
-  // @see IPAddress#isPrivate
+  /**
+   * Return true if the IP address is private, as per RFC 1918 and RFC 4193.
+   *
+   * For example, 192.168.xxx.xxx
+   */
   bool isPrivate() const;
 
-  // @see IPAddress#isMulticast
+  /**
+   * Return true if the IP address is a multicast address.
+   */
   bool isMulticast() const;
 
-  // @see IPAddress#isZero
+  /**
+   * Returns true if the address is all zeros
+   */
   bool isZero() const {
     constexpr auto zero = ByteArray4{{}};
     return 0 == std::memcmp(bytes(), zero.data(), zero.size());
   }
 
+  /**
+   * Return true if the IP address qualifies as broadcast.
+   */
   bool isLinkLocalBroadcast() const {
     return (INADDR_BROADCAST == toLongHBO());
   }
 
-  // @see IPAddress#mask
+  /**
+   * Creates an IPAddressV4 with all but most significant numBits set to
+   * 0.
+   *
+   * @throws IPAddressFormatException if numBits > bitCount()
+   *
+   * @param [in] numBits number of bits to mask
+   * @return IPAddress instance with bits set to 0
+   */
   IPAddressV4 mask(size_t numBits) const;
 
-  // @see IPAddress#str
+  /**
+   * Provides a string representation of address.
+   *
+   * @throws if IPAddressFormatException on `inet_ntop` error.
+   *
+   * The string representation is calculated on demand.
+   */
   std::string str() const;
 
+  /**
+   * Create the inverse arpa representation of the IP address.
+   *
+   */
   std::string toInverseArpaName() const;
 
-  // return underlying in_addr structure
+  /**
+   * Return the underlying `in_addr` structure
+   */
   in_addr toAddr() const { return addr_.inAddr_; }
 
+  /**
+   * Return the IP address represented as a `sockaddr_in` struct
+   *
+   */
   sockaddr_in toSockAddr() const {
     sockaddr_in addr;
     memset(&addr, 0, sizeof(sockaddr_in));
@@ -216,52 +320,91 @@ class IPAddressV4 {
     return addr;
   }
 
+  /**
+   * Return a ByteArray4 containing the bytes of the IP address.
+   */
   ByteArray4 toByteArray() const {
     ByteArray4 ba{{0}};
     std::memcpy(ba.data(), bytes(), 4);
     return ba;
   }
 
-  // @see IPAddress#toFullyQualified
+  /**
+   * Return the fully qualified string representation of the address.
+   *
+   * This is the same as calling str().
+   */
   std::string toFullyQualified() const { return str(); }
 
-  // @see IPAddress#toFullyQualifiedAppend
+  /**
+   * Same as toFullyQualified() but append to an output string.
+   */
   void toFullyQualifiedAppend(std::string& out) const;
 
-  // @see IPAddress#version
+  /**
+   * Returns the version of the IP Address (4).
+   */
   uint8_t version() const { return 4; }
 
   /**
    * Return the mask associated with the given number of bits.
+   *
    * If for instance numBits was 24 (e.g. /24) then the V4 mask returned should
    * be {0xff, 0xff, 0xff, 0x00}.
+   *
    * @param [in] numBits bitmask to retrieve
    * @throws abort if numBits == 0 or numBits > bitCount()
    * @return mask associated with numBits
    */
   static ByteArray4 fetchMask(size_t numBits);
 
-  // Given 2 IPAddressV4, mask pairs extract the longest common IPAddress,
-  // mask pair
+  /**
+   * Given 2 (IPAddressV4, mask) pairs extract the longest common (IPAddressV4,
+   * mask) pair
+   */
   static CIDRNetworkV4 longestCommonPrefix(
       const CIDRNetworkV4& one, const CIDRNetworkV4& two);
-  // Number of bytes in the address representation.
+
+  /**
+   * Return the number of bytes in the IP address.
+   *
+   * @returns 4
+   */
   static size_t byteCount() { return 4; }
-  // get nth most significant bit - 0 indexed
+
+  /**
+   * Get the nth most significant bit of the IP address (0-indexed).
+   * @param bitIndex n
+   */
   bool getNthMSBit(size_t bitIndex) const {
     return detail::getNthMSBitImpl(*this, bitIndex, AF_INET);
   }
-  // get nth most significant byte - 0 indexed
+
+  /**
+   * Get the nth most significant byte of the IP address (0-indexed).
+   * @param byteIndex n
+   */
   uint8_t getNthMSByte(size_t byteIndex) const;
-  // get nth bit - 0 indexed
+
+  /**
+   * Get the nth bit of the IP address (0-indexed).
+   * @param bitIndex n
+   */
   bool getNthLSBit(size_t bitIndex) const {
     return getNthMSBit(bitCount() - bitIndex - 1);
   }
-  // get nth byte - 0 indexed
+
+  /**
+   * Get the nth byte of the IP address (0-indexed).
+   * @param byteIndex n
+   */
   uint8_t getNthLSByte(size_t byteIndex) const {
     return getNthMSByte(byteCount() - byteIndex - 1);
   }
 
+  /**
+   * Returns a pointer to the to IP address bytes, in network byte order.
+   */
   const unsigned char* bytes() const { return addr_.bytes_.data(); }
 
  private:
@@ -277,18 +420,30 @@ class IPAddressV4 {
   } addr_;
 
   /**
-   * Set the current IPAddressV4 object to have the address specified by bytes.
+   * Set the current IPAddressV4 object to the address specified by the
+   * ByteRange given, in network byte order.
+   *
    * Returns IPAddressFormatError if bytes.size() is not 4.
    */
   Expected<Unit, IPAddressFormatError> trySetFromBinary(
       ByteRange bytes) noexcept;
 };
 
-// boost::hash uses hash_value() so this allows boost::hash to work
-// automatically for IPAddressV4
+/**
+ * `boost::hash` uses hash_value() so this allows `boost::hash` to work
+ * automatically for IPAddressV4
+ */
 size_t hash_value(const IPAddressV4& addr);
+
+/**
+ * Appends a string representation of the IP address to the stream using str().
+ */
 std::ostream& operator<<(std::ostream& os, const IPAddressV4& addr);
-// Define toAppend() to allow IPAddressV4 to be used with to<string>
+
+/**
+ * @overloadbrief Define toAppend() to allow IPAddress to be used with
+ * `folly::to<string>`
+ */
 void toAppend(IPAddressV4 addr, std::string* result);
 void toAppend(IPAddressV4 addr, fbstring* result);
 
@@ -298,22 +453,36 @@ void toAppend(IPAddressV4 addr, fbstring* result);
 inline bool operator==(const IPAddressV4& addr1, const IPAddressV4& addr2) {
   return (addr1.toLong() == addr2.toLong());
 }
-// Return true if addr1 < addr2
+
+/**
+ * Return true if addr1 < addr2.
+ */
 inline bool operator<(const IPAddressV4& addr1, const IPAddressV4& addr2) {
   return (addr1.toLongHBO() < addr2.toLongHBO());
 }
-// Derived operators
-inline bool operator!=(const IPAddressV4& a, const IPAddressV4& b) {
-  return !(a == b);
+/**
+ * Return true if addr1 != addr2.
+ */
+inline bool operator!=(const IPAddressV4& addr1, const IPAddressV4& addr2) {
+  return !(addr1 == addr2);
 }
-inline bool operator>(const IPAddressV4& a, const IPAddressV4& b) {
-  return b < a;
+/**
+ * Return true if addr1 > addr2.
+ */
+inline bool operator>(const IPAddressV4& addr1, const IPAddressV4& addr2) {
+  return addr2 < addr1;
 }
-inline bool operator<=(const IPAddressV4& a, const IPAddressV4& b) {
-  return !(a > b);
+/**
+ * Return true if addr1 <= addr2.
+ */
+inline bool operator<=(const IPAddressV4& addr1, const IPAddressV4& addr2) {
+  return !(addr1 > addr2);
 }
-inline bool operator>=(const IPAddressV4& a, const IPAddressV4& b) {
-  return !(a < b);
+/**
+ * Return true if addr1 >= addr2.
+ */
+inline bool operator>=(const IPAddressV4& addr1, const IPAddressV4& addr2) {
+  return !(addr1 < addr2);
 }
 
 } // namespace folly
