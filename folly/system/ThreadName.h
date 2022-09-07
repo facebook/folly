0237b42948898477a3bdd7c4cbac8d600509491b
Srivatsan Ramesh <srir@fb.com>
2022-09-07 16:27:22 -0700

    system/ThreadName.h
    Reviewed By: praihan
    
    Differential Revision: D39123103
    
    fbshipit-source-id: 6beb3dcfe61d727a3d8d8df43bf11cd995bab465

--- folly/system/ThreadName.h
+++ folly/system/ThreadName.h
@@ -41,6 +41,8 @@ bool canSetOtherThreadName();
 /**
  * Get the name of the given thread, or nothing if an error occurs
  * or the functionality is not available.
+ *
+ * @param tid Thread id
  */
 Optional<std::string> getThreadName(std::thread::id tid);
 
@@ -51,14 +53,20 @@ Optional<std::string> getCurrentThreadName();
 
 /**
  * Set the name of the given thread.
+ *
  * Returns false on failure, if an error occurs or the functionality
  * is not available.
+ *
+ * @param tid Thread id
+ * @param name Name to set
  */
 bool setThreadName(std::thread::id tid, StringPiece name);
 bool setThreadName(pthread_t pid, StringPiece name);
 
 /**
  * Equivalent to setThreadName(std::this_thread::get_id(), name);
+ *
+ * @param name Name to set
  */
 bool setThreadName(StringPiece name);
 } // namespace folly
