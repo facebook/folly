3494eb41bc14535ae80e5356dea4506b6833bab2
Peter Dillinger <peterd@meta.com>
2022-11-06 19:28:48 -0800

    Improve EvictingCacheMapBench
    Summary: The essential problem of the existing benchmark was that it favored naive / fragile hashing (by feeding sequential keys) over mature / robust hashing.
    
    Reviewed By: Gownta
    
    Differential Revision: D40882868
    
    fbshipit-source-id: f51cf1009796731d90e9a3dfef20f81f1f2cbd7b

--- folly/container/test/EvictingCacheMapBench.cpp
+++ folly/container/test/EvictingCacheMapBench.cpp
@@ -20,6 +20,14 @@
 
 using namespace folly;
 
+inline uint64_t key(size_t i) {
+  // Sensitive to non-avalanched hasher
+  uint64_t marker = uint64_t{12345} << 48;
+  uint64_t tmp = marker ^ i;
+  int shift = ((i >> 8) & 7) * 8;
+  return (tmp << shift) | (tmp >> (64 - shift));
+}
+
 // This function creates a cache of size `numElts` and scans through it `n`
 // times in order to cause the most churn in the LRU structure as possible.
 // This is meant to exercise the performance of the lookup path in the cache,
@@ -27,25 +35,46 @@ using namespace folly;
 void scanCache(uint32_t n, size_t numElts) {
   BenchmarkSuspender suspender;
 
-  EvictingCacheMap<size_t, size_t> m(numElts);
+  EvictingCacheMap<uint64_t, size_t> m(numElts);
   for (size_t i = 0; i < numElts; ++i) {
-    m.insert(i, i);
+    m.insert(key(i), i);
   }
 
-  suspender.dismissing([&]() {
-    for (uint32_t i = 0; i < n; ++i) {
-      for (size_t j = 0; j < numElts; ++j) {
-        m.get(j);
-      }
+  suspender.dismiss();
+  for (uint32_t i = 0; i < n; ++i) {
+    for (size_t j = 0; j < numElts; ++j) {
+      m.get(key(j));
     }
-  });
+  }
 }
 
-BENCHMARK_PARAM(scanCache, 1000) // 1K
-BENCHMARK_PARAM(scanCache, 10000) // 10K
-BENCHMARK_PARAM(scanCache, 100000) // 100K
-BENCHMARK_PARAM(scanCache, 1000000) // 1M
+void insertCache(uint32_t n, size_t numElts) {
+  // Under-size to force cache evictions
+  EvictingCacheMap<uint64_t, size_t> m(numElts * 9 / 10);
+  for (uint32_t i = 0; i < n; ++i) {
+    for (size_t j = 0; j < numElts; ++j) {
+      m.insert(key(j), i);
+    }
+  }
+}
 
-int main() {
+// Increment by factor of 4 * golden ratio to vary distance between
+// powers of 2
+BENCHMARK_PARAM(scanCache, 1000)
+BENCHMARK_PARAM(scanCache, 6472)
+BENCHMARK_PARAM(scanCache, 41889)
+BENCHMARK_PARAM(scanCache, 271108)
+BENCHMARK_PARAM(scanCache, 1754650) // 1.75M
+BENCHMARK_PARAM(scanCache, 11356334) // 11.3M
+
+BENCHMARK_PARAM(insertCache, 1000)
+BENCHMARK_PARAM(insertCache, 6472)
+BENCHMARK_PARAM(insertCache, 41889)
+BENCHMARK_PARAM(insertCache, 271108)
+BENCHMARK_PARAM(insertCache, 1754650) // 1.75M
+BENCHMARK_PARAM(insertCache, 11356334) // 11.3M
+
+int main(int argc, char** argv) {
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
   runBenchmarks();
 }
