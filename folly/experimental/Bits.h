5c114cc5df47e5bcd428333c3dcd46bbd25eb08d
Orvid King <orvid@meta.com>
2023-05-08 14:58:02 -0700

    Use bextr in folly::Bits
    Summary: Clang doesn't generate this itself, so explicitly do it instead.
    
    Reviewed By: dmm-fb
    
    Differential Revision: D43849336
    
    fbshipit-source-id: 84a0bfe64aa7c9f4d4e104dd19422f7283966995

--- folly/experimental/Bits.h
+++ folly/experimental/Bits.h
@@ -16,6 +16,10 @@
 
 #pragma once
 
+#ifdef __BMI__
+#include <immintrin.h>
+#endif
+
 #include <cstddef>
 #include <limits>
 #include <type_traits>
@@ -289,7 +293,11 @@ inline auto Bits<T, Traits>::get(const T* p, size_t bitStart, size_t count)
 template <class T, class Traits>
 inline auto Bits<T, Traits>::innerGet(const T* p, size_t offset, size_t count)
     -> UnderlyingType {
+#if __BMI__
+  return _bextr_u64(Traits::load(*p), offset, count);
+#else
   return (Traits::load(*p) >> offset) & ones(count);
+#endif
 }
 
 template <class T, class Traits>
