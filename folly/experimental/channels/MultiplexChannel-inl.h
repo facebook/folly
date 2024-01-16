6fadf4d72835606829ca8cac95532834061d1359
Richard Barnes <rbarnes@meta.com>
2024-01-15 21:01:55 -0800

    Fix shadowed variable in folly/experimental/channels/MultiplexChannel-inl.h
    Summary:
    Our upcoming compiler upgrade will require us not to have shadowed variables. Such variables have a _high_ bug rate and reduce readability, so we would like to avoid them even if the compiler was not forcing us to do so.
    
    This codemod attempts to fix an instance of a shadowed variable. Please review with care: if it's failed the result will be a silent bug.
    
    **What's a shadowed variable?**
    
    Shadowed variables are variables in an inner scope with the same name as another variable in an outer scope. Having the same name for both variables might be semantically correct, but it can make the code confusing to read! It can also hide subtle bugs.
    
    This diff fixes such an issue by renaming the variable.
    
     - If you approve of this diff, please use the "Accept & Ship" button :-)
    
    Reviewed By: palmje
    
    Differential Revision: D52790845
    
    fbshipit-source-id: f271e8b1fa41d7a87f230b71994a2359b531ba2b

--- folly/experimental/channels/MultiplexChannel-inl.h
+++ folly/experimental/channels/MultiplexChannel-inl.h
@@ -201,9 +201,9 @@ class MultiplexChannelProcessor : public IChannelCallback {
         [this,
          key = std::move(key),
          subscriptionArg = std::move(subscriptionArg),
-         sender = std::move(sender)]() mutable -> folly::coro::Task<void> {
+         sender_2 = std::move(sender)]() mutable -> folly::coro::Task<void> {
           co_await processNewSubscription(
-              std::move(key), std::move(subscriptionArg), std::move(sender));
+              std::move(key), std::move(subscriptionArg), std::move(sender_2));
         });
     return std::move(receiver);
   }
@@ -214,8 +214,8 @@ class MultiplexChannelProcessor : public IChannelCallback {
         std::vector<std::pair<KeyType, KeyContextType>>>();
     executeWithMutexWhenReady(
         [this,
-         promise = std::move(promise)]() mutable -> folly::coro::Task<void> {
-          co_await processClearUnusedSubscriptions(std::move(promise));
+         promise_2 = std::move(promise)]() mutable -> folly::coro::Task<void> {
+          co_await processClearUnusedSubscriptions(std::move(promise_2));
         });
     return folly::coro::toTask(std::move(future));
   }
