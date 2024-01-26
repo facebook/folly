b0dfaeb9eaa3f5d64083bf71c25893e98f18061e
Andrew Gallagher <andrewjcg@meta.com>
2024-01-26 11:51:14 -0800

    Support customizing signals
    Summary:
    This mimics the `SEGFAULT_SIGNALS` env var support from
    the original `libsegfault.so`:
    https://github.com/lattera/glibc/blob/master/debug/segfault.c#L148-L199.
    Unlike glibc's `libsegfault.so`, which only watches `SIGSEGV` by default
    we install handlers for all "fatal" signals (except `SIGTERM`).
    
    The minor change this makes in behavior for `libFollySegFault.so` is
    that we no longer install a handler for `SIGTERM`.
    
    Reviewed By: luciang
    
    Differential Revision: D53049261
    
    fbshipit-source-id: dacc15efc19dd6a918cc5f1d8adf17609d1f3c74

--- folly/experimental/symbolizer/tool/LibSegFault.cpp
+++ folly/experimental/symbolizer/tool/LibSegFault.cpp
@@ -14,8 +14,69 @@
  * limitations under the License.
  */
 
+/**
+ * A standalone shared library that can be `LD_PRELOAD`d to print symbolized
+ * native stack traces when the process dies due to a signal. By default, this
+ * is enabled for SIGSEGV, SIGILL, SIGBUS, SIGSTKFLT, SIGABRT, and SIGFPE.
+ *
+ * Based on glibc's `libSegFault.so`:
+ * https://github.com/lattera/glibc/blob/master/debug/segfault.c
+ *
+ * Usage:
+ *
+ *   $ LD_PRELOAD=libFollySegFault.so <my_prog> ...
+ *
+ */
+
 #include <folly/experimental/symbolizer/SignalHandler.h>
 
+#include <bitset>
+#include <csignal>
+#include <cstdlib>
+#include <string_view>
+#include <unordered_map>
+
+#include <folly/Range.h>
+
 static void __attribute__((constructor)) install_handler(void) {
-  folly::symbolizer::installFatalSignalHandler();
+  static const std::unordered_map<std::string_view, int> signalMap = {
+      {"segv", SIGSEGV},
+      {"ill", SIGILL},
+#ifdef SIGBUS
+      {"bus", SIGBUS},
+#endif
+#ifdef SIGSTKFLT
+      {"stkflt", SIGSTKFLT},
+#endif
+      {"abrt", SIGABRT},
+      {"fpe", SIGFPE},
+  };
+
+  std::bitset<64> sigMask;
+
+  // The string description of signals to install the handler for.
+  folly::StringPiece sigEnv = std::getenv("FOLLY_SEGFAULT_SIGNALS") ?: "all";
+  while (!sigEnv.empty()) {
+    std::string_view sigName = sigEnv.split_step(" ");
+
+    // If signame of all was given, add in all signals.
+    if (sigName == "all") {
+      for (const auto& ent : signalMap) {
+        sigMask.set(ent.second);
+      }
+    } else {
+      auto sig = signalMap.find(sigName);
+      if (sig == signalMap.end()) {
+        fprintf(
+            stderr,
+            "unknown signal: \"%.*s\"\n",
+            static_cast<int>(sigName.length()),
+            sigName.data());
+        std::abort();
+      }
+      sigMask.set(sig->second);
+    }
+  }
+
+  folly::symbolizer::installFatalSignalHandler(sigMask);
 }
