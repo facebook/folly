07fbb4ec1edfccf8f735bc37d5f50cf0cd6ad9ff
christian <christian>
2024-02-03 16:21:32 +0100

    set write

--- folly/portability/OpenSSL.h
+++ folly/portability/OpenSSL.h
@@ -198,7 +198,7 @@ EC_KEY* EVP_PKEY_get0_EC_KEY(EVP_PKEY* pkey);
 // BIO_METHOD* BIO_meth_new(int type, const char* name);
 // void BIO_meth_free(BIO_METHOD* biom);
 int BIO_meth_set_read(BIO_METHOD* biom, int (*read)(BIO*, char*, int));
-int BIO_meth_set_write(BIO_METHOD* biom, int (*write)(BIO*, const char*, int));
+// int BIO_meth_set_write(BIO_METHOD* biom, int (*write)(BIO*, const char*, int));
 int BIO_meth_set_puts(BIO_METHOD* biom, int (*bputs)(BIO*, const char*));
 int BIO_meth_set_gets(BIO_METHOD* biom, int (*bgets)(BIO*, char*, int));
 int BIO_meth_set_ctrl(BIO_METHOD* biom, long (*ctrl)(BIO*, int, long, void*));
