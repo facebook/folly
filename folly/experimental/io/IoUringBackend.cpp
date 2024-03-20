df4505eaf7f7b61f994664f4da28a25772368a44
Xiaofei Du <xiaofeidu@meta.com>
2024-03-19 17:22:18 -0700

    Create class IoUringProvidedBufferRing
    Summary:
    We are going to add buffer provider for ZC Rx. We should stop
    adding buffer providers into anonymous namespaces of IoUringBackend.cpp as a
    kitchen sink.
    
    This change creates a class IoUringProvidedBufferRing that has a private
    class ProvidedBuffersBuffer. Most of the existing buffer logic is kept intact.
    We now pass io_uring ptr to the provided buffer ring in stead of a backend ptr as
    only the io_uring ptr of the backend is needed, not the backend itself in the buffer
    ring. Exception class LibUringCallError is added to throw when liburing call fails.
    So in the IoUringBackend, it can still return NotAvailable exception when it catches
    the LibUringCallError exception.
    
    By bringing the buffer provider related code out of the anonymous namespace of
    IoUringBackend.cpp, we could also add unit tests for the buffer provider directly (
    No way to test code in anonymous namespace of a .cpp directly).
    
    This paves way for us to add a new buffer provider for ZC Rx
    
    Reviewed By: spikeh
    
    Differential Revision: D55050237
    
    fbshipit-source-id: b08f87dc947cdc3a5e7dd063318470085aa7da87

--- folly/experimental/io/IoUringBackend.cpp
+++ folly/experimental/io/IoUringBackend.cpp
@@ -52,6 +52,11 @@ extern "C" FOLLY_ATTR_WEAK void eb_poll_loop_post_hook(
 #else
 #define FOLLY_IO_URING_UP_TO_DATE 0
 #endif
+
+#if FOLLY_IO_URING_UP_TO_DATE
+#include <folly/experimental/io/IoUringProvidedBufferRing.h>
+#endif
+
 namespace folly {
 
 namespace {
@@ -322,282 +327,10 @@ static folly::Indestructible<SQGroupInfoRegistry> sSQGroupInfoRegistry;
 
 #if FOLLY_IO_URING_UP_TO_DATE
 
-class ProvidedBuffersBuffer {
- public:
-  static size_t calcBufferSize(int bufferShift) {
-    return 1LLU << std::max<int>(5, bufferShift);
-  }
-
-  ProvidedBuffersBuffer(
-      int count, int bufferShift, int ringCountShift, bool huge_pages)
-      : bufferShift_(bufferShift), bufferCount_(count) {
-    // space for the ring
-    int ringCount = 1 << ringCountShift;
-    ringMask_ = ringCount - 1;
-    ringMemSize_ = sizeof(struct io_uring_buf) * ringCount;
-
-    ringMemSize_ = (ringMemSize_ + kBufferAlignMask) & (~kBufferAlignMask);
-
-    if (bufferShift_ < 5) {
-      bufferShift_ = 5; // for alignment
-    }
-
-    sizePerBuffer_ = calcBufferSize(bufferShift_);
-    bufferSize_ = sizePerBuffer_ * count;
-    allSize_ = ringMemSize_ + bufferSize_;
-
-    int pages;
-    if (huge_pages) {
-      allSize_ = (allSize_ + kHugePageMask) & (~kHugePageMask);
-      pages = allSize_ / (1 + kHugePageMask);
-    } else {
-      allSize_ = (kPageMask + kPageMask) & ~kPageMask;
-      pages = allSize_ / (1 + kPageMask);
-    }
-
-    buffer_ = ::mmap(
-        nullptr,
-        allSize_,
-        PROT_READ | PROT_WRITE,
-        MAP_ANONYMOUS | MAP_PRIVATE,
-        -1,
-        0);
-
-    if (buffer_ == MAP_FAILED) {
-      auto errnoCopy = errno;
-      throw std::runtime_error(folly::to<std::string>(
-          "unable to allocate pages of size ",
-          allSize_,
-          " pages=",
-          pages,
-          ": ",
-          folly::errnoStr(errnoCopy)));
-    }
-
-    bufferBuffer_ = ((char*)buffer_) + ringMemSize_;
-    ringPtr_ = (struct io_uring_buf_ring*)buffer_;
-
-    if (huge_pages) {
-      int ret = madvise(buffer_, allSize_, MADV_HUGEPAGE);
-      PLOG_IF(ERROR, ret) << "cannot enable huge pages";
-    }
-  }
-
-  struct io_uring_buf_ring* ring() const noexcept {
-    return ringPtr_;
-  }
-
-  struct io_uring_buf* ringBuf(int idx) const noexcept {
-    return &ringPtr_->bufs[idx & ringMask_];
-  }
-
-  uint32_t bufferCount() const noexcept { return bufferCount_; }
-  uint32_t ringCount() const noexcept { return 1 + ringMask_; }
-
-  char* buffer(uint16_t idx) {
-    size_t offset = (size_t)idx << bufferShift_;
-    return bufferBuffer_ + offset;
-  }
-
-  ~ProvidedBuffersBuffer() { ::munmap(buffer_, allSize_); }
-
-  size_t sizePerBuffer() const { return sizePerBuffer_; }
-
- private:
-  void* buffer_;
-  size_t allSize_;
-
-  size_t ringMemSize_;
-  struct io_uring_buf_ring* ringPtr_;
-  int ringMask_;
-
-  size_t bufferSize_;
-  size_t bufferShift_;
-  size_t sizePerBuffer_;
-  char* bufferBuffer_;
-  uint32_t bufferCount_;
-
-  // static constexpr
-  static constexpr size_t kHugePageMask = (1LLU << 21) - 1; // 2MB
-  static constexpr size_t kPageMask = (1LLU << 12) - 1; // 4095
-  static constexpr size_t kBufferAlignMask{31LLU};
-};
-
-class ProvidedBufferRing : public IoUringBufferProviderBase {
- public:
-  ProvidedBufferRing(
-      IoUringBackend* backend,
-      uint16_t gid,
-      int count,
-      int bufferShift,
-      int ringSizeShift)
-      : IoUringBufferProviderBase(
-            gid, ProvidedBuffersBuffer::calcBufferSize(bufferShift)),
-        backend_(backend),
-        buffer_(count, bufferShift, ringSizeShift, true) {
-    if (count > std::numeric_limits<uint16_t>::max()) {
-      throw std::runtime_error("too many buffers");
-    }
-    if (count <= 0) {
-      throw std::runtime_error("not enough buffers");
-    }
-
-    ioBufCallbacks_.assign((count + (sizeof(void*) - 1)) / sizeof(void*), this);
-
-    initialRegister();
-
-    gottenBuffers_ += count;
-    for (int i = 0; i < count; i++) {
-      returnBuffer(i);
-    }
-  }
-
-  void enobuf() noexcept override {
-    {
-      // what we want to do is something like
-      // if (cachedTail_ != localTail_) {
-      //   publish();
-      //   enobuf_ = false;
-      // }
-      // but if we are processing a batch it doesn't really work
-      // because we'll likely get an ENOBUF straight after
-      enobuf_.store(true, std::memory_order_relaxed);
-    }
-    VLOG_EVERY_N(1, 500) << "enobuf";
-  }
-
-  void unusedBuf(uint16_t i) noexcept override {
-    gottenBuffers_++;
-    returnBuffer(i);
-  }
-
-  uint32_t count() const noexcept override { return buffer_.bufferCount(); }
-
-  void destroy() noexcept override {
-    ::io_uring_unregister_buf_ring(backend_->ioRingPtr(), gid());
-    shutdownReferences_ = 1;
-    auto returned = returnedBuffers_.load();
-    {
-      std::lock_guard<std::mutex> guard(shutdownMutex_);
-      wantsShutdown_ = true;
-      // add references for every missing one
-      // we can assume that there will be no more from the kernel side.
-      // there is a race condition here between reading wantsShutdown_ and
-      // a return incrementing the number of returned references, but it is very
-      // unlikely to trigger as everything is shutting down, so there should not
-      // actually be any buffer returns. Worse case this will leak, but since
-      // everything is shutting down anyway it should not be a problem.
-      uint64_t const gotten = gottenBuffers_;
-      DCHECK(gottenBuffers_ >= returned);
-      uint32_t outstanding = (gotten - returned);
-      shutdownReferences_ += outstanding;
-    }
-    if (shutdownReferences_.fetch_sub(1) == 1) {
-      delete this;
-    }
-  }
-
-  std::unique_ptr<IOBuf> getIoBuf(uint16_t i, size_t length) noexcept override {
-    std::unique_ptr<IOBuf> ret;
-    DCHECK(!wantsShutdown_);
-
-    // use a weird convention: userData = ioBufCallbacks_.data() + i
-    // ioBufCallbacks_ is just a list of the same pointer, to this
-    // so we don't need to malloc anything
-    auto free_fn = [](void*, void* userData) {
-      size_t pprov = (size_t)userData & ~((size_t)(sizeof(void*) - 1));
-      ProvidedBufferRing* prov = *(ProvidedBufferRing**)pprov;
-      uint16_t idx = (size_t)userData - (size_t)prov->ioBufCallbacks_.data();
-      prov->returnBuffer(idx);
-    };
-
-    ret = IOBuf::takeOwnership(
-        (void*)getData(i),
-        sizePerBuffer_,
-        length,
-        free_fn,
-        (void*)(((size_t)ioBufCallbacks_.data()) + i));
-    gottenBuffers_++;
-    return ret;
-  }
-
-  bool available() const noexcept override {
-    return !enobuf_.load(std::memory_order_relaxed);
-  }
-
- private:
-  void initialRegister() {
-    struct io_uring_buf_reg reg;
-    memset(&reg, 0, sizeof(reg));
-    reg.ring_addr = (__u64)buffer_.ring();
-    reg.ring_entries = buffer_.ringCount();
-    reg.bgid = gid();
-
-    int ret = ::io_uring_register_buf_ring(backend_->ioRingPtr(), &reg, 0);
-
-    if (ret) {
-      throw IoUringBackend::NotAvailable(folly::to<std::string>(
-          "unable to register provided buffer ring ",
-          -ret,
-          ": ",
-          folly::errnoStr(-ret)));
-    }
-  }
-
-  std::atomic<uint16_t>* sharedTail() {
-    return reinterpret_cast<std::atomic<uint16_t>*>(&buffer_.ring()->tail);
-  }
-
-  bool tryPublish(uint16_t expected, uint16_t value) noexcept {
-    return sharedTail()->compare_exchange_strong(
-        expected, value, std::memory_order_release);
-  }
-
-  void returnBufferInShutdown() noexcept {
-    { std::lock_guard<std::mutex> guard(shutdownMutex_); }
-    if (shutdownReferences_.fetch_sub(1) == 1) {
-      delete this;
-    }
-  }
-
-  void returnBuffer(uint16_t i) noexcept {
-    if (FOLLY_UNLIKELY(wantsShutdown_)) {
-      returnBufferInShutdown();
-      return;
-    }
-    uint16_t this_idx = static_cast<uint16_t>(returnedBuffers_++);
-    __u64 addr = (__u64)buffer_.buffer(i);
-    uint16_t next_tail = this_idx + 1;
-    auto* r = buffer_.ringBuf(this_idx);
-    r->addr = addr;
-    r->len = buffer_.sizePerBuffer();
-    r->bid = i;
-
-    if (tryPublish(this_idx, next_tail)) {
-      enobuf_.store(false, std::memory_order_relaxed);
-    }
-    DVLOG(9) << "returnBuffer(" << i << ")@" << this_idx;
-  }
-
-  char const* getData(uint16_t i) { return buffer_.buffer(i); }
-
-  IoUringBackend* backend_;
-  ProvidedBuffersBuffer buffer_;
-  std::atomic<bool> enobuf_{false};
-  std::vector<ProvidedBufferRing*> ioBufCallbacks_;
-
-  uint64_t gottenBuffers_{0};
-  std::atomic<uint64_t> returnedBuffers_{0};
-
-  std::atomic<bool> wantsShutdown_{false};
-  std::atomic<uint32_t> shutdownReferences_;
-  std::mutex shutdownMutex_;
-};
-
 template <class... Args>
-ProvidedBufferRing::UniquePtr makeProvidedBufferRing(Args&&... args) {
-  return ProvidedBufferRing::UniquePtr(
-      new ProvidedBufferRing(std::forward<Args>(args)...));
+IoUringProvidedBufferRing::UniquePtr makeProvidedBufferRing(Args&&... args) {
+  return IoUringProvidedBufferRing::UniquePtr(
+      new IoUringProvidedBufferRing(std::forward<Args>(args)...));
 }
 
 #else
@@ -1360,12 +1093,16 @@ void IoUringBackend::initSubmissionLinked() {
     int ringShift =
         std::max<int>(get_shift(options_.initialProvidedBuffersCount), 1);
 
-    bufferProvider_ = makeProvidedBufferRing(
-        this,
-        nextBufferProviderGid(),
-        options_.initialProvidedBuffersCount,
-        sizeShift,
-        ringShift);
+    try {
+      bufferProvider_ = makeProvidedBufferRing(
+          this->ioRingPtr(),
+          nextBufferProviderGid(),
+          options_.initialProvidedBuffersCount,
+          sizeShift,
+          ringShift);
+    } catch (const IoUringProvidedBufferRing::LibUringCallError& ex) {
+      throw NotAvailable(ex.what());
+    }
   }
 }
 
---
+++ folly/experimental/io/IoUringProvidedBufferRing.h
@@ -0,0 +1,132 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <folly/experimental/io/IoUringBase.h>
+#include <folly/experimental/io/Liburing.h>
+#include <folly/portability/SysMman.h>
+
+#if FOLLY_HAS_LIBURING
+
+#include <liburing.h>
+
+namespace folly {
+
+class IoUringProvidedBufferRing : public IoUringBufferProviderBase {
+ public:
+  class LibUringCallError : public std::runtime_error {
+   public:
+    using std::runtime_error::runtime_error;
+  };
+
+  IoUringProvidedBufferRing(
+      io_uring* ioRingPtr,
+      uint16_t gid,
+      int count,
+      int bufferShift,
+      int ringSizeShift);
+
+  void enobuf() noexcept override;
+  void unusedBuf(uint16_t i) noexcept override;
+  void destroy() noexcept override;
+  std::unique_ptr<IOBuf> getIoBuf(uint16_t i, size_t length) noexcept override;
+
+  uint32_t count() const noexcept override { return buffer_.bufferCount(); }
+  bool available() const noexcept override {
+    return !enobuf_.load(std::memory_order_relaxed);
+  }
+
+ private:
+  void initialRegister();
+  void returnBufferInShutdown() noexcept;
+  void returnBuffer(uint16_t i) noexcept;
+
+  std::atomic<uint16_t>* sharedTail() {
+    return reinterpret_cast<std::atomic<uint16_t>*>(&buffer_.ring()->tail);
+  }
+
+  bool tryPublish(uint16_t expected, uint16_t value) noexcept {
+    return sharedTail()->compare_exchange_strong(
+        expected, value, std::memory_order_release);
+  }
+
+  char const* getData(uint16_t i) { return buffer_.buffer(i); }
+
+  class ProvidedBuffersBuffer {
+   public:
+    ProvidedBuffersBuffer(
+        int count, int bufferShift, int ringCountShift, bool huge_pages);
+    ~ProvidedBuffersBuffer() { ::munmap(buffer_, allSize_); }
+
+    static size_t calcBufferSize(int bufferShift) {
+      return 1LLU << std::max<int>(5, bufferShift);
+    }
+
+    struct io_uring_buf_ring* ring() const noexcept {
+      return ringPtr_;
+    }
+
+    struct io_uring_buf* ringBuf(int idx) const noexcept {
+      return &ringPtr_->bufs[idx & ringMask_];
+    }
+
+    uint32_t bufferCount() const noexcept { return bufferCount_; }
+    uint32_t ringCount() const noexcept { return 1 + ringMask_; }
+
+    char* buffer(uint16_t idx) {
+      size_t offset = (size_t)idx << bufferShift_;
+      return bufferBuffer_ + offset;
+    }
+
+    size_t sizePerBuffer() const { return sizePerBuffer_; }
+
+   private:
+    void* buffer_;
+    size_t allSize_;
+
+    size_t ringMemSize_;
+    struct io_uring_buf_ring* ringPtr_;
+    int ringMask_;
+
+    size_t bufferSize_;
+    size_t bufferShift_;
+    size_t sizePerBuffer_;
+    char* bufferBuffer_;
+    uint32_t bufferCount_;
+
+    // static constexpr
+    static constexpr size_t kHugePageMask = (1LLU << 21) - 1; // 2MB
+    static constexpr size_t kPageMask = (1LLU << 12) - 1; // 4095
+    static constexpr size_t kBufferAlignMask{31LLU};
+  };
+
+  io_uring* ioRingPtr_;
+  ProvidedBuffersBuffer buffer_;
+  std::atomic<bool> enobuf_{false};
+  std::vector<IoUringProvidedBufferRing*> ioBufCallbacks_;
+
+  uint64_t gottenBuffers_{0};
+  std::atomic<uint64_t> returnedBuffers_{0};
+
+  std::atomic<bool> wantsShutdown_{false};
+  std::atomic<uint32_t> shutdownReferences_;
+  std::mutex shutdownMutex_;
+};
+
+} // namespace folly
+
+#endif
