17be1d518a8f7b5aa2a943282be44fad7a048ddf
Xiaofei Du <xiaofeidu@meta.com>
2024-03-20 04:06:29 -0700

    Handle memStore empty case in AsyncSocket::processZeroCopyRead
    Summary:
    memStore can be empty when we don't process and return the buffers fast enough.
    So it can return nullptr. We should just resort back to normal read processing
    
    Reviewed By: dmm-fb
    
    Differential Revision: D55104399
    
    fbshipit-source-id: 80984043aaadd08b7fed6ba279963b5e8ad0d9a4

--- folly/io/async/AsyncSocket.cpp
+++ folly/io/async/AsyncSocket.cpp
@@ -3005,6 +3005,9 @@ AsyncSocket::ReadCode AsyncSocket::processZeroCopyRead() {
   }
 
   auto ptr = memStore->get();
+  if (!ptr) {
+    return ReadCode::READ_NOT_SUPPORTED;
+  }
 
   void* copybuf = nullptr;
   size_t copybuf_len = 0;
