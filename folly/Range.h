4532ceced5f63c7f3971aa28b2dd6018c2eb28b9
Richard Barnes <rbarnes@meta.com>
2024-04-17 16:22:46 -0700

    Fix deprecated literal operator in folly/Range.h
    Summary:
    Required for LLVM-17.
    
    Fixes
    ```
         45 fbcode/folly/__range__/buck-headers/folly/Range.h:1680:41: error: identifier '_sp' preceded by whitespace in a literal operator declaration is deprecated [-Werror,-Wdeprecated-literal-operator]
         37 fbcode/folly/__range__/buck-headers/folly/Range.h:1702:44: error: identifier '_sp' preceded by whitespace in a literal operator declaration is deprecated [-Werror,-Wdeprecated-literal-operator]
         37 fbcode/folly/__range__/buck-headers/folly/Range.h:1697:45: error: identifier '_sp' preceded by whitespace in a literal operator declaration is deprecated [-Werror,-Wdeprecated-literal-operator]
         37 fbcode/folly/__range__/buck-headers/folly/Range.h:1692:45: error: identifier '_sp' preceded by whitespace in a literal operator declaration is deprecated [-Werror,-Wdeprecated-literal-operator]
         37 fbcode/folly/__range__/buck-headers/folly/Range.h:1686:44: error: identifier '_sp' preceded by whitespace in a literal operator declaration is deprecated [-Werror,-Wdeprecated-literal-operator]
    ```
    
    Reviewed By: Gownta
    
    Differential Revision: D56208165
    
    fbshipit-source-id: ca37a5b2fe65a638ccdffb8b59eb794e8ec658d5

--- folly/Range.h
+++ folly/Range.h
@@ -1677,29 +1677,29 @@ struct hasher<
  */
 inline namespace literals {
 inline namespace string_piece_literals {
-constexpr Range<char const*> operator"" _sp(
+constexpr Range<char const*> operator""_sp(
     char const* str, size_t len) noexcept {
   return Range<char const*>(str, len);
 }
 
 #if defined(__cpp_char8_t) && __cpp_char8_t >= 201811L
-constexpr Range<char8_t const*> operator"" _sp(
+constexpr Range<char8_t const*> operator""_sp(
     char8_t const* str, size_t len) noexcept {
   return Range<char8_t const*>(str, len);
 }
 #endif
 
-constexpr Range<char16_t const*> operator"" _sp(
+constexpr Range<char16_t const*> operator""_sp(
     char16_t const* str, size_t len) noexcept {
   return Range<char16_t const*>(str, len);
 }
 
-constexpr Range<char32_t const*> operator"" _sp(
+constexpr Range<char32_t const*> operator""_sp(
     char32_t const* str, size_t len) noexcept {
   return Range<char32_t const*>(str, len);
 }
 
-constexpr Range<wchar_t const*> operator"" _sp(
+constexpr Range<wchar_t const*> operator""_sp(
     wchar_t const* str, size_t len) noexcept {
   return Range<wchar_t const*>(str, len);
 }
