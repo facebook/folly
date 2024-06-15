c091ef450323e50fe19e3b85dce1d4e9244bec59
Yedidya Feldblum <yfeldblum@meta.com>
2024-06-14 19:10:14 -0700

    revise hazptr_domain default executor not to use ThreadLocal
    Summary:
    A workaround for a case where it happens to be called in a deeply-nested call-stack from within thread-exit.
    
    The default executor is `QueuedImmediateExecutor` which uses `ThreadLocal` internally. This is necessary for `QueuedImmediateExecutor`, since it is valid for there to be many concurrently-living instances of this executor type.
    
    The `hazptr_domain` default executor does not have this requirement so, to have equivalent queued-immediate behavior, it does not have to use `ThreadLocal` internally and can use keyword `thread_local` instead.
    
    Reviewed By: ot
    
    Differential Revision: D58530680
    
    fbshipit-source-id: bcec4dbc7f8a67498660b596bb41386f9dba5859

--- folly/synchronization/HazptrDomain.cpp
+++ folly/synchronization/HazptrDomain.cpp
@@ -14,13 +14,58 @@
  * limitations under the License.
  */
 
-#include <folly/executors/QueuedImmediateExecutor.h>
 #include <folly/synchronization/HazptrDomain.h>
+
+#include <cstddef>
+#include <functional>
+#include <queue>
+#include <type_traits>
+
+#include <folly/Executor.h>
+#include <folly/Indestructible.h>
+#include <folly/ScopeGuard.h>
+#include <folly/executors/InlineExecutor.h>
 
 namespace folly::detail {
 
+namespace {
+
+/// HazptrDefaultExecutor
+///
+/// Like QueuedImmediateExecutor, but:
+/// * Only a singleton and not otherwise instantiated.
+/// * Using keyword thread_local v.s. class ThreadLocal.
+struct HazptrDefaultExecutor final : InlineLikeExecutor {
+  using Func = Function<void()>;
+  using Queue = std::queue<Func>;
+
+  void add(Func func) override {
+    thread_local Queue* current;
+
+    std::optional<Queue> oqueue; // do not use this directly!
+    if (!current) {
+      current = &oqueue.emplace();
+    }
+    auto cleanup = makeGuard([&] { current = nullptr; });
+    auto& queue = *current; // use this directly!
+
+    queue.push(std::move(func));
+
+    if (queue.size() == 1) {
+      while (!queue.empty()) {
+        auto qfunc = std::ref(queue.front());
+        invokeCatchingExns("HazptrDefaultExecutor::run: ", qfunc);
+        queue.pop();
+      }
+    }
+  }
+};
+
+} // namespace
+
 folly::Executor::KeepAlive<> hazptr_get_default_executor() {
-  return &folly::QueuedImmediateExecutor::instance();
+  static Indestructible<HazptrDefaultExecutor> instance;
+  return &*instance;
 }
 
 } // namespace folly::detail
