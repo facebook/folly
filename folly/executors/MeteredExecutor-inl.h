8fc24a36c1878a846f376c4783ccba54584e1bd2
Giuseppe Ottaviano <ott@meta.com>
2024-06-25 10:29:06 -0700

    Generalize and optimize MeteredExecutor
    Summary:
    `MeteredExecutor`, as implemented, can introduce a noticeable concurrency bottleneck due to its serializing of the enqueue on the wrapped executor. Any work done in the worker before scheduling the next worker is in the critical path, so we need to make it as fast as possible.
    
    This currently includes dequeueing from the notification queue (note that `scheduleCallback()` happens after that). To reduce latency, instead, do all accounting operations on a single state word so we can schedule the next worker before dequeueing the task.
    
    Furthermore, the existing implementation ends up scheduling workers even when the queue is empty, wasting work on the wrapped executor: this is because of the condition `queue_.drive(consumer) || !queue_.arm()`, which is true whenever we processed a task, even if there is no other task in the queue. This behavior was somehow even sanctioned in the `CostOfMeteredExecutors` test, despite being undesirable. The new implementation correctly only schedules one work per submitted task by doing atomic accounting in the state word.
    
    Finally, reintroduce `maxReadAtOnce`, removed in D33112001, but renamed `maxInQueue` for more clarity. The new implementation makes it easier to support it, and benchmarks show that increasing it from the default 1 can significantly reduce the bottleneck on high-parallelism executors.
    
    Reviewed By: Gownta
    
    Differential Revision: D58999318
    
    fbshipit-source-id: 1e78855f603805ddc988eb55906c4755ca9ee376

--- folly/executors/MeteredExecutor-inl.h
+++ folly/executors/MeteredExecutor-inl.h
@@ -25,8 +25,8 @@ template <template <typename> class Atom>
 MeteredExecutorImpl<Atom>::MeteredExecutorImpl(
     KeepAlive keepAlive, Options options)
     : options_(std::move(options)), kaInner_(std::move(keepAlive)) {
-  queue_.setMaxReadAtOnce(1);
-  queue_.arm();
+  CHECK_GE(options_.maxInQueue, 1);
+  CHECK_LT(options_.maxInQueue, uint32_t(1) << 31);
 }
 
 template <template <typename> class Atom>
@@ -52,107 +52,142 @@ std::unique_ptr<QueueObserver> MeteredExecutorImpl<Atom>::setupQueueObserver() {
 }
 
 template <template <typename> class Atom>
+template <class F>
+void MeteredExecutorImpl<Atom>::modifyState(F f) {
+  uint64_t oldState = state_.load(std::memory_order_relaxed);
+  uint64_t newState;
+  do {
+    newState = f(oldState);
+    // Verify invariants: no more in-queue than allowed.
+    DCHECK_LE(newState >> kInQueueShift, options_.maxInQueue);
+    // No more in queue than pending tasks.
+    DCHECK_LE(newState >> kInQueueShift, newState & kSizeMask);
+  } while (!state_.compare_exchange_strong(
+      oldState,
+      newState,
+      std::memory_order_seq_cst,
+      std::memory_order_relaxed));
+}
+
+template <template <typename> class Atom>
 void MeteredExecutorImpl<Atom>::add(Func func) {
-  auto task = Task(std::move(func));
-  auto rctxp = RequestContext::saveContext();
+  auto task = Task(std::move(func), RequestContext::saveContext());
   if (queueObserver_) {
-    auto payload = queueObserver_->onEnqueued(rctxp.get());
+    auto payload = queueObserver_->onEnqueued(task.requestContext());
     task.setQueueObserverPayload(payload);
   }
-  if (queue_.push(std::move(rctxp), std::move(task))) {
-    scheduleCallback();
+
+  queue_.enqueue(std::move(task));
+
+  bool shouldScheduleWorker;
+  modifyState([&](uint64_t state) {
+    state += kSizeInc;
+    CHECK_NE(state & kSizeMask, 0)
+        << "Too many pending tasks in MeteredExecutor";
+    if (!(state & kPausedBit) &&
+        ((state >> kInQueueShift) < options_.maxInQueue)) {
+      state += kInQueueInc;
+      shouldScheduleWorker = true;
+    } else {
+      shouldScheduleWorker = false;
+    }
+    return state;
+  });
+
+  if (shouldScheduleWorker) {
+    scheduleWorker();
   }
 }
 
 template <template <typename> class Atom>
 bool MeteredExecutorImpl<Atom>::pause() {
-  auto state = state_.execState.load();
-  do {
-    DCHECK(state == PAUSED || state == PAUSED_PENDING || state == RUNNING);
-    if (state != RUNNING) {
-      return false;
-    }
-  } while (!state_.execState.compare_exchange_weak(state, PAUSED));
-  return true;
+  auto oldState = state_.fetch_or(kPausedBit, std::memory_order_relaxed);
+  return !(oldState & kPausedBit);
 }
 
 template <template <typename> class Atom>
 bool MeteredExecutorImpl<Atom>::resume() {
-  auto state = state_.execState.load();
-  do {
-    DCHECK(state == PAUSED || state == PAUSED_PENDING || state == RUNNING);
-    if (state == RUNNING) {
-      return false;
+  bool wasPaused = false;
+  size_t workersToSchedule = 0;
+  modifyState([&](uint64_t state) {
+    if (state & kPausedBit) {
+      wasPaused = true;
+    } else {
+      wasPaused = false;
+      return state;
     }
-  } while (!state_.execState.compare_exchange_weak(state, RUNNING));
-  if (state == PAUSED_PENDING) {
-    scheduleCallback();
+    // Workers may have aborted without consuming tasks, reschedule them.
+    auto curSize = state & kSizeMask;
+    auto curInQueue = state >> kInQueueShift;
+    DCHECK_LE(curInQueue, options_.maxInQueue);
+    DCHECK_LE(curInQueue, curSize);
+    workersToSchedule =
+        std::min(static_cast<uint64_t>(options_.maxInQueue), curSize) -
+        curInQueue;
+    state &= ~kPausedBit;
+    state += workersToSchedule * kInQueueInc;
+    return state;
+  });
+
+  if (!wasPaused) {
+    return false;
+  }
+
+  for (size_t i = 0; i < workersToSchedule; ++i) {
+    scheduleWorker();
   }
   return true;
 }
 
 template <template <typename> class Atom>
-bool MeteredExecutorImpl<Atom>::shouldSkip() {
-  uint8_t state = state_.execState.load();
-  // we should never have shouldSkip invoked twice once marked as callback
-  // pending
-  DCHECK(state == RUNNING || state == PAUSED);
-  if (FOLLY_UNLIKELY(state != RUNNING)) {
-    if (state_.execState.compare_exchange_strong(state, PAUSED_PENDING)) {
-      return true;
-    }
-  }
-  return state != RUNNING;
+void MeteredExecutorImpl<Atom>::Task::run() && {
+  folly::RequestContextScopeGuard rctxGuard{std::move(rctx_)};
+  invokeCatchingExns("MeteredExecutor", std::exchange(func_, {}));
 }
 
 template <template <typename> class Atom>
-void MeteredExecutorImpl<Atom>::loopCallback() {
-  DCHECK(state_.callbackScheduled.exchange(false));
-  if (shouldSkip()) {
+void MeteredExecutorImpl<Atom>::worker() {
+  bool shouldAbort = false;
+  bool shouldRescheduleWorker = false;
+  modifyState([&](uint64_t state) {
+    if (state & kPausedBit) {
+      shouldAbort = true;
+      shouldRescheduleWorker = false;
+    } else {
+      shouldAbort = false;
+      // More work to do than workers in queue, re-schedule the worker without
+      // changing the in-queue count.
+      shouldRescheduleWorker = (state & kSizeMask) > (state >> kInQueueShift);
+      DCHECK_GT(state & kSizeMask, 0);
+      state -= kSizeInc;
+    }
+    if (!shouldRescheduleWorker) {
+      state -= kInQueueInc;
+    }
+    return state;
+  });
+
+  if (shouldAbort) {
     return;
   }
-  Consumer consumer(*this);
-  if (queue_.drive(consumer) || !queue_.arm()) {
-    scheduleCallback();
+  if (shouldRescheduleWorker) {
+    scheduleWorker();
   }
-  consumer.executeIfNotEmpty();
+
+  Task task;
+  CHECK(queue_.try_dequeue(task));
+  std::move(task).run();
 }
 
 template <template <typename> class Atom>
-void MeteredExecutorImpl<Atom>::scheduleCallback() {
-  DCHECK(!state_.callbackScheduled.exchange(true));
-  folly::RequestContextScopeGuard g{std::shared_ptr<RequestContext>()};
-  kaInner_->add([self = getKeepAliveToken(this)] { self->loopCallback(); });
+void MeteredExecutorImpl<Atom>::scheduleWorker() {
+  folly::RequestContextScopeGuard rctxGuard{nullptr};
+  kaInner_->add([self = getKeepAliveToken(this)] { self->worker(); });
 }
 
 template <template <typename> class Atom>
 MeteredExecutorImpl<Atom>::~MeteredExecutorImpl() {
   joinKeepAlive();
-}
-
-template <template <typename> class Atom>
-MeteredExecutorImpl<Atom>::Consumer::~Consumer() {
-  DCHECK(!first_ || !first_->hasFunc());
-}
-
-template <template <typename> class Atom>
-void MeteredExecutorImpl<Atom>::Consumer::executeIfNotEmpty() {
-  if (first_) {
-    RequestContextScopeGuard guard(std::move(firstRctx_));
-    auto first = std::move(first_);
-    first->run();
-  }
-}
-
-template <template <typename> class Atom>
-void MeteredExecutorImpl<Atom>::Consumer::operator()(
-    Task&& task, std::shared_ptr<RequestContext>&& rctx) {
-  if (self_.queueObserver_) {
-    self_.queueObserver_->onDequeued(task.getQueueObserverPayload());
-  }
-  DCHECK(!first_);
-  first_ = std::make_optional<Task>(std::move(task));
-  firstRctx_ = std::move(rctx);
 }
 
 } // namespace detail
