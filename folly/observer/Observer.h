a2281893ec27378bfa057c488aac179d16fcbedd
Andrejs Krasilnikovs <andrejs@meta.com>
2024-07-02 17:33:38 -0700

    Comment on addCallback
    Reviewed By: Orvid
    
    Differential Revision: D59167705
    
    fbshipit-source-id: 4cca14a85c219e195a4e07f3b0ba216d194c55c3

--- folly/observer/Observer.h
+++ folly/observer/Observer.h
@@ -239,6 +239,10 @@ class Observer {
     return version < core_->getVersionLastChange();
   }
 
+  /**
+   * Add a callback to be called when the Observer is updated. The callback
+   * will be removed when the returned CallbackHandle is destroyed.
+   */
   [[nodiscard]] CallbackHandle addCallback(
       Function<void(Snapshot<T>)> callback) const;
 
