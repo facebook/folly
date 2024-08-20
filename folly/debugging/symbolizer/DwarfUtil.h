1e3d5d5770e60242d2e3ef13f3874ea7ac5a6e60
Orvid King <orvid@meta.com>
2024-08-20 14:03:09 -0700

    Remove the autodeps-skip in the symbolizers dwarf helpers
    Summary: The tooling keeps trying to remove this skip, so specify the right dependency manually so that it can be removed safely.
    
    Reviewed By: stanleyz510, skrueger
    
    Differential Revision: D61494099
    
    fbshipit-source-id: 5cb30f9bd4fbab2de3a3db7c653d174ae1620cb0

--- folly/debugging/symbolizer/DwarfUtil.h
+++ folly/debugging/symbolizer/DwarfUtil.h
@@ -26,7 +26,7 @@
 #include <folly/experimental/symbolizer/ElfCache.h>
 
 #if FOLLY_HAVE_DWARF && FOLLY_HAVE_ELF
-#include <dwarf.h>
+#include <dwarf.h> // @manual=fbsource//third-party/libdwarf:dwarf
 
 namespace folly {
 namespace symbolizer {
