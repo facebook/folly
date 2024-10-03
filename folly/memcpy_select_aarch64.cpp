46291715158e1961f87ac2942741f615cf9892a8
Michael van der Westhuizen <rmikey@meta.com>
2024-10-03 08:40:39 -0700

    Add optimised memcpy, memset and memmove for the aarch64 memops extension
    Summary:
    Newer Arm CPUs will expose a FEAT_MOPS, and on newer kernels this funcitonality will be exposed via the HWCAP2_MOPS bit in AT_HWCAPS2.
    
    Wire up support for the hardware-optimised memory operations ahead of silicon availability.
    
    Reviewed By: Gownta
    
    Differential Revision: D63754476
    
    fbshipit-source-id: f067dc2942e3f7715da8a0fe281149c0269fd913

--- folly/memcpy_select_aarch64.cpp
+++ folly/memcpy_select_aarch64.cpp
@@ -56,18 +56,40 @@
 
 #include <asm/hwcap.h> // @manual
 
+#if defined(__has_include)
+#if __has_include(<sys/ifunc.h>)
+#include <sys/ifunc.h>
+#endif
+#endif
+
+#if !defined(HWCAP2_MOPS)
+#define HWCAP2_MOPS (1UL << 43)
+#endif
+
 extern "C" {
 
 void* __folly_memcpy_aarch64(void* dst, const void* src, std::size_t size);
+void* __folly_memcpy_aarch64_mops(void* dst, const void* src, std::size_t size);
 void* __folly_memcpy_aarch64_simd(void* dst, const void* src, std::size_t size);
 void* __folly_memcpy_aarch64_sve(void* dst, const void* src, std::size_t size);
 
 void* __folly_memmove_aarch64(void* dst, const void* src, std::size_t len);
+void* __folly_memmove_aarch64_mops(void* dst, const void* src, std::size_t len);
 void* __folly_memmove_aarch64_simd(void* dst, const void* src, std::size_t len);
 void* __folly_memmove_aarch64_sve(void* dst, const void* src, std::size_t len);
 
+[[gnu::no_sanitize_address]]
 decltype(&__folly_memcpy_aarch64) __folly_detail_memcpy_resolve(
-    uint64_t hwcaps) {
+    uint64_t hwcaps, const void* arg2) {
+#if defined(_IFUNC_ARG_HWCAP)
+  if (hwcaps & _IFUNC_ARG_HWCAP && arg2 != nullptr) {
+    const __ifunc_arg_t* args = reinterpret_cast<const __ifunc_arg_t*>(arg2);
+    if (args->_hwcap2 & HWCAP2_MOPS) {
+      return __folly_memcpy_aarch64_mops;
+    }
+  }
+#endif
+
   if (hwcaps & HWCAP_SVE) {
     return __folly_memcpy_aarch64_sve;
   }
@@ -79,8 +101,18 @@ decltype(&__folly_memcpy_aarch64) __folly_detail_memcpy_resolve(
   return __folly_memcpy_aarch64;
 }
 
+[[gnu::no_sanitize_address]]
 decltype(&__folly_memmove_aarch64) __folly_detail_memmove_resolve(
-    uint64_t hwcaps) {
+    uint64_t hwcaps, const void* arg2) {
+#if defined(_IFUNC_ARG_HWCAP)
+  if (hwcaps & _IFUNC_ARG_HWCAP && arg2 != nullptr) {
+    const __ifunc_arg_t* args = reinterpret_cast<const __ifunc_arg_t*>(arg2);
+    if (args->_hwcap2 & HWCAP2_MOPS) {
+      return __folly_memmove_aarch64_mops;
+    }
+  }
+#endif
+
   if (hwcaps & HWCAP_SVE) {
     return __folly_memmove_aarch64_sve;
   }
