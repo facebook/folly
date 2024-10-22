a6ee2720f8054f03e73bdd435212c764f63def72
Yedidya Feldblum <yfeldblum@meta.com>
2024-10-22 08:26:41 -0700

    formatting for folly/algorithm/simd/detail/test/TraitsTest.cpp
    Reviewed By: Gownta
    
    Differential Revision: D64644006
    
    fbshipit-source-id: a04e329dc8d74660db93de60a297c3574d0d1c9f

--- folly/algorithm/simd/detail/test/TraitsTest.cpp
+++ folly/algorithm/simd/detail/test/TraitsTest.cpp
@@ -29,34 +29,45 @@ struct SimdTraitsTest : testing::Test {};
 namespace simd_friendly_equivalent_scalar_test {
 
 // ints
-static_assert(std::is_same_v<
-              std::int8_t,
-              simd_friendly_equivalent_scalar_t<signed char>>);
-static_assert(std::is_same_v<
-              std::uint8_t,
-              simd_friendly_equivalent_scalar_t<unsigned char>>);
+static_assert( //
+    std::is_same_v< //
+        std::int8_t,
+        simd_friendly_equivalent_scalar_t<signed char>>);
+static_assert( //
+    std::is_same_v<
+        std::uint8_t,
+        simd_friendly_equivalent_scalar_t<unsigned char>>);
 
-static_assert(
-    std::is_same_v<std::int16_t, simd_friendly_equivalent_scalar_t<short>>);
-static_assert(std::is_same_v<
-              std::uint16_t,
-              simd_friendly_equivalent_scalar_t<unsigned short>>);
+static_assert( //
+    std::is_same_v< //
+        std::int16_t,
+        simd_friendly_equivalent_scalar_t<short>>);
+static_assert( //
+    std::is_same_v<
+        std::uint16_t,
+        simd_friendly_equivalent_scalar_t<unsigned short>>);
 
-static_assert(
-    std::is_same_v<std::int32_t, simd_friendly_equivalent_scalar_t<int>>);
-static_assert(std::is_same_v<
-              std::uint32_t,
-              simd_friendly_equivalent_scalar_t<unsigned int>>);
+static_assert( //
+    std::is_same_v< //
+        std::int32_t,
+        simd_friendly_equivalent_scalar_t<int>>);
+static_assert( //
+    std::is_same_v<
+        std::uint32_t,
+        simd_friendly_equivalent_scalar_t<unsigned int>>);
 
-static_assert(std::is_same_v<
-              std::int64_t,
-              simd_friendly_equivalent_scalar_t<std::int64_t>>);
-static_assert(std::is_same_v<
-              std::uint64_t,
-              simd_friendly_equivalent_scalar_t<std::uint64_t>>);
+static_assert( //
+    std::is_same_v<
+        std::int64_t,
+        simd_friendly_equivalent_scalar_t<std::int64_t>>);
+static_assert( //
+    std::is_same_v<
+        std::uint64_t,
+        simd_friendly_equivalent_scalar_t<std::uint64_t>>);
 
 // floats
-static_assert(std::is_same_v<float, simd_friendly_equivalent_scalar_t<float>>);
+static_assert( //
+    std::is_same_v<float, simd_friendly_equivalent_scalar_t<float>>);
 static_assert(
     std::is_same_v<double, simd_friendly_equivalent_scalar_t<double>>);
 
@@ -75,9 +86,10 @@ static_assert( //
 
 // const
 
-static_assert(std::is_same_v<
-              const std::int32_t,
-              simd_friendly_equivalent_scalar_t<const int>>);
+static_assert( //
+    std::is_same_v<
+        const std::int32_t,
+        simd_friendly_equivalent_scalar_t<const int>>);
 
 // pointers
 
@@ -104,29 +116,34 @@ static_assert(!std::is_invocable_v<sfinae_call, NotSimdFriendly>);
 namespace as_simd_friendly_type_test {
 
 template <typename T>
-using asSimdFriendlyResult = std::invoke_result_t<AsSimdFriendlyFn, T>;
+using AsSimdFriendlyResult = std::invoke_result_t<AsSimdFriendlyFn, T>;
 
-static_assert(std::is_same_v<
-              folly::span<std::int32_t>,
-              asSimdFriendlyResult<folly::span<std::int32_t>>>);
+static_assert( //
+    std::is_same_v<
+        folly::span<std::int32_t>,
+        AsSimdFriendlyResult<folly::span<std::int32_t>>>);
 
-static_assert(std::is_same_v<
-              folly::span<std::int32_t>,
-              asSimdFriendlyResult<folly::span<int>>>);
+static_assert( //
+    std::is_same_v<
+        folly::span<std::int32_t>,
+        AsSimdFriendlyResult<folly::span<int>>>);
 
-static_assert(std::is_same_v<
-              folly::span<std::int32_t>,
-              asSimdFriendlyResult<std::vector<int>&>>);
+static_assert( //
+    std::is_same_v<
+        folly::span<std::int32_t>,
+        AsSimdFriendlyResult<std::vector<int>&>>);
 
-static_assert(std::is_same_v<
-              folly::span<const std::int32_t>,
-              asSimdFriendlyResult<const std::vector<int>&>>);
+static_assert( //
+    std::is_same_v<
+        folly::span<const std::int32_t>,
+        AsSimdFriendlyResult<const std::vector<int>&>>);
 
-static_assert(std::is_same_v<
-              folly::span<const double>,
-              asSimdFriendlyResult<const std::vector<double>&>>);
+static_assert( //
+    std::is_same_v<
+        folly::span<const double>,
+        AsSimdFriendlyResult<const std::vector<double>&>>);
 
-static_assert(std::is_same_v<double, asSimdFriendlyResult<double>>);
+static_assert(std::is_same_v<double, AsSimdFriendlyResult<double>>);
 
 static_assert(!std::is_invocable_v<AsSimdFriendlyFn, std::set<int>>);
 
@@ -135,30 +152,35 @@ static_assert(!std::is_invocable_v<AsSimdFriendlyFn, std::set<int>>);
 namespace as_simd_friendly_uint_type_test {
 
 template <typename T>
-using asSimdFriendlyUintResult = std::invoke_result_t<AsSimdFriendlyUintFn, T>;
+using AsSimdFriendlyUintResult = std::invoke_result_t<AsSimdFriendlyUintFn, T>;
 
-static_assert(std::is_same_v<
-              folly::span<std::uint32_t>,
-              asSimdFriendlyUintResult<folly::span<std::uint32_t>>>);
+static_assert( //
+    std::is_same_v<
+        folly::span<std::uint32_t>,
+        AsSimdFriendlyUintResult<folly::span<std::uint32_t>>>);
 
-static_assert(std::is_same_v<
-              folly::span<std::uint32_t>,
-              asSimdFriendlyUintResult<folly::span<std::int32_t>>>);
+static_assert( //
+    std::is_same_v<
+        folly::span<std::uint32_t>,
+        AsSimdFriendlyUintResult<folly::span<std::int32_t>>>);
 
-static_assert(std::is_same_v<
-              folly::span<std::uint32_t>,
-              asSimdFriendlyUintResult<folly::span<int>>>);
+static_assert( //
+    std::is_same_v<
+        folly::span<std::uint32_t>,
+        AsSimdFriendlyUintResult<folly::span<int>>>);
 
-static_assert(std::is_same_v<
-              folly::span<std::uint32_t>,
-              asSimdFriendlyUintResult<std::vector<int>&>>);
+static_assert( //
+    std::is_same_v<
+        folly::span<std::uint32_t>,
+        AsSimdFriendlyUintResult<std::vector<int>&>>);
 
-static_assert(std::is_same_v<
-              folly::span<const std::uint32_t>,
-              asSimdFriendlyUintResult<const std::vector<std::uint32_t>&>>);
+static_assert( //
+    std::is_same_v<
+        folly::span<const std::uint32_t>,
+        AsSimdFriendlyUintResult<const std::vector<std::uint32_t>&>>);
 
 static_assert(
-    std::is_same_v<std::uint32_t, asSimdFriendlyUintResult<std::int32_t>>);
+    std::is_same_v<std::uint32_t, AsSimdFriendlyUintResult<std::int32_t>>);
 
 static_assert(
     !std::is_invocable_v<AsSimdFriendlyUintFn, const std::vector<double>&>);
