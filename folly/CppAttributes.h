0b61d58796825c6643e98787a57ed2981faef001
Michael Ranieri <mranieri@meta.com>
2024-12-13 14:05:10 -0800

    support msvc::no_unique_address
    Summary: https://en.cppreference.com/w/cpp/language/attributes/no_unique_address
    
    Reviewed By: Gownta
    
    Differential Revision: D67054400
    
    fbshipit-source-id: 07609ac5227baa20bae4f3100d13bd1039358d76

--- folly/CppAttributes.h
+++ folly/CppAttributes.h
@@ -118,6 +118,8 @@
  */
 #if FOLLY_HAS_CPP_ATTRIBUTE(no_unique_address)
 #define FOLLY_ATTR_NO_UNIQUE_ADDRESS no_unique_address
+#elif FOLLY_HAS_CPP_ATTRIBUTE(msvc::no_unique_address)
+#define FOLLY_ATTR_NO_UNIQUE_ADDRESS msvc::no_unique_address
 #else
 #define FOLLY_ATTR_NO_UNIQUE_ADDRESS
 #endif
