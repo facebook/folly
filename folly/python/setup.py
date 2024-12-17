14375463791da59385b60c342f13a8d0f46cccf8
G S Marzot <gmarzot@marzresearch.net>
2024-12-16 19:15:42 -0500

    remove archaic DESTDIR, inject library paths from CMAKE variables, call install with PYTHONPATH set

--- folly/python/setup.py
+++ folly/python/setup.py
@@ -18,41 +18,64 @@
 # Cython requires source files in a specific structure, the structure is
 # created as tree of links to the real source files.
 
+import sys, platform
 from Cython.Build import cythonize
 from Cython.Compiler import Options
 from setuptools import Extension, setup
+import os
+import json
 
 Options.fast_fail = True
+
+if sys.platform == "darwin" and platform.machine() == "arm64":
+    # Macos (arm64, homebrew path is different than intel)
+    library_dirs = ["/opt/homebrew/lib"]
+else:
+    # Debian/Ubuntu
+    library_dirs = ["/usr/lib", "/usr/lib/x86_64-linux-gnu"]
+    
+# Add library paths from CMAKE_PREFIX_PATH
+if cmake_prefix_path := os.environ.get("CMAKE_PREFIX_PATH"):
+    library_dirs.extend(p + "/lib" for p in cmake_prefix_path.split(":"))
 
 exts = [
     Extension(
         "folly.executor",
-        sources=["folly/executor.pyx","folly/executor_intf.cpp"],
+        sources=[
+            "folly/executor.pyx",
+            "folly/executor_intf.cpp",
+            "folly/ProactorExecutor.cpp",
+            "folly/error.cpp",
+        ],
         libraries=["folly", "glog", "double-conversion"],
         extra_compile_args=["-std=c++20"],  # C++20 for coroutines
         include_dirs=[".", "../.."],  # cython generated code
-        library_dirs=[
-            "/usr/lib",
-            "/usr/lib/x86_64-linux-gnu"  # Common location on Debian/Ubuntu
-        ],
+        library_dirs=library_dirs,
     ),
     Extension(
         "folly.iobuf",
-        sources=["folly/iobuf.pyx","folly/iobuf_intf.cpp"],
-        libraries=["folly", "glog", "double-conversion"],
+        sources=[
+            "folly/iobuf.pyx",
+            "folly/iobuf_intf.cpp",
+            "folly/iobuf_ext.cpp",
+            "folly/error.cpp",
+        ],
+        libraries=["folly", "glog", "double-conversion", "fmt"],
         extra_compile_args=["-std=c++20"],  # C++20 for coroutines
         include_dirs=[".","../.."],  # cython generated code
-        library_dirs=[
-            "/usr/lib",
-            "/usr/lib/x86_64-linux-gnu"  # Common location on Debian/Ubuntu
-        ],
+        library_dirs=library_dirs,
     ),
     Extension(
         "folly.fiber_manager",
-        sources=["folly/fiber_manager.pyx","folly/fibers.cpp"],
-        libraries=["folly", "glog", "double-conversion"],
+        sources=[
+            "folly/fiber_manager.pyx",
+            "folly/fibers.cpp",
+            "folly/error.cpp",
+        ],
+        libraries=["folly", "glog", "double-conversion", "fmt", "event"],
         extra_compile_args=["-std=c++20"],  # C++20 for coroutines
         include_dirs=[".","../.."],  # cython generated code
+        library_dirs=library_dirs,
     )
 ]
 
