94815b27a57aed95550c38dd33f349209ad09b90
Yedidya Feldblum <yfeldblum@meta.com>
2025-01-23 11:51:20 -0800

    cut legacy backcompat alias folly::io::COMPRESSION_LEVEL_DEFAULT
    Summary: All uses have been migrated so the unused alias may now be removed.
    
    Reviewed By: skrueger
    
    Differential Revision: D68529340
    
    fbshipit-source-id: e69c9225399c6cbd6afee478951d14dade1f19df

--- folly/compression/Compression.h
+++ folly/compression/Compression.h
@@ -543,7 +543,6 @@ bool hasStreamCodec(CodecType type);
 namespace folly::io {
 using folly::compression::Codec;
 using folly::compression::CodecType;
-using folly::compression::COMPRESSION_LEVEL_DEFAULT;
 using folly::compression::getAutoUncompressionCodec;
 using folly::compression::getCodec;
 using folly::compression::getStreamCodec;
