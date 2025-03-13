505e6efc24a05226987fd3564b757a28c79359e5
Yedidya Feldblum <yfeldblum@meta.com>
2025-03-13 14:08:35 -0700

    fix test failures from missing symbols in ElfTest
    Summary: The toolchain did not see that these symbols were used, since they were only accessed via dynamic lookup and not in any way that the toolchain can track. Mark them as `used` for the compiler and `retain` for the linker.
    
    Reviewed By: nslingerland
    
    Differential Revision: D71060303
    
    fbshipit-source-id: 558cc118fa37a2712d09b60251a94b638a3f01dd

--- folly/debugging/symbolizer/test/ElfTest.cpp
+++ folly/debugging/symbolizer/test/ElfTest.cpp
@@ -27,13 +27,13 @@ using folly::symbolizer::ElfFile;
 
 // Add some symbols for testing. Note that we have to be careful with type
 // signatures here to prevent name mangling
-uint64_t kIntegerValue = 1234567890ULL;
-const char* kStringValue = "coconuts";
 extern "C" {
-int sum_func(int lhs, int rhs) {
+[[gnu::used, gnu::retain]] uint64_t kIntegerValue = 1234567890ULL;
+[[gnu::used, gnu::retain]] const char* kStringValue = "coconuts";
+[[gnu::noinline, gnu::used, gnu::retain]] int sum_func(int lhs, int rhs) {
   return lhs + rhs;
 }
-int sub_func(int lhs, int rhs) {
+[[gnu::noinline, gnu::used, gnu::retain]] int sub_func(int lhs, int rhs) {
   return lhs - rhs;
 }
 }
