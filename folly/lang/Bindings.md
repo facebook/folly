7e294af340887e42b97f496bbe4a8f4d13770884
Alexey Spiridonov <lesha@meta.com>
2025-03-17 23:57:00 -0700

    Safer, more customizable arg bindings
    Summary:
    Don't worry, the implementation is under 300 lines. The rest is docs & code.
    
    `Bindings.md` cover the general motivation for this mechanism. It's in in `folly/lang` rather than`folly/coro/safe` owing to its multiple applications. Specifically:
     - I use it up-stack for `async_closure()` argument handling. The fact that it's extensible  makes it very easy to integrate "standard" verbs like `constant()` with closure-specific things like `as_capture()`.
     - `lang/named/` implementation -- I haven't updated my "named tuple" / "named captures" implementation yet, but D63299799 shows that keying bindings by types (including literal strings) is very easy, and composes as one would want.
     - Future: `async_closure_named()` -- will be lightweight sugar combining `named/` lexical scopes with `async_closure()`. Essentially, this gives full Python-style kwarg support for any functions that want it.
    
    Reviewed By: ispeters
    
    Differential Revision: D63299787
    
    fbshipit-source-id: 418c0c2d769bd7175abf11d90be1fb19bd1cb692

---
+++ folly/lang/Bindings.md
@@ -0,0 +1,240 @@
+# Binding API inputs to storage with `folly/lang/Bindings.h`
+
+## User guide
+
+Are you trying to call a `folly::bindings`-enabled API?  For simple usage,
+you should not need to read this file at all!  Read the API docs instead.
+
+NEVER write functions that pass `folly::bindings` helper types (`constant`,
+`by_ref`, `bound_args`, etc) by reference.  These immovable objects must
+only exist in the statement that constructed them.
+
+The high-level idea of `folly::bindings` is to offer **the caller** a
+vocabulary to describe the storage types to be used by the callee.
+
+For example, if a callee wants to store a generic tuple, the caller may
+write `constant(5), by_ref(b, c, non_constant(d))` to define the storage as
+`std::tuple<const int, const int&, const int&, int&>{5, b, c, d}`.
+
+Your specific API's docs are authoritative -- a callee can change modifier
+meanings, or define new ones.  That said, suggested modifier semantics are:
+  - Pass non-movable, non-copyable types via `make_in_place<T>()` or
+    `make_in_place_with(fn)`.
+  - The API decides whether `by_ref` / `by_non_const_ref` are supported.
+    If NOT, then the callee's signature decides between by-value & by-ref.
+      * Using `by_ref` should cause a compile error (`static_assert`).
+    If YES, they tell the callee to take the argument by reference. Then:
+      * WATCH OUT: `by_ref` defaults to `const`, unlike standard C++.
+        Rationale: Programming best practice is to minimize "implicit"
+        mutation.  When writing modern C++, two argument passing styles
+        predominate: `T` -- you own this, and `const T&` -- a read-only
+        input.  Any mutable "output" references, like `T&`, ought to be
+        plainly visible at the callsite to avoid bugs, but unfortunately C++
+        doesn't support this.  `by_ref` remedies this flaw.
+      * Passing a variable without modifiers will be pass-by-value.
+  - Unlike `std::as_const`, which changes the `const`ness of the input,
+    `{non_,}constant` say whether the storage should be made `const`.
+    That is, `constant` is like `const T var` in the callee's signature.
+      * `constant(std::move(var))` moves in the value, and stores it as `const`.
+      * `non_constant` never removes a `const` qualifier from the underlying
+        data.  Rather, it can override the "references default to `const`"
+        behavior, or to override a `constant` modifier that it surrounds.
+
+## Library authors only
+
+### What is this for?
+
+You are developing a new API, and need your caller to ergonomically pass a
+list of arguments to be stored in your callee code.  Today's options are:
+  - Pass regular arguments
+  - Pass a struct
+  - Pass a tuple, potentially as `auto`
+  - Pass a callable that constructs a tuple or a struct
+
+These options have tradeoffs, but none provide ALL of these features:
+  - Easy in-place construction
+  - Let the caller set the target storage (value or ref, const or not) for
+    the binding, with the callee just specifying just `auto`.  Consequences:
+      * The callee can reflect on the supplied args, without C++26 P2996
+      * `folly::bindings::constant` lets you move a non-`const` object into
+        `const` storage, while `std::as_const` cannot.
+  - Define custom binding logic for some args, like `as_capture` in
+    `async_closure`, or named arguments in `folly/lang/named`.
+  - Allow helper functions to return several adjacent arguments -- as if
+    your helpers could return an argument pack, without needing to pack them
+    in tuples & having the consumer recursively `std::tuple_cat`.
+
+`folly::bindings` addresses all of the above.  It is a customizable tool to
+uniformly bind:
+  - input references & in-place constructors
+  - to reference or value storage inside your API.
+
+### When might you use it?
+
+Consider this `folly::bindings` expression:
+
+  bound_args ba{5, constant{a}, by_ref{non_constant{b, std::move(c)}, d}};
+
+Stored with the default `binding_policy`, this is akin to:
+
+  std::tuple<int, const A, B&, C&&, const D&> tup =
+      std::forward_as_tuple{5, a, b, std::move(c), d};
+
+Note that `folly::bindings` shows one non-standard behavior above --
+`by_ref` emits a `const` reference unless overridden.
+
+Regular C++ arguments are fine (and preferred!) when the destination types
+are known in advance, and the types are movable.  But, in trickier cases
+`folly::bindings` saves the day:
+  - It lets the caller ergonomically declare a structure at the same time it
+    is constructed or passed (`async_closure`, named scopes).
+  - In order to in-place construct an immovable type `A` by-value inside the
+    caller's storage, you need an implicit conversion operator.
+    `folly::bindings::make_in_place*` implements one on your behalf.
+  - API-specific binding customizations can be provided via helper classes.
+    For example, `folly/lang/named` enables kwargs support.
+
+### What changes in the UX over standard C++ bindings?
+
+That depends on how you integrate `folly::bindings`, but...  the recommended
+way is to use the standard `binding_policy`, possibly extending it in a
+careful way.  The goal should be "low user surprisal", so we stay close to
+standard C++ semantics, except for a couple of restrictions to make argument
+passing less bug-prone.
+
+With the vanilla `binding_policy`, besides `make_in_place*` support, you get:
+  - Arguments are bound by value, unless the callsite includes `by_ref`.
+    This should reduce bugs related to aliasing the caller doesn't expect.
+  - Reference args default to `const`.
+  - While `copy{}` and `move{}` modifiers aren't yet provided, `category_t`
+    and `binding_policy` allow for the notion of the caller restricting that
+    an argument be passed by copy, or move-copy.
+
+In "synchronous" APIs, where your code only runs while the user's original
+statement is active, another viable integration is to define a policy that
+defaults to bind-by-reference.  This policy could be added to `Bindings.h`.
+
+### To integrate `Bindings.h`, take `bound_args` via CTAD in an immovable class
+
+The simple way to make a `folly::bindings` API is to take one `bound_args`:
+
+```cpp
+template <typename T>
+void foo(bound_args<T> args) {
+  // Read "Using your bound args" for how to access `args`
+}
+// User code
+foo(bound_args{5, constant(make_in_place<Bar>(x), std::move(y))});
+```
+
+#### Alternative: API with no user-visible `bound_args`
+
+If the one-`bound_args` pattern does not work for you, there's another way.
+Before going down this road, read this section *carefully*.  Needing to depend
+on CTAD limits your template deduction capabilities, and forces you to
+implement each API method as a class.
+
+In `Bindings.h`, all the modifiers (like `constant`) look similar to this:
+
+```cpp
+template <typename... Ts>
+class YOUR_TYPE : private bound_args<Ts...> { // (1)
+  // (2) -- the ctor must take `bound_args<Ts>...` **by value**.
+  using bound_args<Ts...>::bound_args;
+};
+template <typename... Ts>
+YOUR_TYPE(Ts&&...) -> YOUR_TYPE<ext::deduce_bound_args_t<Ts>...>; // (3)
+```
+
+Your API's implementation could follow a similar pattern, so you should
+understand how (1-3) work together to bind the input arguments.  Afterwards,
+I will describe how to add your business logic.
+
+(1) Deriving from a `NonCopyableNonMovable` base (`bound_args`) encourages
+users to pass the outer type **only** via prvalue semantics.  This is
+required in order to minimize lifetime bugs.  These "binding" types
+typically store references, which (thanks to lifetime extension) are valid
+in the current statement, but can easily become invalid later.  Insistiing
+on pass-by-prvalue makes it harder for the user to accidentally write
+lifetime bugs.  Why go this far to prevent pass-by-move?  Implementation
+experience shows that assigning a binding type to a named variable feels
+"natural", but using it later can cause bugs that are hard to spot.  For one
+example, see the `#if 0` in the test under `all_tests_run_at_build_time`.
+
+Notes:
+  - `private` inheritance prevents `YOUR_TYPE` from being nested inside
+    binding modifiers like `constant()`.  You can relax to `public`, but
+    only if your type has the inherited ctor shown above, and can logically
+    be thought of as a bag of args.
+  - A linter is proposed, but not yet implemented, for detecting cases
+    where `Bindings.h` helpers are being taken by-reference.  With this
+    linter, the prvalue-only protection against lifetime bugs will be robust.
+
+(2) Prvalue semantics only allows us to take arguments by-value.  C++20
+lacks perfect forwarding for prvalues, and there is not even an accepted
+proposal for future releases (though P2785 would help).  Yet, in generic
+code, we want to allow packs of arguments where some are binding helpers
+(like `constant(5)`) and others are perfect-forwarded references (like `x`).
+Without prvalue perfect forwarding, the next best trick is to implicitly
+convert every arg to a `bound_args<T>` value, as done by this ctor.
+  - If the argument type `T` derives from `like_bound_args`, we wrap it
+    in `struct bound_args<T> : T`, moving the underlying data via the
+    implementation-detail `bound_args_unsafe_move` protocol.  This handles the
+    case when the user passes a modifier like `constant(5)` as an arg.
+  - For all other `T`, `bound_args<T>` simply captures a forwarding
+    reference to the input.
+
+IMPORTANT:
+  - To be lifetime-safe, this ctor takes the `bound_args<Ts>` by-value.
+  - Do **NOT** assume that `Ts` are your input types.  A single `bound_args`
+    may represent a sequence of arguments to bind (e.g.  `constant(5, x)`,
+    and the base class `bound_args<Ts...>` takes care of flattening these
+    for you.  The usage is explained below.
+
+(3) The deduction guide is necessary so that the class's ctor (2) can
+implicitly convert each argument into a `bound_args<T>` value.  In other
+words, it arranges for `T` to record the type of the argument as provided.
+
+Once the base `bound_arg<Ts...>` is constructed, you can access the
+**flattened** bound args:
+  - The "input" and "storage" types for each argument via the class template
+    `binding_policy`, and member type list `binding_list_t`.
+  - The argument-to-store via the `unsafe_tuple_to_bind()` method.  Each
+    tuple entry is either a reference, or a type that is implicitly
+    convertible to the storage type.  In order to use this value, you
+    **must** coerce each entry to the policy-computed storage type.  The
+    method's `unsafe_` prefix signals that it is only lifetime-safe when
+    called within the statement that instantiated your class.
+
+#### Using the bound args in your business logic
+
+There are two natural choices for **when** to run your business logic:
+
+  - With the immovable `YOUR_TYPE` shown above, you can think of
+    the type as an ephemeral "list of bound args", and provide one or more
+    methods (or even `operator()`) to operate on the args.  The methods
+    should be `&&`-qualified (i.e. destructive) to prevent binding reuse.
+    For example, `AsyncClosure.h` follows this pattern.
+
+  - If `YOUR_TYPE` can represent or store the API's result, you could run
+    the business logic in the ctor directly.  This is not a commonly
+    recommendable pattern, since error handling gets trickier (either your
+    class models both value & error states, or your ctor throws).  However,
+    it can give a simpler UX.  Implementing this requires some adjustments:
+      - Stop inheriting from `bound_args`, since you would no longer want to
+        store the bound args tuple, but only the API's result.
+      - Instead, lightly refactor `Bindings.h` to expose the
+        argument-flattening logic by composition.
+      - So long as it doesn't store any input references, `YOUR_TYPE` can
+        now be movable or copyable.
+
+Here is a method for `YOUR_TYPE` that stores the args as a tuple of values:
+
+```cpp
+auto store_tuple() && {
+  return [&]<typename... Bs>(tag<Bs...>) {
+    return std::tuple<binding_policy<Bs>::storage_type...>{
+      std::move(*this)->unsafe_tuple_to_bind()};
+  }(bound_args<Ts...>::binding_list_t{});
+}
+```
---
+++ folly/lang/test/BindingsTest.cpp
@@ -0,0 +1,466 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <folly/lang/Bindings.h>
+#include <folly/portability/GTest.h>
+
+#ifndef _WIN32 // Explained in `Bindings.h`
+
+namespace folly::bindings::detail {
+
+using namespace folly::bindings::ext;
+
+struct Foo : folly::NonCopyableNonMovable {
+  constexpr explicit Foo(bool* made, int n) : n_(n) {
+    if (made) {
+      *made = true;
+    }
+  }
+  int n_;
+};
+
+// This is here so that test "runs" show up in CI history
+TEST(BindingsTest, all_tests_run_at_build_time) {
+  // This is a manually-enabled example of the `lifetimebound` annotation on
+  // `in_place_bound_args::unsafe_tuple_to_bind()`.  With `lifetimebound` it
+  // won't compile, without it would hit an ASAN failure.  It has to be a
+  // runtime test because `constexpr` evaluation detects usage of dangling
+  // references regardless of `lifetimebound`..
+#if 0
+  int n = 1337;
+  bool made = false;
+  auto fooMaker = make_in_place<Foo>(&made, n).unsafe_tuple_to_bind();
+  // UNSAFE: `fooMaker` contains a ref to the prvalue `&made`, which became
+  // invalid at the `;` of the previous line.
+  lite_tuple::tuple<Foo> foo = std::move(fooMaker);
+  EXPECT_TRUE(made);
+  EXPECT_EQ(1337, std::get<0>(foo).n_);
+  FAIL() << "Should not compile, or at least fail under ASAN.";
+#endif
+}
+
+// Better UX than `assert()` in constexpr tests.
+constexpr void test(bool ok) {
+  if (!ok) {
+    throw std::exception(); // Throwing in constexpr code is a compile error
+  }
+}
+
+constexpr auto check_ref_bound_args() {
+  int y = 5;
+  static_assert(std::is_same_v<decltype(bound_args{5}), bound_args<int&&>>);
+  {
+    auto lval = bound_args(y);
+    static_assert(std::is_same_v<decltype(lval), bound_args<int&>>);
+    static_assert(
+        std::is_same_v<
+            decltype(lval)::binding_list_t,
+            tag_t<binding_t<bind_info_t{}, int&>>>);
+    y += 20;
+    static_assert(
+        std::is_same_v<
+            decltype(std::move(lval).unsafe_tuple_to_bind()),
+            lite_tuple::tuple<int&>>);
+    test(25 == lite_tuple::get<0>(std::move(lval).unsafe_tuple_to_bind()));
+  }
+  {
+    auto rval = bound_args(std::move(y));
+    static_assert(std::is_same_v<decltype(rval), bound_args<int&&>>);
+    static_assert(
+        std::is_same_v<
+            decltype(rval)::binding_list_t,
+            tag_t<binding_t<bind_info_t{}, int&&>>>);
+    y -= 10;
+    static_assert(
+        std::is_same_v<
+            decltype(std::move(rval).unsafe_tuple_to_bind()),
+            lite_tuple::tuple<int&&>>);
+    test(15 == lite_tuple::get<0>(std::move(rval).unsafe_tuple_to_bind()));
+  }
+  return true;
+}
+
+static_assert(check_ref_bound_args());
+
+constexpr auto check_nested_bound_args() {
+  int b = 2, d = 4;
+  using FlatT =
+      decltype(bound_args{1.2, bound_args{b, 'x'}, d, bound_args{}, "abc"});
+  static_assert(
+      std::is_same_v<
+          FlatT,
+          bound_args<
+              double&&,
+              bound_args<int&, char&&>,
+              int&,
+              bound_args<>,
+              const char(&)[4]>>);
+  constexpr auto BI = bind_info_t{};
+  static_assert(
+      std::is_same_v<
+          FlatT::binding_list_t,
+          tag_t<
+              binding_t<BI, double&&>,
+              binding_t<BI, int&>,
+              binding_t<BI, char&&>,
+              binding_t<BI, int&>,
+              binding_t<BI, const char(&)[4]>>>);
+  return true;
+}
+
+static_assert(check_nested_bound_args());
+
+constexpr auto check_const_and_non_const() {
+  double b = 2.3;
+
+  using non_const_bas = decltype(non_constant(1, bound_args(b, 'c')));
+  static_assert(
+      std::is_same_v<
+          non_const_bas,
+          non_constant<int&&, bound_args<double&, char&&>>>);
+  constexpr bind_info_t def_non_const_bi{
+      category_t{}, constness_t::non_constant};
+  static_assert(
+      std::is_same_v<
+          non_const_bas::binding_list_t,
+          tag_t<
+              binding_t<def_non_const_bi, int&&>,
+              binding_t<def_non_const_bi, double&>,
+              binding_t<def_non_const_bi, char&&>>>);
+
+  using const_bas = decltype(constant{non_constant{1, bound_args{b, 'c'}}});
+  static_assert(
+      std::is_same_v<
+          const_bas,
+          constant<non_constant<int&&, bound_args<double&, char&&>>>>);
+  constexpr bind_info_t def_const_bi{category_t{}, constness_t::constant};
+  static_assert(
+      std::is_same_v<
+          const_bas::binding_list_t,
+          tag_t<
+              binding_t<def_const_bi, int&&>,
+              binding_t<def_const_bi, double&>,
+              binding_t<def_const_bi, char&&>>>);
+
+  static_assert(
+      std::is_same_v<
+          decltype(constant(b))::binding_list_t,
+          tag_t<binding_t<def_const_bi, double&>>>);
+
+  static_assert(
+      std::is_same_v<
+          decltype(constant(non_constant(b)))::binding_list_t,
+          tag_t<binding_t<def_const_bi, double&>>>);
+
+  static_assert(
+      std::is_same_v<
+          decltype(non_constant(b))::binding_list_t,
+          tag_t<binding_t<def_non_const_bi, double&>>>);
+
+  static_assert(
+      std::is_same_v<
+          decltype(non_constant(constant(b)))::binding_list_t,
+          tag_t<binding_t<def_non_const_bi, double&>>>);
+
+  return true;
+}
+
+static_assert(check_const_and_non_const());
+
+constexpr auto check_by_ref() {
+  double b = 2.3;
+
+  using ref = decltype(by_ref{1, bound_args{b, 'c'}});
+  static_assert(
+      std::is_same_v<ref, by_ref<int&&, bound_args<double&, char&&>>>);
+  constexpr bind_info_t ref_def_bi{category_t::ref, constness_t{}};
+  static_assert(
+      std::is_same_v<
+          ref::binding_list_t,
+          tag_t<
+              binding_t<ref_def_bi, int&&>,
+              binding_t<ref_def_bi, double&>,
+              binding_t<ref_def_bi, char&&>>>);
+
+  using non_constant_ref =
+      decltype(non_constant{by_ref{1, bound_args{b, 'c'}}});
+  static_assert(
+      std::is_same_v<
+          non_constant_ref,
+          non_constant<by_ref<int&&, bound_args<double&, char&&>>>>);
+  constexpr bind_info_t ref_non_const_bi{
+      category_t::ref, constness_t::non_constant};
+  using non_const_bindings = tag_t<
+      binding_t<ref_non_const_bi, int&&>,
+      binding_t<ref_non_const_bi, double&>,
+      binding_t<ref_non_const_bi, char&&>>;
+  static_assert(
+      std::is_same_v<non_constant_ref::binding_list_t, non_const_bindings>);
+
+  using non_const_ref = decltype(by_non_const_ref{1, bound_args{b, 'c'}});
+  static_assert(
+      std::is_same_v<
+          non_const_ref,
+          by_non_const_ref<int&&, bound_args<double&, char&&>>>);
+  static_assert(
+      std::is_same_v<non_const_ref::binding_list_t, non_const_bindings>);
+
+  constexpr bind_info_t ref_const_bi{category_t::ref, constness_t::constant};
+  static_assert(
+      std::is_same_v<decltype(constant(by_ref(b))), constant<by_ref<double&>>>);
+  static_assert(
+      std::is_same_v<
+          decltype(constant(by_ref(b)))::binding_list_t,
+          tag_t<binding_t<ref_const_bi, double&>>>);
+  static_assert(
+      std::is_same_v<decltype(by_ref(constant(b))), by_ref<constant<double&>>>);
+  static_assert(
+      std::is_same_v<
+          decltype(by_ref(constant(b)))::binding_list_t,
+          tag_t<binding_t<ref_const_bi, double&>>>);
+
+  using bind_ref_non_const = tag_t<binding_t<ref_non_const_bi, double&>>;
+  static_assert(
+      std::is_same_v<
+          decltype(by_non_const_ref(b))::binding_list_t,
+          bind_ref_non_const>);
+  static_assert(
+      std::is_same_v<
+          decltype(non_constant(by_ref(b)))::binding_list_t,
+          bind_ref_non_const>);
+  static_assert(
+      std::is_same_v<
+          decltype(by_ref(non_constant(b)))::binding_list_t,
+          bind_ref_non_const>);
+
+  return true;
+}
+
+static_assert(check_by_ref());
+
+constexpr auto check_in_place_bound_args_one_line() {
+  bool made = false;
+
+  static_assert(
+      1 ==
+      std::tuple_size_v<
+          decltype(make_in_place<Foo>(&made, 37).unsafe_tuple_to_bind())>);
+
+  // Binding prvalues is ok since `Foo` is constructed in the same statement.
+  Foo foo =
+      lite_tuple::get<0>(make_in_place<Foo>(&made, 37).unsafe_tuple_to_bind());
+  test(made);
+  test(foo.n_ == 37);
+
+  int n = 3;
+  Foo f2 =
+      lite_tuple::get<0>(make_in_place<Foo>(nullptr, n).unsafe_tuple_to_bind());
+  ++n;
+  test(3 == f2.n_);
+  test(4 == n);
+
+  return true;
+}
+
+static_assert(check_in_place_bound_args_one_line());
+constexpr auto check_in_place_bound_args_step_by_step() {
+  bool made = false;
+
+  // These vars can't be prvalues since the `Foo` ctor is delayed.
+  bool* made_ptr = &made;
+  int n = 37;
+
+  // Not a prvalue due to [[clang::lifetimebound]] on `what_to_bind()`.
+  auto b = make_in_place<Foo>(made_ptr, n);
+  static_assert(
+      std::is_same_v<decltype(b), in_place_bound_args<Foo, bool*&, int&>>);
+  auto [fooMaker] = std::move(b).unsafe_tuple_to_bind();
+  test(!made);
+
+  Foo foo = std::move(fooMaker);
+  test(made);
+  test(foo.n_ == n);
+
+  return true;
+}
+
+static_assert(check_in_place_bound_args_step_by_step());
+
+// NB: These signatures are NOT meant to be user-visible.
+constexpr auto check_in_place_bound_args_type_sig() {
+  static_assert(
+      std::is_same_v<
+          decltype(make_in_place<Foo>(nullptr, 7)),
+          in_place_bound_args<Foo, std::nullptr_t, int>>);
+
+  int n = 7;
+  static_assert(
+      std::is_same_v<
+          decltype(make_in_place<Foo>(nullptr, n)),
+          in_place_bound_args<Foo, std::nullptr_t, int&>>);
+
+  // Composes with projection modifiers as expected
+  using const_in_place = decltype(constant(make_in_place<Foo>(nullptr, 7)));
+  static_assert(
+      std::is_same_v<
+          const_in_place,
+          constant<in_place_bound_args<Foo, std::nullptr_t, int>>>);
+  constexpr bind_info_t const_bi{category_t{}, constness_t::constant};
+  static_assert(
+      std::is_same_v<
+          const_in_place::binding_list_t,
+          tag_t<binding_t<const_bi, Foo>>>);
+
+  return true;
+}
+
+static_assert(check_in_place_bound_args_type_sig());
+
+constexpr auto check_in_place_bound_args_via_fn() {
+  // Test for issues with prvalue lambdas
+  Foo f1 = lite_tuple::get<0>(
+      make_in_place_with([]() {
+        return Foo{nullptr, 17};
+      }).unsafe_tuple_to_bind());
+  test(17 == f1.n_);
+
+  auto fn = []() { return Foo{nullptr, 37}; };
+  auto b2 = make_in_place_with(fn);
+  static_assert(
+      std::is_same_v<decltype(b2), in_place_fn_bound_args<Foo, decltype(fn)>>);
+  static_assert(
+      std::is_same_v<
+          decltype(b2)::binding_list_t,
+          tag_t<binding_t<bind_info_t{}, Foo>>>);
+  static_assert(
+      1 == std::tuple_size_v<decltype(std::move(b2).unsafe_tuple_to_bind())>);
+  Foo f2 = lite_tuple::get<0>(std::move(b2).unsafe_tuple_to_bind());
+  test(37 == f2.n_);
+
+  return true;
+}
+
+static_assert(check_in_place_bound_args_via_fn());
+
+constexpr auto check_in_place_bound_args_modifier_distributive_property() {
+  constexpr bind_info_t def_non_const_bi{
+      category_t{}, constness_t::non_constant};
+  constexpr bind_info_t ref_non_const_bi{
+      category_t::ref, constness_t::non_constant};
+  using expected_binding_list = tag_t<
+      binding_t<def_non_const_bi, bool&&>,
+      binding_t<ref_non_const_bi, double&>,
+      binding_t<def_non_const_bi, int>,
+      binding_t<ref_non_const_bi, char&&>>;
+
+  double b = 2;
+  static_assert(
+      std::is_same_v<
+          expected_binding_list,
+          decltype(non_constant(
+              true, by_ref(b), make_in_place<int>(3), by_ref('c')))::
+              binding_list_t>);
+  static_assert(
+      std::is_same_v<
+          expected_binding_list,
+          decltype(non_constant(
+              non_constant(true),
+              by_non_const_ref(b),
+              non_constant(make_in_place<int>(3)),
+              by_non_const_ref('c')))::binding_list_t>);
+
+  return true;
+}
+
+static_assert(check_in_place_bound_args_modifier_distributive_property());
+
+template <typename B>
+using first_policy =
+    binding_policy<type_list_element_t<0, typename B::binding_list_t>>;
+
+template <typename B>
+using store = typename first_policy<B>::storage_type;
+
+constexpr auto check_in_place_binding_storage_type() {
+  int b = 2;
+
+  static_assert(std::is_same_v<store<decltype(constant(b))>, const int>);
+  static_assert(std::is_same_v<store<decltype(non_constant(b))>, int>);
+  static_assert(std::is_same_v<store<decltype(constant(5))>, const int>);
+  static_assert(std::is_same_v<store<decltype(non_constant(5))>, int>);
+
+  static_assert(std::is_same_v<store<decltype(by_ref(b))>, const int&>);
+  static_assert(
+      std::is_same_v<store<decltype(constant(by_ref(b)))>, const int&>);
+  static_assert(std::is_same_v<store<decltype(by_non_const_ref(b))>, int&>);
+  static_assert(std::is_same_v<store<decltype(by_ref(5))>, const int&&>);
+  static_assert(
+      std::is_same_v<store<decltype(constant(by_ref(5)))>, const int&&>);
+  static_assert(std::is_same_v<store<decltype(by_non_const_ref(5))>, int&&>);
+
+  static_assert(
+      std::is_same_v<
+          first_policy<decltype(make_in_place<int>(5))>::storage_type,
+          int>);
+  static_assert(
+      std::is_same_v<
+          store<decltype(constant(make_in_place<int>(5)))>,
+          const int>);
+
+  return true;
+}
+
+static_assert(check_in_place_binding_storage_type());
+
+constexpr auto check_unsafe_move() {
+  int y = 5;
+  bound_args one_ref{y};
+  bound_args wrapped{bound_args_unsafe_move::from(std::move(one_ref))};
+
+  bool made = false;
+  bool* made_ptr = &made;
+  auto in_place = make_in_place<Foo>(made_ptr, y);
+
+  bound_args merged1{
+      0xdeadbeef,
+      bound_args_unsafe_move::from(std::move(wrapped)),
+      bound_args_unsafe_move::from(std::move(in_place))};
+  auto merged2 = bound_args_unsafe_move::from(std::move(merged1));
+
+  test(!made);
+
+  static_assert(
+      std::is_same_v<
+          decltype(merged2),
+          bound_args<
+              unsigned int&&, // the now-destroyed ephemeral 0xdeadbeef
+              bound_args<int&>, // wrapped ref to `y`
+              in_place_bound_args<Foo, bool*&, int&>>>);
+
+  return true;
+}
+
+static_assert(check_unsafe_move());
+
+// A minimal test for `using signature_type = storage_type`...
+static_assert(
+    std::is_same_v<
+        typename first_policy<decltype(constant(by_ref(5)))>::signature_type,
+        const int&&>);
+
+} // namespace folly::bindings::detail
+
+#endif
