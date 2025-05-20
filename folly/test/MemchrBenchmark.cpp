2036f1fc84fead0614a4e758a31b5f7b874b98a0
Alexander Komarov <izard.ak@gmail.com>
2025-05-20 11:31:58 +0200

    Addressed review item
    replaced volatile with folly::compiler_must_not_predict(result_offset); as requested by reviewer

--- folly/test/MemchrBenchmark.cpp
+++ folly/test/MemchrBenchmark.cpp
@@ -71,13 +71,15 @@ static void addMemchrBenchmark(const std::string& name) {
 
 // Only to fool compiler optimizer not to optimize out unused results, could 
 // have used pragmas for that
-volatile static uint64_t result_offset = 0;
+static uint64_t result_offset = 0;
 static FOLLY_NOINLINE void* std_memchr(const void *s, int c, size_t l) {
+  folly::compiler_must_not_predict(result_offset);
   result_offset =  (uint64_t)std::memchr((void *)s, c, l)-(uint64_t)s;
   return (void*)((uint64_t)s + result_offset);
 }
 
 static FOLLY_NOINLINE void* folly_memchr(const void *s, int c, size_t l) {
+  folly::compiler_must_not_predict(result_offset);
   result_offset =  (uint64_t)folly::memchr_long((void *)s, c, l)-(uint64_t)s;
   return (void*)((uint64_t)s + result_offset);
 }
