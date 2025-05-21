60a0450fd053c114d2d09f3484c1caf494db61ed
generatedunixname89002005307016 <generatedunixname89002005307016@meta.com>
2025-05-20 17:53:28 -0700

    Add missing Pyre mode headers] [batch:9/1757] [shard:20/N] [A]
    Differential Revision: D75072880
    
    fbshipit-source-id: 240a16415fd4d91024d6bf64f7a88aa138223495

--- folly/python/test/test_set_executor.py
+++ folly/python/test/test_set_executor.py
@@ -13,6 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# pyre-unsafe
+
 import unittest
 
 # pyre-fixme[21]: Could not find name `test_set_executor_cython` in `folly.python.test`.
