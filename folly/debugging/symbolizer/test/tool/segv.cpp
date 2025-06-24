a5b93a4af57e062f71421ebb457f1b0e00c369db
Yedidya Feldblum <yfeldblum@meta.com>
2025-06-23 21:46:35 -0700

    extend signal-handler segv testing tool with sigaltstack support
    Reviewed By: dmm-fb
    
    Differential Revision: D77172719
    
    fbshipit-source-id: fa53c8612bd05dc9b6bf2317a395598730e055fb

--- folly/debugging/symbolizer/test/tool/segv.cpp
+++ folly/debugging/symbolizer/test/tool/segv.cpp
@@ -17,28 +17,50 @@
 #include <cstdlib>
 #include <cstring>
 
+#ifndef _WIN32
+#include <signal.h>
+#endif
+
 #include <folly/debugging/symbolizer/SignalHandler.h>
 #include <folly/portability/SysMman.h>
 #include <folly/portability/Unistd.h>
 
-static bool should_install_handlers(int argc, char** argv) {
+static bool has_opt(
+    int const argc, char const* const* const argv, char const* const name) {
   for (int i = 1; i < argc; ++i) {
-    if (strcmp(argv[i], "--handle") == 0) {
+    auto const arg = argv[i];
+    if (arg[0] == '-' && arg[1] == '-' && strcmp(arg + 2, name) == 0) {
       return true;
     }
   }
   return false;
 }
 
-int main(int argc, char** argv) {
+[[maybe_unused]] static constexpr size_t kPageSize = 4096;
+
+int main(int const argc, char const* const* const argv) {
   fprintf(stderr, "%d\n", int(getpid()));
   fflush(stderr);
 
-  if (should_install_handlers(argc, argv)) {
+#ifndef _WIN32
+  if (has_opt(argc, argv, "altstack")) {
+    auto const perms = PROT_READ | PROT_WRITE;
+    auto const flags = MAP_ANONYMOUS | MAP_PRIVATE;
+    auto const alloc = mmap(nullptr, kPageSize * 4, perms, flags, -1, 0);
+    stack_t ss;
+    ss.ss_sp = alloc;
+    ss.ss_size = kPageSize * 4;
+    ss.ss_flags = 0;
+    sigaltstack(&ss, nullptr);
+  }
+#endif
+
+  if (has_opt(argc, argv, "handle")) {
     folly::symbolizer::installFatalSignalHandler();
   }
 
+  auto const perms = PROT_NONE;
   auto const flags = MAP_ANONYMOUS | MAP_PRIVATE;
-  auto const alloc = mmap(nullptr, 4096, PROT_NONE, flags, -1, 0);
+  auto const alloc = mmap(nullptr, kPageSize, perms, flags, -1, 0);
   return *static_cast<int const*>(alloc); // read the first int: segv
 }
