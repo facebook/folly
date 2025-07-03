8b0eef8b3ae770cef4ac7d89144085e2b598fa3b
Richard Barnes <rbarnes@meta.com>
2025-07-02 21:52:16 -0700

    Use `co_withExecutor` in folly/docs/examples/folly/coro/AsyncScope.cpp +5
    Summary:
    `yourTask().scheduleOn(ex)` is deprecated in favor of `co_withExecutor(ex, yourTask())`
    
    For `Task`, both forms are equivalent, but `co_withExecutor` makes it much easier to migrate to the [lifetime-safe `NowTask`](https://fb.prod.workplace.com/groups/192968587972839/posts/1784449738824708) (and upcoming `SafeTask`).
    
    Thanks to [C++ ADL](https://en.cppreference.com/w/cpp/language/adl.html), you can write simply `co_withExecutor`; it does not *have* to be qualified with the namespace `folly::coro::`.
    
    [This post](https://fb.prod.workplace.com/groups/192968587972839/posts/1693720697897613) explains the safety benefits of `co_withExecutor`. Moreover, the new name better aligns with the other `folly::coro` protocol of `co_withCancellation()`.
    
    This diff generated with:
    ```
    fbpython fbcode/scripts/rbarnes/schedule_on_fixer.py
    ```
    
     - If you approve of this diff, please use the "Accept & Ship" button :-)
    
    Reviewed By: meyering
    
    Differential Revision: D77703066
    
    fbshipit-source-id: 248fa963e7ed94a0d443f8041745cbaade0db335

--- folly/docs/examples/folly/coro/CancellableAsyncScope.cpp
+++ folly/docs/examples/folly/coro/CancellableAsyncScope.cpp
@@ -35,9 +35,9 @@ CO_TEST(CancellableAsyncScope, demo) {
   };
 
   folly::coro::CancellableAsyncScope scope;
-  scope.add(incrementBy5().scheduleOn(folly::getGlobalCPUExecutor()));
-  scope.add(incrementBy10().scheduleOn(folly::getGlobalCPUExecutor()));
-  scope.add(incrementBy100().scheduleOn(folly::getGlobalCPUExecutor()));
+  scope.add(co_withExecutor(folly::getGlobalCPUExecutor(), incrementBy5()));
+  scope.add(co_withExecutor(folly::getGlobalCPUExecutor(), incrementBy10()));
+  scope.add(co_withExecutor(folly::getGlobalCPUExecutor(), incrementBy100()));
 
   EXPECT_FALSE(scope.isScopeCancellationRequested());
 
