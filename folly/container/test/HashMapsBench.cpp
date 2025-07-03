bed6aa8a66a6d3253cb0697b1381e81d5affe0c1
Yedidya Feldblum <yfeldblum@meta.com>
2025-07-03 16:33:38 -0700

    post for migrating from folly::sformat to fmt::format in folly/
    Reviewed By: dtolnay
    
    Differential Revision: D77640458
    
    fbshipit-source-id: 70fd54d20eb52148aea2f6a267fd54a8041c6395

--- folly/container/test/HashMapsBench.cpp
+++ folly/container/test/HashMapsBench.cpp
@@ -388,7 +388,7 @@ void runAllHashMapTests() {
 #define Z(test, map, key, value_size)                                          \
   for (auto size = FLAGS_map_size_min; size <= FLAGS_map_size_max;             \
        size *= FLAGS_map_size_step) {                                          \
-    auto value = folly::sformat("a[{}]", #value_size);                         \
+    auto value = fmt::format("a[{}]", #value_size);                            \
     tests[size][#test][#key][value][#map] = [=](int iters) {                   \
       benchmark##test<map, key, std::array<uint8_t, value_size>>(iters, size); \
       return iters;                                                            \
