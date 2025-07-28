f3788065c41a127c363ae109e968f39ad6a37986
generatedunixname89002005287564 <generatedunixname89002005287564@meta.com>
2025-07-28 15:00:01 -0700

    fbcode//folly/container/test:evicting_cache_map_test
    Reviewed By: dtolnay
    
    Differential Revision: D79073536
    
    fbshipit-source-id: 81455fbf936e1ae596aec4ee39b047e40d7d8387

--- folly/container/test/EvictingCacheMapTest.cpp
+++ folly/container/test/EvictingCacheMapTest.cpp
@@ -587,7 +587,7 @@ TEST(EvictingCacheMap, IteratorSanityTest) {
 
   std::set<int> seen;
   for (auto& it : map) {
-    EXPECT_EQ(0, seen.count(it.first));
+    EXPECT_EQ(0, seen.contains(it.first));
     seen.insert(it.first);
     EXPECT_EQ(it.first * 2, it.second);
   }
