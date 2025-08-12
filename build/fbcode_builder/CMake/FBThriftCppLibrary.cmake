c939730adc55e9012c66e3a0ec47cb6c55aaf8b5
Victor Zverovich <viz@meta.com>
2025-08-12 10:05:01 -0700

    Add support for decoupled serialization to OSS
    Summary: Update the OSS configs to use `<module>_types_serialization.cpp` introduced in D79106423 and remove the `__FBTHRIFT_SEPARATE_SERIALIZATION` workaround.
    
    Reviewed By: iahs
    
    Differential Revision: D80006546
    
    fbshipit-source-id: cdffcef00baff6bb2109a10f59e4cc34439669c7

--- build/fbcode_builder/CMake/FBThriftCppLibrary.cmake
+++ build/fbcode_builder/CMake/FBThriftCppLibrary.cmake
@@ -69,8 +69,9 @@ function(add_fbthrift_cpp_library LIB_NAME THRIFT_FILE)
     "${output_dir}/gen-cpp2/${base}_data.h"
     "${output_dir}/gen-cpp2/${base}_data.cpp"
     "${output_dir}/gen-cpp2/${base}_types.cpp"
-    "${output_dir}/gen-cpp2/${base}_types_compact.cpp"
     "${output_dir}/gen-cpp2/${base}_types_binary.cpp"
+    "${output_dir}/gen-cpp2/${base}_types_compact.cpp"
+    "${output_dir}/gen-cpp2/${base}_types_serialization.cpp"
     "${output_dir}/gen-cpp2/${base}_metadata.cpp"
   )
   foreach(service IN LISTS ARG_SERVICES)
@@ -85,6 +86,7 @@ function(add_fbthrift_cpp_library LIB_NAME THRIFT_FILE)
       "${output_dir}/gen-cpp2/${service}AsyncClient.cpp"
       "${output_dir}/gen-cpp2/${service}_processmap_binary.cpp"
       "${output_dir}/gen-cpp2/${service}_processmap_compact.cpp"
+      "${output_dir}/gen-cpp2/${service}_processmap_serialization.cpp"
     )
   endforeach()
 
