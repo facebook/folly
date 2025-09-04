7068d24e846ca7c19a37641c2dcb81cc042e9a39
Yuxuan Chen <ych@meta.com>
2025-09-03 19:40:09 -0700

    implement libc++ specific whole word bitset find_first and find_next functions
    Summary:
    Efficient (but unfortunately, not very clean) implementation of bitset find_first and find_next using bit counting STL functions one word at a time.
    
    The motivation of this diff is to provide an implementation for `std::bitset::_Find_first()` and `std::bitset::_Find_next()` which are provided as extensions only in GNU C++. These two functions are not standardized so unfortunately libc++ won't implement them. To make things worse, the specification of bitset doesn't expose any implementation detail on the underlying data structure which are usually implemented as [words](https://github.com/llvm/llvm-project/blob/main/libcxx/include/bitset#L199C3-L199C17).
    
    The implementation here chose to poke into `std::__bit_reference` since it's relatively stable. We can obtain such from the `operator[]`. Then, we use the dangerous `reinterpret_cast` to find the underlying word
    
    This implementation did not refer to how `_Find_first` and `_Find_next` are implemented due to licensing reasons. I implemented this strategy based on the belief that checking entire words are faster than checking it bit by bit. It's not hard to see that repeating the whole steps of reading a word from memory, computing a mask, and checking eq to zero is a lot slower than `std::countr_zero` at word level.
    
    My microbenchmark results seem to support this claim.
    
    Reviewed By: yfeldblum
    
    Differential Revision: D81173093
    
    fbshipit-source-id: 8590d2dbb4300d397e094b8be67df327bf7fdb04

--- folly/container/StdBitset.h
+++ folly/container/StdBitset.h
@@ -19,10 +19,77 @@
 #include <folly/CPortability.h>
 #include <folly/Portability.h>
 
+#include <bit>
 #include <bitset>
 #include <cassert>
 
 namespace folly {
+
+#ifdef _LIBCPP_VERSION
+
+constexpr size_t kWordSize = 64;
+
+namespace libcpp_detail {
+struct std_bit_reference_layout {
+  size_t* address;
+  size_t mask;
+};
+
+// Return the content of the 64-bit word, given an index.
+template <size_t N>
+size_t* get_underlying_data_region(const std::bitset<N>& bitset) {
+  // You are only getting the __bit_reference as the return if you access
+  // with operator[], which is not const.
+  auto& bitset_ref = const_cast<std::bitset<N>&>(bitset);
+  std::__bit_reference bitref = bitset_ref[0];
+  auto* br =
+      reinterpret_cast<std_bit_reference_layout*>(std::addressof(bitref));
+  return br->address;
+}
+
+template <size_t N>
+size_t std_bitset_find_next(const std::bitset<N>& bitset, size_t start) {
+  constexpr size_t max_words = N / kWordSize + 1;
+  if constexpr (N == 0) {
+    return 1;
+  }
+
+  // Check starting in the middle of a word
+  size_t start_in_word = start % kWordSize;
+  size_t word_idx = start / kWordSize;
+  size_t* data = get_underlying_data_region(bitset);
+
+  if (start_in_word) {
+    // Invariant: start != 0, 1 <= start_in_word <= 63
+    // test the remaining bits in the same word as previous
+    size_t mask = (~size_t(0)) << start_in_word;
+    size_t partial_word = data[word_idx] & mask;
+
+    size_t idx_in_word = std::countr_zero(partial_word);
+    if (idx_in_word != kWordSize) {
+      // We found the next
+      return start - start_in_word + idx_in_word;
+    }
+    // Special treatment of the partial first word was done. Skip it in the
+    // next loop for whole words.
+    word_idx += 1;
+  }
+
+  while (word_idx < max_words) {
+    size_t word = data[word_idx];
+    size_t idx_in_word = std::countr_zero(word);
+    if (idx_in_word != kWordSize) {
+      // we found the first
+      return word_idx * kWordSize + idx_in_word;
+    }
+    word_idx += 1;
+  }
+
+  return N;
+}
+} // namespace libcpp_detail
+
+#endif // _LIBCPP_VERSION
 
 struct std_bitset_find_first_fn {
   /// Return the index of the first set bit in a bitset, or bitset.size() if
@@ -30,13 +97,14 @@ struct std_bitset_find_first_fn {
   template <size_t N>
   FOLLY_ALWAYS_INLINE size_t
   operator()(const std::bitset<N>& bitset) const noexcept {
-    // equivalent to #if defined(__GLIBCXX__)
-    if constexpr (kIsGlibcxx) {
-      // GNU provides non-standard (its a hold over from the original SGI
-      // implementation) _Find_first(), which efficiently returns the index of
-      // the first set bit.
-      return bitset._Find_first();
-    }
+#if defined(__GLIBCXX__)
+    // GNU provides non-standard (its a hold over from the original SGI
+    // implementation) _Find_first(), which efficiently returns the index of
+    // the first set bit.
+    return bitset._Find_first();
+#elif defined(_LIBCPP_VERSION)
+    return folly::libcpp_detail::std_bitset_find_next(bitset, 0);
+#endif
 
     for (size_t i = 0; i < bitset.size(); ++i) {
       if (bitset[i]) {
@@ -58,13 +126,14 @@ struct std_bitset_find_next_fn {
   operator()(const std::bitset<N>& bitset, size_t prev) const noexcept {
     assert(prev < bitset.size());
 
-    // equivalent to #if defined(__GLIBCXX__)
-    if constexpr (kIsGlibcxx) {
-      // GNU provides non-standard (its a hold over from the original SGI
-      // implementation) _Find_next(), which given an index, efficiently returns
-      // the index of the first set bit after the index.
-      return bitset._Find_next(prev);
-    }
+#if defined(__GLIBCXX__)
+    // GNU provides non-standard (its a hold over from the original SGI
+    // implementation) _Find_next(), which given an index, efficiently returns
+    // the index of the first set bit after the index.
+    return bitset._Find_next(prev);
+#elif defined(_LIBCPP_VERSION)
+    return folly::libcpp_detail::std_bitset_find_next(bitset, prev + 1);
+#endif
 
     for (size_t i = prev + 1; i < bitset.size(); ++i) {
       if (bitset[i]) {
