8f97fed6ace7a4d8c64523e3f8f7168ec7612c33
Yedidya Feldblum <yfeldblum@meta.com>
2025-10-02 09:39:41 -0700

    cpu_set_state wrapping sched_getaffinity()
    Reviewed By: skrueger
    
    Differential Revision: D83265685
    
    fbshipit-source-id: b90382dc2df909ef293002f84cf99403380557f4

--- folly/system/HardwareConcurrency.cpp
+++ folly/system/HardwareConcurrency.cpp
@@ -18,19 +18,57 @@
 
 #include <thread>
 
+#include <folly/Utility.h>
+#include <folly/lang/SafeAssert.h>
 #include <folly/portability/Sched.h>
 
 namespace folly {
 
-unsigned int hardware_concurrency() noexcept {
-#if defined(__linux__) && !defined(__ANDROID__)
-  cpu_set_t cpuset;
-  if (!sched_getaffinity(0, sizeof(cpuset), &cpuset)) {
-    auto count = CPU_COUNT(&cpuset);
-    if (count != 0) {
-      return count;
+#if defined(__linux__)
+
+cpu_set_state cpu_set_state::ask(cpu_set_t* stackset) noexcept {
+  static std::atomic<std::size_t> setszc{0}; // monotonic, like in the kernel
+  auto setszcv = setszc.load(std::memory_order_relaxed);
+  auto setsz = setszcv ? setszcv : CPU_SETSIZE;
+  auto set = stackset && setsz == CPU_SETSIZE ? stackset : CPU_ALLOC(setsz);
+  while (true) {
+    FOLLY_SAFE_CHECK(!!set);
+    /// sched_getaffinity:
+    /// * returns 0 on success and -1 on failure
+    /// * fails with EINVAL if the provided set is too small
+    int const ret = sched_getaffinity(0, CPU_ALLOC_SIZE(setsz), set);
+    if (ret == 0) {
+      break;
+    }
+    FOLLY_SAFE_PCHECK(errno == EINVAL);
+    CPU_FREE(set == stackset ? nullptr : set);
+    setsz *= 2;
+    set = CPU_ALLOC(setsz);
+  }
+  /// atomic-max: setszc <- max(setszc, setsz)
+  while (setsz > setszcv) {
+    if (setszc.compare_exchange_strong(
+            setszcv, setsz, std::memory_order_relaxed)) {
+      setszcv = setsz;
     }
   }
+  FOLLY_SAFE_CHECK(setsz <= setszcv);
+  return {set, setsz, set != stackset};
+}
+
+cpu_set_state::~cpu_set_state() {
+  CPU_FREE(!alloc_ ? nullptr : set_);
+  set_ = nullptr;
+  size_ = 0;
+  alloc_ = false;
+}
+
+#endif
+
+unsigned int hardware_concurrency() noexcept {
+#if defined(__linux__)
+  cpu_set_t stackset;
+  return to_narrow(cpu_set_state::ask(&stackset).cpu_count());
 #endif
 
   return std::thread::hardware_concurrency();
