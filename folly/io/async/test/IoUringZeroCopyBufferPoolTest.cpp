8eb23beee7da782588a7356b224b0f9618c9387d
David Wei <davidhwei@meta.com>
2025-11-14 12:03:11 -0800

    IoUringZeroCopyBufferPool: add basic test for refill
    Summary: Add a basic test that exercises the different paths through `IoUringZeroCopyBufferPool::returnBuffer()`.
    
    Reviewed By: vishwanath1306
    
    Differential Revision: D87034950
    
    fbshipit-source-id: 0227115f70e9601aba001caa8e91e7347c51ad0c

--- folly/io/async/test/IoUringZeroCopyBufferPoolTest.cpp
+++ folly/io/async/test/IoUringZeroCopyBufferPoolTest.cpp
@@ -29,6 +29,18 @@ class IoUringZeroCopyBufferPoolTestHelper {
     return IoUringZeroCopyBufferPool::UniquePtr(new IoUringZeroCopyBufferPool(
         params, IoUringZeroCopyBufferPool::TestTag{}));
   }
+
+  explicit IoUringZeroCopyBufferPoolTestHelper(IoUringZeroCopyBufferPool& pool)
+      : pool(pool) {}
+
+  uint32_t* getHead() { return pool.rqRing_.khead; }
+  uint32_t getRingUsedCount() { return pool.rqTail_ - *getHead(); }
+  uint32_t getRingFreeCount() {
+    return pool.rqEntries_ - (pool.rqTail_ - *getHead());
+  }
+  size_t getPendingBuffersSize() { return pool.pendingBuffers_.size(); }
+
+  IoUringZeroCopyBufferPool& pool;
 };
 } // namespace folly
 
@@ -44,9 +56,9 @@ TEST(IoUringZeroCopyBufferPoolTest, GetBuf) {
       .queueId = 0,
   };
   auto pool = IoUringZeroCopyBufferPoolTestHelper::create(params);
-  io_uring_cqe cqe;
+  io_uring_cqe cqe{};
   cqe.res = 2048;
-  io_uring_zcrx_cqe zcqe;
+  io_uring_zcrx_cqe zcqe{};
   zcqe.off = 0;
   auto buf = pool->getIoBuf(&cqe, &zcqe);
   ASSERT_EQ(buf->capacity(), 4096);
@@ -63,9 +75,9 @@ TEST(IoUringZeroCopyBufferPoolTest, DelayedDestruction) {
       .queueId = 0,
   };
   auto pool = IoUringZeroCopyBufferPoolTestHelper::create(params);
-  io_uring_cqe cqe;
+  io_uring_cqe cqe{};
   cqe.res = 2048;
-  io_uring_zcrx_cqe zcqe;
+  io_uring_zcrx_cqe zcqe{};
   zcqe.off = 0;
   auto buf1 = pool->getIoBuf(&cqe, &zcqe);
   cqe.res = 2048;
@@ -74,4 +86,51 @@ TEST(IoUringZeroCopyBufferPoolTest, DelayedDestruction) {
   buf1.reset();
   pool.reset();
   buf2.reset();
+}
+
+TEST(IoUringZeroCopyBufferPoolTest, RefillTest) {
+  IoUringZeroCopyBufferPool::Params params = {
+      .ring = nullptr,
+      .numPages = 8,
+      .pageSize = 4096,
+      .rqEntries = 2,
+      .ifindex = 0,
+      .queueId = 0,
+  };
+  auto pool = IoUringZeroCopyBufferPoolTestHelper::create(params);
+  IoUringZeroCopyBufferPoolTestHelper helper(*pool);
+  EXPECT_EQ(helper.getRingUsedCount(), 0);
+  EXPECT_EQ(helper.getRingFreeCount(), 2);
+
+  io_uring_cqe cqe{};
+  io_uring_zcrx_cqe zcqe{};
+
+  cqe.res = 2048;
+  zcqe.off = 0;
+  auto buf1 = pool->getIoBuf(&cqe, &zcqe);
+
+  zcqe.off += 4096;
+  auto buf2 = pool->getIoBuf(&cqe, &zcqe);
+
+  zcqe.off += 4096;
+  auto buf3 = pool->getIoBuf(&cqe, &zcqe);
+
+  buf1.reset();
+  buf2.reset();
+  buf3.reset();
+  EXPECT_EQ(helper.getRingUsedCount(), 2);
+  EXPECT_EQ(helper.getRingFreeCount(), 0);
+  EXPECT_EQ(helper.getPendingBuffersSize(), 1);
+
+  *helper.getHead() += 2;
+  EXPECT_EQ(helper.getRingUsedCount(), 0);
+  EXPECT_EQ(helper.getRingFreeCount(), 2);
+  EXPECT_EQ(helper.getPendingBuffersSize(), 1);
+
+  zcqe.off += 4096;
+  auto buf4 = pool->getIoBuf(&cqe, &zcqe);
+  buf4.reset();
+  EXPECT_EQ(helper.getRingUsedCount(), 2);
+  EXPECT_EQ(helper.getRingFreeCount(), 0);
+  EXPECT_EQ(helper.getPendingBuffersSize(), 0);
 }
