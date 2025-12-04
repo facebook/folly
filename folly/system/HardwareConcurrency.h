1a6b29a7f27c8d2d72fda2821b31f53c60f7e814
Yedidya Feldblum <yfeldblum@meta.com>
2025-12-04 13:49:54 -0800

    rename and document folly::available_concurrency
    Summary:
    * Rename from `folly::hardware_concurrency` to `folly::available_concurrency`.
    * Document the main difference compared to `std::thread::hardware_concurrency`.
    
    Reviewed By: luciang
    
    Differential Revision: D87954227
    
    fbshipit-source-id: 65ad6afbac4646891e9bd84b2f50c93fada2246c

--- folly/system/HardwareConcurrency.h
+++ folly/system/HardwareConcurrency.h
@@ -85,6 +85,46 @@ class cpu_set_state {
 
 #endif
 
-unsigned int hardware_concurrency() noexcept;
+/// available_concurrency
+///
+/// Returns a number suitable for use as a size for thread-pools or as a size
+/// for cpu-striped data-structures. Consider the purpose of this function as
+/// returning some measure of the concurrency available to the calling program.
+///
+/// Caution: May fail by returning 0, which is never a success value.
+///
+/// Caution: The value returned may change over the lifetime of the process or
+/// of the system.
+///
+/// Caution: This does not necessarily return a number of processors. This is
+/// *not* an alias, for example, for get_nprocs, get_nprocs_conf, or sysconf
+/// with key any of _SC_NPROCESSORS_ONLN or _SC_NPROCESSORS_CONF.
+///
+/// Caution: May succeed but return a number that is not necessarily larger than
+/// the maximum value returnable by a call to getcpu(). For cpu-striped data
+/// structures, this means that the number returned by getcpu() may not be a
+/// valid index into the data-structure; either the result must be %'d by the
+/// number of stripes, or the number of stripes must be the bit-ceiling of the
+/// result of this function and then the result of getcpu() must be &'d. For
+/// use-cases which must be free of % and & operations, this function is not
+/// suitable.
+///
+/// Note: This may use arbitrary sources of information (signals) as inputs for
+/// calculating a suitable return value. It is not specified to use any specific
+/// signal in any specific way.
+/// * The result of std::thread::hardware_concurrency (all).
+/// * The result of get_nprocs, get_nprocs_conf, or sysconf as noted above.
+/// * The result of sched_getaffinity (linux but not android).
+/// * Unspecified others....
+///
+/// mimic: std::thread::hardware_concurrency
+unsigned int available_concurrency() noexcept;
+
+/// hardware_concurrency
+///
+/// An alias for available_concurrency.
+inline unsigned int hardware_concurrency() noexcept {
+  return available_concurrency();
+}
 
 } // namespace folly
