7e50d1042c972762438c0848176bdc57fb46fe74
Dmitry Ilvokhin <ilvokhin@meta.com>
2026-01-06 04:48:40 -0800

    Reduce amount of code on F14 find hot path
    Summary:
    Before this change we evaluated two branches for each find: one for
    `sz == 0` and one for `sz == 1`. In cases, when container is not empty we can
    evaluate only one branch. Moreover, turns out, when we nest branches compiler
    is smart enough to avoid shifts for size calculation.
    
    Before:
    
    `F14Value`:
    
    ```
    00000000002252d0 <codeSize_find_F14Value(<...>)>:
      2252d0: mov    0x8(%rdi),%rax
      2252d4: mov    %rax,%rcx
      2252d7: shr    $0x8,%rcx                                      ; This shift can
                                                                    ; be avoided.
      2252db: je     2252ff <codeSize_find_F14Value(<...>)+0x2f>    ; Branch #1.
      2252dd: push   %rbp
      2252de: mov    %rsp,%rbp
      2252e1: push   %r14
      2252e3: push   %rbx
      2252e4: cmp    $0x1,%rcx
      2252e8: jne    225302 <codeSize_find_F14Value(<...>)+0x32>    ; Branch #2.
      2252ea: mov    0x10(%rdi),%rcx
      2252ee: and    $0xfffffffffffffff8,%rcx
      2252f2: xor    %eax,%eax
      2252f4: cmp    %si,(%rcx)
      2252f7: sete   %al
      2252fa: pop    %rbx
      2252fb: pop    %r14
      2252fd: pop    %rbp
      2252fe: ret
      2252ff: xor    %eax,%eax
      225301: ret
      225302: movswq %si,%rdx
      <...>
    ```
    
    `F14Vector`:
    
    ```
    0000000000225490 <codeSize_find_F14Vector(<...>)>:
      225490: mov    0x10(%rdi),%r8
      225494: xor    %eax,%eax
      225496: cmp    $0x100,%r8                         ; No shift here:
                                                        ; 0x100 >> 8 == 1 (size).
      22549d: jb     22554b <codeSize_find_F14Vector(<...>)+0xbb>
      2254a3: push   %rbp
      2254a4: mov    %rsp,%rbp
      2254a7: push   %r15
      2254a9: push   %r14
      2254ab: push   %rbx
      2254ac: movswq %si,%rax
      <...>
    ```
    
    After:
    
    `F14Value`:
    
    ```
    00000000002252c0 <codeSize_find_F14Value(<...>)>:
      2252c0: push   %rbp
      2252c1: mov    %rsp,%rbp
      2252c4: push   %r14
      2252c6: push   %rbx
      2252c7: mov    0x8(%rdi),%r8
      2252cb: cmp    $0x1ff,%r8                                  ; No shift anymore.
      2252d2: ja     2252f4 <codeSize_find_F14Value(<...>)+0x34> ; We jump directly
                                                                 ; to findImpl from
                                                                 ; here.
      2252d4: xor    %eax,%eax
      2252d6: cmp    $0x100,%r8                                  ;
      2252dd: jb     2252ef <codeSize_find_F14Value(<...>)+0x2f>
      2252df: mov    0x10(%rdi),%rcx
      2252e3: and    $0xfffffffffffffff8,%rcx
      2252e7: xor    %eax,%eax
      2252e9: cmp    %si,(%rcx)
      2252ec: sete   %al
      2252ef: pop    %rbx
      2252f0: pop    %r14
      2252f2: pop    %rbp
      2252f3: ret
      2252f4: movswq %si,%rax
      <...>
    ```
    
    `F14Vector`:
    
    ```
    0000000000225480 <codeSize_find_F14Vector(<...>)>:
      225480: mov    0x10(%rdi),%r8
      225484: xor    %eax,%eax
      225486: cmp    $0x100,%r8
      22548d: jb     22553b <codeSize_find_F14Vector(<...>)+0xbb>
      225493: push   %rbp
      225494: mov    %rsp,%rbp
      225497: push   %r15
      225499: push   %r14
      22549b: push   %rbx
      22549c: movswq %si,%rax
      <...>
    ```
    
    Reviewed By: ot
    
    Differential Revision: D89190976
    
    fbshipit-source-id: 43015f030f4b9792f28157ffd47f4f989913689c

--- folly/container/detail/F14Table.h
+++ folly/container/detail/F14Table.h
@@ -1769,15 +1769,15 @@ class F14Table : public Policy {
   template <typename K>
   FOLLY_ALWAYS_INLINE ItemIter find(K const& key) const {
     const auto sz = size();
-    if (sz == 0) {
-      return ItemIter{};
-    }
-    // There is no easy way to obtain a begin iterator with the current policy
-    // design.
-    // As a result, F14Vector* containers (kEnableItemIteration == false) do
-    // not benefit from this optimization yet.
-    if constexpr (kEnableItemIteration) {
-      if (sz == 1) {
+    if (sz <= 1) {
+      if (sz == 0) {
+        return ItemIter{};
+      }
+      // There is no easy way to obtain a begin iterator with the current policy
+      // design.
+      // As a result, F14Vector* containers (kEnableItemIteration == false) do
+      // not benefit from this optimization yet.
+      if constexpr (kEnableItemIteration) {
         ItemIter beg = begin();
         return this->keyMatchesItem(key, beg.citem()) ? beg : ItemIter{};
       }
