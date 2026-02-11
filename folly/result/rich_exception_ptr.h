0bb941b22fd3fd2f4c6a9d7e79ea9a0d17f9a507
Alexey Spiridonov <lesha@meta.com>
2026-02-11 01:23:01 -0800

    result no longer implemented via Expected
    Summary:
    The change has a few main benefits:
     - For types with throwing move ctors, the new `result<T>` has no valueless-by-exception state, unlike `folly::Expected`, which does. Simpler usage model, simpler code.
     - The new `result<>` (i.e. `void`) is only 8 bytes, not 16.
     - Perf is better. Up-stack, I replaced `Try<T>` with `result<T>` inside of `folly::coro` tasks, and ran some benchmarks. The old `result` was slower than `Try`, and the offending code mapped onto the `Expected::operator=` impl. The new `result` was neutral-to-better than `Try` despite being more feature-rich.
    
    Replacing the internals of `result` not to use `folly::Expected` is tricky, because some of the interface behaviors used to delegate to `Expected`. To make sure I broke nothing of value, I focused on before/after testing:
     - Stacked below this change are a bunch of new tests for areas that were poorly covered. I.e. I explicitly covered the areas where I was implicitly relying on `Expected` before. D92113457
     - Remove tests for the valuless-by-exception behavior. D92770825
     - Add more benchmarks, to ensure perf noninferiority.
     - Do the refactor (this diff)
     - Add some more tests for the new behaviors (D92775096)
    
    The "before" tests are green, as are the "after" tests. So, all behavior changes are circumscribed by the test changes you see on this diff, and the new tests up-stack.
    
    Reviewed By: janondrusek
    
    Differential Revision: D92590863
    
    fbshipit-source-id: cb03bf8cb184af87111e4bbd840a6a3853c737f3

--- folly/result/rich_exception_ptr.h
+++ folly/result/rich_exception_ptr.h
@@ -32,9 +32,8 @@ class immortal_rich_error_t;
 
 namespace detail {
 
-// These are in `detail` since they're opt-build fallbacks for a debug-fatal.
+// This is in `detail` since it's an opt-build fallback for a debug-fatal.
 struct bad_result_access_error : public std::exception {};
-struct empty_result_error : public std::exception {};
 
 // Stub types that will be replaced by integrations with other folly/ types.
 //
