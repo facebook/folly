4230c7bb0f3e5e5f8aecba6d2fe22b90c67d3632
generatedunixname1401739305002972 <noreply+1401739305002972@fb.com>
2026-04-26 03:05:01 -0700

    Fix PYRE_MISSING_ANNOTATIONS issues in fbcode/folly/python/test/teardown.py
    Differential Revision: D102504637
    
    fbshipit-source-id: b6420a4914d85a449d556649511ad74235d4dea9

--- folly/python/test/teardown.py
+++ folly/python/test/teardown.py
@@ -13,8 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# pyre-unsafe
-
 import asyncio
 import unittest
 from sys import platform
