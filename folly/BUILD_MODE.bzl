facebookincubator/fizz

(fbcode/[f-i] (excl infra_asic_fpga)) Apply starlark_fmt to **/.bzl files (10/68)
Summary: Apply starlark-fmt (BUCKFORMAT) formatting to BUCK/TARGETS/.bzl/.sky files.

Reviewed By: mzlee

Differential Revision: D103610275

fbshipit-source-id: 475a7b187f50346ed61d74ed7ffb6b8e650b0753

fizz/BUILD_MODE.bzl
@@ -4,14 +4,13 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-""" build mode definitions for fizz """
+"""build mode definitions for fizz"""
 
 load("@fbcode//:BUILD_MODE.bzl", get_parent_modes = "get_empty_modes")
 load("@fbcode//fizz:defs.bzl", "FIZZ_CXX_WARNINGS")
 load("@fbcode_macros//build_defs:create_build_mode.bzl", "extend_build_modes")
 
-_extra_cflags = [
-]
+_extra_cflags = []
 
 _common_flags = FIZZ_CXX_WARNINGS
 
@@ -29,8 +28,7 @@ _extra_gcc_flags = _common_flags + [
     "-Wall",
 ]
 
-_tags = [
-]
+_tags = []
 
 _modes = extend_build_modes(
     get_parent_modes(),
@@ -41,5 +39,5 @@ _modes = extend_build_modes(
 )
 
 def get_modes():
-    """ Return modes for this file """
+    """Return modes for this file"""
     return _modes

facebook/folly

folly/BUILD_MODE.bzl
@@ -1,4 +1,4 @@
-""" build mode definitions for folly """
+"""build mode definitions for folly"""
 
 load("@fbcode//:BUILD_MODE.bzl", get_parent_modes = "get_empty_modes")
 load("@fbcode_macros//build_defs:create_build_mode.bzl", "extend_build_modes")
@@ -8,8 +8,7 @@ _extra_cflags = [
     "-Wunused-parameter",
 ]
 
-_extra_cxxflags = [
-]
+_extra_cxxflags = []
 
 _extra_clang_flags = [
     "-Wconditional-uninitialized",
@@ -64,5 +63,5 @@ _modes = extend_build_modes(
 )
 
 def get_modes():
-    """ Return modes for this file """
+    """Return modes for this file"""
     return _modes