e923aadc6888e8c390a28d4a12f65f7830cae59c
Richard Barnes <rbarnes@meta.com>
2026-07-27 15:20:49 -0700

    Suppress false clang17 -Wunreachable-code in fibers/Semaphore
    Summary:
    Fixes a Windows clang 15.0.5 -> 17.0.6 `-Werror,-Wunreachable-code` build break
    surfaced by the toolchain switch in D110606117 (T281071588).
    
    In `Semaphore::co_wait`'s cancellation path, clang17 wrongly concludes the
    `if (cancelled)` block after the `co_await` is unreachable:
    
    ```
    folly/fibers/Semaphore.cpp(176,11): error: code will never be executed [-Werror,-Wunreachable-code]
    ```
    
    The block is genuinely reachable on cancellation (the existing TODO already notes
    the compiler mis-analyzes this coroutine control flow). Wrap it in
    `FOLLY_PUSH_WARNING` / `FOLLY_CLANG_DISABLE_WARNING("-Wunreachable-code")` /
    `FOLLY_POP_WARNING` and include `<folly/CPortability.h>` for those macros.
    Applied to both the `fbcode/folly` and `xplat/folly` copies.
    
    Reviewed By: skrueger
    
    Differential Revision: D113783784
    
    fbshipit-source-id: 0168ffc8139242555617802aad42d135e80978e6

--- folly/fibers/Semaphore.cpp
+++ folly/fibers/Semaphore.cpp
@@ -16,6 +16,8 @@
 
 #include <folly/fibers/Semaphore.h>
 
+#include <folly/CPortability.h>
+
 namespace folly {
 namespace fibers {
 
@@ -172,9 +174,12 @@ coro::Task<void> Semaphore::co_wait() {
         // from a thread requesting cancellation.
         // TODO: This is not unreachable code, but the compiler wrongly thinks
         // it is. Once the compiler is fixed we can remove this.
+        FOLLY_PUSH_WARNING
+        FOLLY_CLANG_DISABLE_WARNING("-Wunreachable-code")
         if (cancelled) {
           co_yield folly::coro::co_stopped_may_throw;
         }
+        FOLLY_POP_WARNING
 
         co_return;
       }
