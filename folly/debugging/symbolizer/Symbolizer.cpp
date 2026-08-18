a46dfd8a6474f988c26f9facd3b71e59d28ae49f
generatedunixname89002005232357 <generatedunixname89002005232357@meta.com>
2026-08-18 09:44:12 -0700

    Revert D115867061: Cache symbolization in getStackTraceStr
    Differential Revision:
    D115867061
    
    Original commit changeset: 6588a0ad2483
    
    Original Phabricator Diff: D115867061
    
    fbshipit-source-id: aeda885cae61ee900052c66f819919043b0cee96

--- folly/debugging/symbolizer/Symbolizer.cpp
+++ folly/debugging/symbolizer/Symbolizer.cpp
@@ -24,7 +24,6 @@
 #include <folly/debugging/symbolizer/Dwarf.h>
 #include <folly/debugging/symbolizer/Elf.h>
 #include <folly/debugging/symbolizer/ElfCache.h>
-#include <folly/debugging/symbolizer/SymbolCache.h>
 #include <folly/debugging/symbolizer/detail/Debug.h>
 #include <folly/lang/SafeAssert.h>
 #include <folly/lang/ToAscii.h>
@@ -584,9 +583,10 @@ std::string getStackTraceStr(
     return {};
   }
 
+  ElfCache elfCache;
   LocationInfoMode mode =
       showFullInfo ? LocationInfoMode::FULL : LocationInfoMode::FAST;
-  Symbolizer symbolizer(/*cache=*/nullptr, mode, getSharedSymbolCache(mode));
+  Symbolizer symbolizer(&elfCache, mode);
   symbolizer.symbolize(addresses, frames, frameCount);
 
   StringSymbolizePrinter printer;
